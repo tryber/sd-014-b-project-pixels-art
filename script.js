6b1fcd4a4f44c953e2809ccbcafbb0099e12a71e
Davisralves <davialves324@gmail.com>
2021-07-08 18:59:39 -0300

    adicionando requisito 7

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function criaPixels(x,y){
+function criaPixels(x,y){           // cria a tabela de pixel art com o tamanho x de lagura e y de altura.
     let div = document.querySelector('#pixel-board');
     
     for (let index = 0; index < y; index ++){
@@ -14,15 +14,45 @@ function criaPixels(x,y){
     } 
 };
 
-function selectColor(color){
-color = document.querySelector('.' + color);
-color.classList.add('selected');
+function selectColorAndRemove(event){                    // Apartir da variavel 'color' adiciona a classe selected
+    let cor = event.target.classList[1];
+    let color = document.querySelector('.' + cor);
+    color.classList.add('selected');
+    switch (cor) {
+        case 'preto':
+        document.querySelector('.vermelho').classList.remove('selected');   
+        document.querySelector('.azul').classList.remove('selected');   
+        document.querySelector('.roxo').classList.remove('selected');
+        break; 
+        case 'vermelho':
+        document.querySelector('.preto').classList.remove('selected');   
+        document.querySelector('.azul').classList.remove('selected');   
+        document.querySelector('.roxo').classList.remove('selected');
+        break;   
+        case 'roxo':
+        document.querySelector('.vermelho').classList.remove('selected');   
+        document.querySelector('.azul').classList.remove('selected');   
+        document.querySelector('.preto').classList.remove('selected');
+        break;
+        case 'azul':
+        document.querySelector('.vermelho').classList.remove('selected');   
+        document.querySelector('.roxo').classList.remove('selected');   
+        document.querySelector('.preto').classList.remove('selected');
+        break;      
+} 
 };
 
-function removeColor(color){
-    color = document.querySelector('.' + color);
-    color.className.remove = 'selected';
-    };
 
+let colors = document.querySelectorAll('.color');
 criaPixels(5,5);
-selectColor('preto');
+colors[0].classList.add('selected'); // seleciona o preto.
+colors[0].addEventListener('click', selectColorAndRemove);
+colors[1].addEventListener('click', selectColorAndRemove);
+colors[2].addEventListener('click', selectColorAndRemove);
+colors[3].addEventListener('click', selectColorAndRemove);
+
+
+
+
+
+
