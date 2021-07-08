dc7777f9b1c1412c61f84fcc2ea7131d90385f8a
Gustavo <gus.inacio@hotmail.com>
2021-07-08 19:27:28 -0300

    Requisito #8 fixed (i guess)

--- script.js
+++ script.js
@@ -16,6 +16,15 @@ yellow.addEventListener('click', handleChangeSelected);
 green.addEventListener('click', handleChangeSelected)
 black.addEventListener('click', handleChangeSelected);
 
+let getPixel = document.getElementsByClassName('pixel')
+for(let index = 0; index < getPixel.length; index +=1){
+    getPixel[index].addEventListener('click', setColor);
+}
+function setColor(event){
+    let getColor = document.querySelector('.selected')
+    let getBgColor = window.getComputedStyle(getColor).getPropertyValue("background-color")
+    event.target.style.backgroundColor = getBgColor
+}
 
 /* Rerencias 
 https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
