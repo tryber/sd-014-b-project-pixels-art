d98b68fb2d7a89fc0b33248b453ce27907b70b6a
Arturo Vaine <arturo.vaine@gmail.com>
2021-07-08 17:48:05 -0300

    Adiciona button e seta classe 'selected' para cor preta na paleta.

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    document.getElementById('black').classList.add('selected');
+    // Requisito 6: 6 - Defina a cor preta como cor inicial. Ao carregar a página, a cor preta já deve estar selecionada para pintar os pixels
+}
+
+
+// var randomColor = Math.floor(Math.random()*16777215).toString(16);
