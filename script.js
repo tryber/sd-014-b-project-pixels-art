212b5558ab044ab1f4a3585cc1ad33eaa75c1b43
Arturo Vaine <arturo.vaine@gmail.com>
2021-07-08 20:33:39 -0300

    Adiciona estilos de classes no css.

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 window.onload = function start() {
   document.getElementById('black').classList.add('selected');
-  // let colorZero = 'black';
-  const firstColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`; // Sugestão ESLint: prefer-template
-  const secondColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  const thirdColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  // 16777215 <--> FFFFFF ; decimal <--> hexadecimal
-  // https://www.w3schools.com/jsref/jsref_tostring_number.asp
-  // https://www.rapidtables.com/convert/number/decimal-to-hex.html
+  let aux = false;
+
+  let firstColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`; // Sugestão ESLint: prefer-template
+  let secondColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  let thirdColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 
   document.getElementById('color1').style.backgroundColor = firstColor;
   document.getElementById('color2').style.backgroundColor = secondColor;
   document.getElementById('color3').style.backgroundColor = thirdColor;
-  // Requisito 6: 6 - Defina a cor preta como cor inicial. Ao carregar a página, a cor preta já deve estar selecionada para pintar os pixels
 };
+
+// Requisito 6: 6 - Defina a cor preta como cor inicial. Ao carregar a página, a cor preta já deve estar selecionada para pintar os pixels
+// let colorZero = 'black';
+// 16777215 <--> FFFFFF ; decimal <--> hexadecimal
+// Função toString, fonte: https://www.w3schools.com/jsref/jsref_tostring_number.asp
+// Conversão decimal <-> hexadecimal, fonte: https://www.rapidtables.com/convert/number/decimal-to-hex.html
+
 
 /*
 function randomColor () {
