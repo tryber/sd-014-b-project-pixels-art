660ce1ec1bf27c265219b46f9e933e47d59bc4c5
Matheus Laurindo <matheus104521@gmail.com>
2021-07-08 18:17:58 -0300

    Conclui o desafio 6

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
-window.onload = function (){
+window.onload = function () {
+    const colors = document.querySelectorAll('.color');
+    
+    for (let key of colors) {
+        const getProp = window.getComputedStyle(key, null).getPropertyValue('background-color');
+
+        if (getProp === 'rgb(0, 0, 0)') {
+            key.classList.add('selected');
+        }
+    };
+}
+
+function generatePixels() {
     let pixelBoard = document.querySelector('#pixel-board');
 
-    //Cria as linhas dos pixels
+    //Cria as linhas dos pixels  
     for(let index = 0; index < 5; index += 1) {
-        let rowBoard = document.createElement('section');
+        const rowBoard = document.createElement('section');
         rowBoard.setAttribute('class', 'row-pixel');
 
         pixelBoard.appendChild(rowBoard);
+        //Cria e adiciona a cada linha, 5 pixels        
+        for (let indice = 1; indice <= 5; indice += 1) { 
+            const rowPixel = document.querySelectorAll('.row-pixel')[index];
 
-        //Cria e adiciona a cada linha, 5 pixels
-        for(let indice = 1; indice <= 5; indice += 1 ) {
-            let rowPixel = document.querySelectorAll('.row-pixel')[index];
-
-            let pixel = document.createElement('div');
+            const pixel = document.createElement('div');
             pixel.setAttribute('class', 'pixel');
 
             rowPixel.appendChild(pixel);
         };
     };
-
 }
 
+generatePixels();
