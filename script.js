478d14d2c7333929045f96acab3a6312e7f9a1f5
Levi Manoel <levimanoel.deob@gmail.br>
2021-07-08 18:46:29 -0300

    Requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-function createPixelBoard() {
+let paint = document.querySelector('#pixel-board');
+
+function putColors () {
+  let colorsPallet;
+  colorsPallet = document.querySelectorAll('.color');
+  //For baseado no do colega Glauco Lombeira
+  for (let color of colorsPallet) {
+    if (colorsPallet[0] === color) {
+      color.style.backgroundColor = 'black';
+      color.classList.add('selected');
+    } else if (colorsPallet[1] === color) {
+      color.style.backgroundColor = 'yellow';
+    } else if (colorsPallet[2] === color) {
+      color.style.backgroundColor = 'red';
+    } else if (colorsPallet[3] === color) {
+      color.style.backgroundColor = 'green';
+    } else if (colorsPallet[4] === color) {
+      color.style.backgroundColor = 'blue';
+    }
+  }
+}
+
+function createPixelBoard () {
   for (let index = 0; index < 5; index += 1){
     let tableRow = document.createElement('div');
     tableRow.className = 'pixel tr';
@@ -12,6 +34,47 @@ function createPixelBoard() {
 
 }
 
+function addListener () {
+  document.querySelector('#black').addEventListener('click', changeSelectedColorBlack);
+  document.querySelector('#yellow').addEventListener('click', changeSelectedColorYellow);
+  document.querySelector('#red').addEventListener('click', changeSelectedColorRed);
+  document.querySelector('#green').addEventListener('click', changeSelectedColorGreen);
+  document.querySelector('#blue').addEventListener('click', changeSelectedColorBlue);
+}
+
+paint.addEventListener('click', (event) => {
+  let alreadySelected = document.querySelector('.selected').style.backgroundColor;
+  event.target.style.backgroundColor = alreadySelected;
+});
+
+function clearSelectedColor () {
+  for(let index = 0; index < 5; index += 1){
+    document.querySelectorAll('.color')[index].className = 'color';
+  }
+}
+function changeSelectedColorBlack () {
+  clearSelectedColor();
+  document.querySelector('#black').className = 'color selected';
+}
+function changeSelectedColorYellow () {
+  clearSelectedColor();
+  document.querySelector('#yellow').className = 'color selected';
+}
+function changeSelectedColorRed () {
+  clearSelectedColor();
+  document.querySelector('#red').className = 'color selected';
+}
+function changeSelectedColorGreen () {
+  clearSelectedColor();
+  document.querySelector('#green').className = 'color selected';
+}
+function changeSelectedColorBlue () {
+  clearSelectedColor();
+  document.querySelector('#blue').className = 'color selected';
+}
+
 window.onload = function () {
   createPixelBoard();
+  addListener();
+  putColors();
 }
