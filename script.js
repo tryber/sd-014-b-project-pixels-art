617d9301b734f8344b1e082a42cc279ecf59f08e
Kelner Roberto <kelner250@hotmail.com>
2021-07-08 15:39:06 -0300

    requisitos #2 e #3

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-const secaoPaleta = document.getElementById('color-pallete');
+// Cria paleta de cores e coloca cor pra cada um
+
+const secaoPaleta = document.getElementById('color-palette');
+const arrColor = ['black', 'red', 'green', 'blue'];
 
 function criaPaleta (){
-    for (let index = 1; index < 5; index += 1){
+    for (let index = 0; index < 4; index += 1){
         let palleteBox = document.createElement('div');
         palleteBox.className = 'color';
+        palleteBox.style.backgroundColor = arrColor[index];
         palleteBox.style.border = '1px solid black';
         secaoPaleta.appendChild(palleteBox);
     }
 }
 criaPaleta();
 
-const takeEachPalleteBox = document.querySelectorAll('.color');
-function createColorToPallete() {
-    for (let index = 0; index < takeEachPalleteBox.length; index += 1) {
-        if (takeEachPalleteBox[index].style.backgroundColor === 'white') {
-            takeEachPalleteBox.style.backgroundColor = 
-        }
-    }
-}
-createColorToPallete();
-takeEachPalleteBox.forEach(createColorToPallete);
+
