295cda61d2e1bf70c17d31dbf106f9b9afcc4f83
Everson Elias Gonçalves de Oliveira <eversoneliasg@gmail.com>
2021-07-08 18:43:37 -0300

    Requisito #5

--- script.js
+++ script.js
@@ -1,36 +1,34 @@
-window.onload = function () {
-  const blackBox = document.getElementById('blackBox');
-  const cornBox = document.getElementById('cornFlowerBox');
-  const hotBox = document.getElementById('hotPinkBox');
-  const tiffanyBox = document.getElementById('tiffanyBlueBox');
-  const cleaningbutton = document.getElementById('clear-board');
-  const pixelsArray = document.querySelectorAll('.pixel');
+function changeToSelectedColor(event) {
+  const pickedColor = document.querySelector('.selected');
+  const style = window.getComputedStyle(pickedColor);
+  event.target.style.backgroundColor = style.getPropertyValue('background-color');
+}
 
-  blackBox.addEventListener('click', pickColor);
-  cornBox.addEventListener('click', pickColor);
-  hotBox.addEventListener('click', pickColor);
-  tiffanyBox.addEventListener('click', pickColor);
-  cleaningbutton.addEventListener('click', resetPixels);
+function pickColor(event) {
+  const previousSelected = document.querySelector('.selected');
+  previousSelected.classList.remove('selected');
+  event.target.classList.add('selected');
+}
 
+function resetPixels() {
   for (let index = 0; index < pixelsArray.length; index += 1) {
-    pixelsArray[index].addEventListener('click', changeToSelectedColor);
+    pixelsArray[index].style.removeProperty('background-color');
   }
+}
 
-  function changeToSelectedColor(event) {
-    const pickedColor = document.querySelector('.selected');
-    const style = window.getComputedStyle(pickedColor);
-    event.target.style.backgroundColor = style.getPropertyValue('background-color');
-  }
+const blackBox = document.getElementById('blackBox');
+const cornBox = document.getElementById('cornFlowerBox');
+const hotBox = document.getElementById('hotPinkBox');
+const tiffanyBox = document.getElementById('tiffanyBlueBox');
+const cleaningbutton = document.getElementById('clear-board');
+const pixelsArray = document.querySelectorAll('.pixel');
 
-  function pickColor(event) {
-    const previousSelected = document.querySelector('.selected');
-    previousSelected.classList.remove('selected');
-    event.target.classList.add('selected');
-  }
+blackBox.addEventListener('click', pickColor);
+cornBox.addEventListener('click', pickColor);
+hotBox.addEventListener('click', pickColor);
+tiffanyBox.addEventListener('click', pickColor);
+cleaningbutton.addEventListener('click', resetPixels);
 
-  function resetPixels() {
-    for (let index = 0; index < pixelsArray.length; index += 1) {
-      pixelsArray[index].style.removeProperty('background-color');
-    }
-  }
-};
+for (let index = 0; index < pixelsArray.length; index += 1) {
+  pixelsArray[index].addEventListener('click', changeToSelectedColor);
+}
