20eb2edd686f2b2ff51e7d0fd31dcc5ee644ca32
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-07-08 17:03:16 -0300

    Atende requisito 4

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-window.onload = function colorsPalet(){
-    let palet = document.getElementById('color-palette');  
+ window.onload = function colorsPalet(){
+    let palet = document.getElementById('color-palette'); 
+    let board = document.getElementById('pixel-board');
     for(let index = 1; index <= 4; index += 1){   
         let paletItem = document.createElement('div');
         paletItem.className = "color";
@@ -11,5 +12,26 @@ window.onload = function colorsPalet(){
     paletItem[2].style.backgroundColor = "blue";
     paletItem[3].style.backgroundColor = "green";
 
-}
+           for(index = 1; index <= 25; index += 1){
+           let line = document.createElement('div');
+           line.className = "pixel";
+           board.appendChild(line);
+           }
+        
 
+         
+ 
+
+          /* let line = document.getElementsByTagName('tr')
+           for(index = 0; index <= line.length; index += 1){
+               let square = document.createElement('td')
+               square.className = "pixel";
+               line[index].appendChild(square);
+
+           }
+     */
+
+
+ 
+
+}
