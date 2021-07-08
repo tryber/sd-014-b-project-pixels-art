da16c60a4411ea4ae680eda604114cd34d84ea4a
Fernando Serpa <serpaef@outlook.com>
2021-07-08 16:26:12 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 // seleciona a cor
 const colorOptions = document.querySelectorAll('.color');
+
+colorOptions[0].style.backgroundColor = 'black';
+colorOptions[1].style.backgroundColor = 'red';
+colorOptions[2].style.backgroundColor = 'green';
+colorOptions[3].style.backgroundColor = 'blue';
+
+const pixels = document.querySelectorAll('.pixel');
+
 function selectColor(event) {
     document.querySelector('.selected').classList.remove('selected');
     event.target.classList.add('selected');
 }
 
-for (let index = 0; index < colorOptions.length; index += 1) {
-    colorOptions[index].addEventListener('click', selectColor);
+for (let color of colorOptions) {
+    color.addEventListener('click', selectColor);
+}
+
+function colorizePixel(event) {
+    let newColor = document.querySelector('.selected').style.backgroundColor;
+    event.target.style.backgroundColor = newColor;
+}
+
+for(let pixel of pixels) {
+    pixel.addEventListener('click', colorizePixel);
 }
