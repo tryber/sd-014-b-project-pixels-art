1d4caa0e3b7c3c7737f54d093b85f8ada346e245
Ronaldo Junior <ronaldosenajr@gmail.com>
2021-07-08 14:42:32 -0400

    Realizado o requisito 7

--- script.js
+++ script.js
@@ -3,10 +3,7 @@ const colors = ['black','red','green', 'blue'];
 
 window.onload = function(){
     colorsPalette[0].classList.add('selected');
-    
-
 }
-
 
 setColorsInPalette();
 
@@ -14,6 +11,14 @@ function setColorsInPalette(){
     for (let i = 0; i < colorsPalette.length; i += 1){
         let color = colorsPalette[i];
         color.style.backgroundColor = colors[i];
+        color.addEventListener('click', setSelectedColor);
     }
+}
+
+function setSelectedColor(event){
+    let seletectedColor = document.querySelector('.selected');
+    seletectedColor.classList.remove('selected');
+    let newSelectedColor = event.target;
+    newSelectedColor.classList.add('selected');
 }
 
