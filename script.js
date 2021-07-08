e61c3c4cb56b36813c37bd12b453fcadb0799d7e
Ana-Alice-Honorio <ana.alice.honorio@gmail.com>
2021-07-08 20:05:58 -0300

    testando

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+window.onload =function (){
+  
+  let black = document.getElementById('black');
+  let blue = document.getElementById('blue');
+  let red = document.getElementById('red');
+  let green = document.getElementById('green');
 
+  function paintBrush(event) {
+    let preColor = document.querySelectorAll('.selected');
+preColor.classList.remove('selected');
+event.target.classList.add('selected');
+input.value = '';
+  }
+}
