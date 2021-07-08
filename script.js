91a40d2e61f25a633e1bdac71d7224ae50f6683e
Juliana Braga <juliana4braga@gmail.com>
2021-07-08 19:23:52 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function makeFramePixel() {
 }
 */
 
-let pixel = document. querySelectorAll("#pixel-board td")
+let pixel = document. querySelectorAll("#pixel-board td");
 for (let index = 0; index < pixel.length; index += 1) {
   pixel[index].classList.add("pixel");
 }
@@ -17,3 +17,21 @@ for (let index = 0; index < pixel.length; index += 1) {
 document.getElementById("pixel-board").style.marginLeft = "10px";
 
 document.getElementById("black").classList.add("selected");
+
+
+let blackColor = document.getElementById("black");
+let yellowColor = document.getElementById("yellow");
+let redColor = document.getElementById("red");
+let blueColor = document.getElementById("blue");
+
+blackColor.addEventListener("click", pickColor);
+yellowColor.addEventListener("click", pickColor);
+redColor.addEventListener("click", pickColor);
+blueColor.addEventListener("click", pickColor);
+
+function pickColor (event) {
+  let colorWithSelected = document.querySelector(".selected");
+  colorWithSelected.classList.remove("selected");
+  event.target.classList.add("selected");
+  console.log(event.target);
+}
