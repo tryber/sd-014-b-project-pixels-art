0b1a63938d79de2668ee25f346e42674fda493f7
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-07-08 15:56:45 -0300

    Requisito #4

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let colors = ['background: rgb(0, 0, 0)', 'background: rgb(252, 163, 17)', 'background: rgb(229, 229, 229)', 'background: rgb(125, 231, 85)'];
+// Adiciona as cores da paleta de cores;
+const colors = ['background: rgb(0, 0, 0)', 'background: rgb(252, 163, 17)', 'background: rgb(229, 229, 229)', 'background: rgb(125, 231, 85)'];
 
 const trChindren = document.querySelector('tr').children;
+
 for (let index = 0; index < trChindren.length; index += 1) {
-  let td = trChindren[index];
-  let color = colors[index];
-  td.setAttribute('style', color);
+  trChindren[index].setAttribute('style', colors[index]);
 }
