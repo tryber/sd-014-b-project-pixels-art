bc65894d8f41ac239bbb09884f8965d9962ae1b7
Leomar Linhares <leomar_linhares@hotmail.com>
2021-07-08 18:28:47 -0300

    Recria a função createColorPalette para atender a exigência do lint :rocket:

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
+function colorSelector(event) {
+  document.querySelector('.selected').classList.remove('selected');
+  event.target.classList.add('selected');
+}
+
+function paint(event) {
+  const selectedColor = document.querySelector('.selected').style.backgroundColor;
+  const element = event.target;
+  element.style.backgroundColor = selectedColor;
+}
+
+function randomHexColorGenerator() {
+  const hexCharacters = 'abcdef1234567890'
+  let hexColorCode = '#';
+  for (let index = 0; index < 6; index++) {
+    hexColorCode += hexCharacters.charAt(Math.floor(Math.random() * 16));
+  }
+  return hexColorCode;
+};
+
 function createColorPalette() {
   const paletteColor = document.getElementById('color-palette');
 
   for (let index = 0; index < 4; index += 1) {
     const color = document.createElement('div');
     color.className = 'color';
-    color.addEventListener('click', colorSelector)
+    color.addEventListener('click', colorSelector);
 
-    switch (index) {
-      case 0:
-        color.style.backgroundColor = 'black';
-        color.classList.add('selected');
-        break;
-
-      case 1:
-        color.style.backgroundColor = 'red';
-        break;
-
-      case 2: 
-        color.style.backgroundColor = 'green';
-        break;
-
-      case 3:
-        color.style.backgroundColor = 'blue';
-        break;
-      
-      default:
-        break;
+    if (index === 0) {
+      color.style.backgroundColor = 'black';
+      color.classList.add('selected');
+    } else {
+      color.style.backgroundColor = randomHexColorGenerator();
     }
 
     paletteColor.appendChild(color);
@@ -42,21 +47,20 @@ function createPixelBoard() {
     for (let subIndex = 0; subIndex < 5; subIndex += 1) {
       const pixel = document.createElement('div');
       pixel.className = 'pixel';
-      pixel.addEventListener('click', paint)
+      pixel.addEventListener('click', paint);
       line.appendChild(pixel);
     }
     pixelBoard.appendChild(line);
   }
 }
 
-function colorSelector(event) {
-  document.querySelector('.selected').classList.remove('selected');
-  event.target.classList.add('selected');
-}
+function newBoard() {
+  const pixels = document.getElementsByClassName('pixel');
 
-function paint(event) {
-  const selectedColor = document.querySelector('.selected').style.backgroundColor;
-  event.target.style.backgroundColor = selectedColor;
+  for (let index = 0; index < pixels.length; index += 1) {
+    const element = pixels[index];
+    element.style.backgroundColor = 'white';
+  }
 }
 
 createColorPalette();
