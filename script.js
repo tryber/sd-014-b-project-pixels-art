fb765259de9c9068e508ad3383819ebaf15819ae
vinicius-mns <vinicius_mns@hotmail.com>
2021-07-08 17:27:52 -0300

    requisito #7

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
- 
+let select = document.querySelector('#color-palette').children
+
+let corUm = select[0].addEventListener('click', preto)
+
+let corDois = select[1].addEventListener('click', vermelho)
+
+let corTres = select[2].addEventListener('click', amarelo)
+
+let corQuatro = select[3].addEventListener('click', verde)
+
+function preto() {
+    select[0].classList.add ('selected')
+    select[1].classList.remove ('selected')
+    select[2].classList.remove ('selected')
+    select[3].classList.remove ('selected')
+}
+function vermelho() {
+    select[0].classList.remove ('selected')
+    select[1].classList.add ('selected')
+    select[2].classList.remove ('selected')
+    select[3].classList.remove ('selected')
+}
+function amarelo() {
+    select[0].classList.remove ('selected')
+    select[1].classList.remove ('selected')
+    select[2].classList.add ('selected')
+    select[3].classList.remove ('selected')
+}
+function verde() {
+    select[0].classList.remove ('selected')
+    select[1].classList.remove ('selected')
+    select[2].classList.remove ('selected')
+    select[3].classList.add ('selected')
+}
+
+
+
