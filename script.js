6351f9f7daa659be8eab15de0bf6eba4d1569e5c
Kauan Carvalho <kauancarvalho987@gmail.com>
2021-07-08 16:18:31 -0300

    enviando o requisito 06

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-let base = 5;
-let pixBoard = document.getElementById('pixel-board');
+window.onload = function() {
+  const blackColor = document.getElementById('colorOne') 
+  blackColor.classList.add('selected')
+}
 
+const base = 5;
+const pixBoard = document.getElementById('pixel-board');
 
 function createPixBoard() {
-    for (let index = 1; index <= base; index += 1) {
-        let line = document.createElement('div');
-        pixBoard.appendChild(line);
-        for (let secondIndex = 0; secondIndex < base; secondIndex += 1) {
-            let pixel = document.createElement('div');
-            pixel.className = 'pixel';
-            line.appendChild(pixel);
+  for (let index = 1; index <= base; index += 1) {
+    const line = document.createElement('div');
+    pixBoard.appendChild(line);
+  for (let secondIndex = 0; secondIndex < base; secondIndex += 1) {
+    const pixel = document.createElement('div');
+    pixel.className = 'pixel';
+    line.appendChild(pixel);
         }
     }
 }
 createPixBoard()
 
-
-
-
+function selectedClass() {
+    
+}
