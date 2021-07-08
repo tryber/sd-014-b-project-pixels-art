287ac841026dd96e08c3fc8a312c4670075b6522
Grasiela Gomes <produtoradama@gmail.com>
2021-07-08 19:02:40 -0300

    set color and paint pixel

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+window.onload = () => {
+  const selectBlack = document.querySelector('.black');
+  selectBlack.classList.add('selected');
+};
+
 function createPixel(value) {
   const pixelBoard = document.querySelector('#pixel-board');
   for (let i = 0; i < value; i += 1) {
@@ -8,17 +13,31 @@ function createPixel(value) {
 }
 createPixel(5 ** 2);
 
-const selectBlack = document.querySelector('.black');
-selectBlack.classList.add('selected');
-
 function setPixelColor(color) {
   document.addEventListener('click', (event) => {
     if (event.target.classList.contains('pixel')) {
-      event.target.style.backgroundColor = color;
+      event.target.style.backgroundColor = `${color}`;
     }
   }, false);
 }
-setPixelColor('yellow');
+
+function getPalleteColor() {
+  const pallete = document.querySelector('#color-palette');
+  const colors = document.querySelectorAll('.color');
+  pallete.addEventListener('click', (event) => {
+    colors.forEach((color) => {
+      color.classList.remove('selected');
+    });
+    if (event.target.classList.contains('color')) {
+      event.target.classList.add('selected');
+      setPixelColor(event.target.classList[1]);
+    } else {
+      colors[0].classList.add('selected');
+      setPixelColor('black');
+    }
+  });
+}
+getPalleteColor();
 
 function clearColors() {
   const pixels = document.querySelectorAll('.pixel');
