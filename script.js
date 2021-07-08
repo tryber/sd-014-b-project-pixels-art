8950feabff63ef60f6dc28f46272f83a4cd2d87a
Everson Elias Gonçalves de Oliveira <eversoneliasg@gmail.com>
2021-07-08 18:38:09 -0300

    Requisitos  e #9

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
 window.onload = function () {
-  
   const blackBox = document.getElementById('blackBox');
-  
   const cornBox = document.getElementById('cornFlowerBox');
-  
   const hotBox = document.getElementById('hotPinkBox');
-  
   const tiffanyBox = document.getElementById('tiffanyBlueBox');
+  const cleaningbutton = document.getElementById('clear-board');
+  const pixelsArray = document.querySelectorAll('.pixel');
 
   blackBox.addEventListener('click', pickColor);
   cornBox.addEventListener('click', pickColor);
   hotBox.addEventListener('click', pickColor);
   tiffanyBox.addEventListener('click', pickColor);
-
-  function pickColor(event) {
-    let previousSelected = document.querySelector('.selected');
-    previousSelected.classList.remove('selected');
-    event.target.classList.add('selected');
-  }
-
-  const pixelsArray = document.querySelectorAll('.pixel');
+  cleaningbutton.addEventListener('click', resetPixels);
 
   for (let index = 0; index < pixelsArray.length; index += 1) {
     pixelsArray[index].addEventListener('click', changeToSelectedColor);
   }
 
   function changeToSelectedColor(event) {
-    let pickedColor = document.querySelector('.selected');
-    let style = window.getComputedStyle(pickedColor);
+    const pickedColor = document.querySelector('.selected');
+    const style = window.getComputedStyle(pickedColor);
     event.target.style.backgroundColor = style.getPropertyValue('background-color');
   }
 
-}
+  function pickColor(event) {
+    const previousSelected = document.querySelector('.selected');
+    previousSelected.classList.remove('selected');
+    event.target.classList.add('selected');
+  }
+
+  function resetPixels() {
+    for (let index = 0; index < pixelsArray.length; index += 1) {
+      pixelsArray[index].style.removeProperty('background-color');
+    }
+  }
+};
