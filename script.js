e07bd8c1301c085c0797327fab756846231cd1eb
Bruno Trindade <brunotrndd@gmail.com>
2021-07-08 20:43:59 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-document.getElementById('black').classList.add('selected');
+window.onload = function () {
+  document.getElementById('black').classList.add('selected');
+};
+
+const palletColors = document.querySelectorAll('.color');
+
+function selectColor(event) {
+  for (let index = 0; index < palletColors.length; index += 1) {
+    palletColors[index].classList.remove('selected');
+    event.target.classList.add('selected');
+  }
+}
+
+for (let index = 0; index < palletColors.length; index += 1) {
+  palletColors[index].addEventListener('click', selectColor, false);
+}
