c3cb23c364682d8dad834acd6a67b5229071960d
Vanim <vanimouzad123@gmail.com>
2021-07-08 15:05:56 -0300

    Requisito 4 e 5

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 window.onload = function () { 
     let colorsPallete = document.querySelectorAll('.color');
 
-    for (element of colorsPallete) {
-        if (colorsPallete[0] == element) {
+    for (let element of colorsPallete) {
+        if (colorsPallete[0] === element) {
             element.style.backgroundColor = 'black';
-        } else if (colorsPallete[1] == element) {
+        } else if (colorsPallete[1] === element) {
             element.style.backgroundColor = 'red';
-        } else if (colorsPallete[2] == element) {
+        } else if (colorsPallete[2] === element) {
             element.style.backgroundColor = 'blue';
-        } else if (colorsPallete[3] == element) {
+        } else if (colorsPallete[3] === element) {
             element.style.backgroundColor = 'green';
         }
+    }
+
+    let pixelBoard = document.querySelector('#pixel-board');
+    
+    for (let i = 1; i <= 25; i += 1) {
+        let pixel = document.createElement('div');
+        pixel.classList.add('pixel')
+        pixelBoard.appendChild(pixel);
+
     }
 }
 
