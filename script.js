e984bc957dbc9c2f3673c0c7975382e716b60476
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-07-08 20:00:53 -0300

    colocando cores em cada quadradinho por evento de clique

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const colorOne = document.querySelector('#black');
 const colorTwo = document.querySelector('#pink');
 const colorTree = document.querySelector('#green');
 const colorFour = document.querySelector('#purple');
+const whiteBox = document.querySelector('#pixel-board');
 
 function selectColor(event) {
   const corSelecionada = document.querySelector('.selected');
@@ -12,3 +13,10 @@ colorOne.addEventListener('click', selectColor);
 colorTwo.addEventListener('click', selectColor);
 colorTree.addEventListener('click', selectColor);
 colorFour.addEventListener('click', selectColor);
+
+function colorir(event) {
+  const corSelecionada = document.querySelector('.selected').id;
+  event.target.style.backgroundColor = corSelecionada;  
+}
+whiteBox.addEventListener('click', colorir);
+
