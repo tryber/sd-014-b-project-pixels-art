7af2f22248b20fa5601cdbd10866ec3197616338
Matheus <mateeus.soarees@outlook.com>
2021-07-08 18:04:33 -0300

    Inserção de algumas referencias usadas

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ function criaGrid() {
   const grid = document.getElementById('pixel-board');
 
   for (let i = 0; i < alturaGrid; i++) {
+    //https://developer.mozilla.org/en-US/docs/Web/API/HTMLTableElement/insertRow
     const coluna = grid.insertRow(0);
     for (let x = 0; x < larguraGrid; x++) {
+      //https://developer.mozilla.org/en-US/docs/Web/API/HTMLTableRowElement/insertCell
       let celula = coluna.insertCell(0);
       celula.classList.add('pixel');
       celula.style.backgroundColor = 'white';
