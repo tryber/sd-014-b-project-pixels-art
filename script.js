95df57b8ac62852928afd581e8e185894987fe58
samsantosb <samsantosb@outlook.com>
2021-07-08 20:41:48 -0300

    mais algumas mudanças

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
+/*outras refs *
+Tabelas Display Flex: https://www.maujor.com/tutorial/anti-heroi-css-display-table.php
+Flexbox: https://www.w3schools.com/css/css3_flexbox.asp
+Javascript Arrow Functions: https://www.w3schools.com/js/js_arrow_function.asp/
+*/
 //refs: Ajudas do meu colega, Glauco.
 
 //essa função seleciona cores, remove e muda as cores da classe selected
+
 const select = () => {
-  const reset = () => {
+  const change = document.querySelector('#color-palette');
+
+  change.addEventListener('click', (e) => {
     const selected = document.querySelector('.selected');
     selected.classList.remove('selected');
-  };
-  const change = document.querySelector('#color-palette');
-  change.addEventListener('click', (e) => {
-    reset();
     e.target.classList.add('selected');
   });
 };
@@ -23,13 +27,13 @@ const paint = () => {
   });
 };
 
-//essa função limpa tudo
+//essa função limpa tudo https://www.codegrepper.com/code-examples/javascript/javascript+change+color+for+class+name
 const clear = () => {
-  const del = document.querySelector('#clear-board');
-  del.addEventListener('click', () => {
+  const totalDeleteLesgoTrybers = document.querySelector('#clear-board');
+  totalDeleteLesgoTrybers.addEventListener('click', () => {
     const pxl = document.querySelectorAll('.pixel');
-    for (let i = 0; i < pxl.length; i++) {
-      pxl[i].style.backgroundColor = 'white';
+    for (let index = 0; index < pxl.length; index += 1) {
+      pxl[index].style.backgroundColor = 'white';
     }
   });
 };
