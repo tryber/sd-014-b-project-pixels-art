aeeaa30ee3c0bc5dd50caae23bdb3cd518aed2fa
andremorsch <andremorsch37@gmail.com>
2021-07-08 19:34:58 -0300

    exercicio #7

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function trocaSelected(evento) {
-    let tirarSelected = document.querySelector(".selected")
-    tirarSelected.classList.remove("selected")
-    evento.target.classList.add("selected")
+    let tirarSelected = document.querySelector('.selected')
+    tirarSelected.classList.remove('selected')
+    evento.target.classList.add('selected')
 }
+
+function corSelecionada () {
+    let paleta = document.getElementsByClassName('color')
+    let colorselec = []
+
+    for (let i = 0; i < paleta.length; i +=1){
+        colorselec[i] = paleta[i]
+        colorselec[i].addEventListener('click', trocaSelected)
+    }
+    console.log(paleta)
+}
+corSelecionada()
