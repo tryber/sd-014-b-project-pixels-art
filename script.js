ad2e48c032882184a71cb510d7239d9bec49e014
dhenycruz <dheniarley.ds@gmail.com>
2021-07-08 17:58:41 -0300

    Requisito #3.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Selecting each color palette.
 const colors = document.querySelectorAll('.color');
 
-colors[0].setAttribute('style', 'background-color: red');
+colors[0].setAttribute('style', 'background-color: black');
 colors[1].setAttribute('style', 'background-color: green');
 colors[2].setAttribute('style', 'background-color: yellow');
 colors[3].setAttribute('style', 'background-color: pink');
