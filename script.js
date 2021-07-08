768e9cab20a95a5d2512a4a1b1809062f7143bae
Ana-Alice-Honorio <ana.alice.honorio@gmail.com>
2021-07-08 18:41:33 -0300

    correções

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-let pegaPreto = document.getElementsByClassName('color');
-pegaPreto[0].classList.add('selected');
-function criaGrid() {
-  const alturaGrid = 5;
-  const larguraGrid = 5;
-  const grid = document.getElementById('pixel-board');
+
