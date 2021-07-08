cea52397f0df69311f68c9c893d16d0448313ea2
Matheus <mateeus.soarees@outlook.com>
2021-07-08 16:42:59 -0300

    Requisito 4 e 5

---
+++ script.js
@@ -0,0 +1,15 @@
+function criaGrid() {
+  const alturaGrid = 5;
+  const larguraGrid = 5;
+  const grid = document.getElementById('pixel-board');
+
+  for (let i = 0; i < alturaGrid; i++) {
+    const coluna = grid.insertRow(0);
+    for (let x = 0; x < larguraGrid; x++) {
+      let celula = coluna.insertCell(0);
+      celula.classList.add('pixel');
+      celula.style.backgroundColor = 'white';
+    }
+  }
+}
+criaGrid();
