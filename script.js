a79a05832452d838de39ae3126a323abc08bf610
Danilo dos Santos <danilo.santossouza@outlook.com>
2021-07-08 16:13:55 -0300

    Requisito 3 e 4 resolvidos

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 window.onload = function (){
 
-  let body = document.getElementsByTagName('body');
+  let colorPalette = document.getElementById('color-palette');
 
+  let colorBlack = document.createElement('div');
+  colorBlack.className = "color"
+  colorBlack.style.backgroundColor = "black";
+  colorPalette.appendChild(colorBlack);
+
+  let colorGreen = document.createElement('div');
+  colorGreen.className = "color"
+  colorGreen.style.backgroundColor = "#42883E";
+  colorPalette.appendChild(colorGreen);
   
+  let colorBlue = document.createElement('div');
+  colorBlue.className = "color"
+  colorBlue.style.backgroundColor = "#282E62";
+  colorPalette.appendChild(colorBlue);
+
+  let colorYellow = document.createElement('div');
+  colorYellow.className = "color"
+  colorYellow.style.backgroundColor = "#E6BF5E";
+  colorPalette.appendChild(colorYellow);
 }
