32433418af8504b518522900d50145130388f46e
Mathluiz23 <matheus_luizs@hotmail.com>
2021-07-08 17:19:56 -0300

    Requisito #3

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let color1 = document.getElementsByClassName('color')[0];
-color1.style.backgroundColor = "blue";
+color1.style.backgroundColor = "black";
 
 let color2 = document.getElementsByClassName('color')[1];
-color2.style.backgroundColor = "green";
+color2.style.backgroundColor = "blue";
 
 let color3 = document.getElementsByClassName('color')[2];
-color3.style.backgroundColor = "yellow";
+color3.style.backgroundColor = "green";
 
 let color4 = document.getElementsByClassName('color')[3];
-color4.style.backgroundColor = "gray";
+color4.style.backgroundColor = "yellow";
+
+let color5 = document.getElementsByClassName('color')[4];
+color5.style.backgroundColor = "gray";
+
