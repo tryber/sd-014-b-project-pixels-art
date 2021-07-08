96da69b3c00eaacb433d8a67a0127f84b493d6f9
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-07-08 16:44:09 -0300

    Requisito 3

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-window.onload = function() {
+window.onload = function() {  
 
   let numberOfColors = 4;
   let colorPalette = document.querySelector('#color-palette');
-  
 
   for (let index = 0; index < numberOfColors; index += 1) {
     let newColor = document.createElement('li');
     newColor.className = 'color';
     colorPalette.appendChild(newColor);
-    newColor.style.backgroundColor = generateRandomColor();
+    if (index === 0) {
+      newColor.style.backgroundColor = 'black';
+    } else {
+      newColor.style.backgroundColor = generateRandomColor();
+    }
   }
 
   function generateRandomColor() {
     let r = Math.random() * 255;
     let g = Math.random() * 255;
     let b = Math.random() * 255;
-   
-   return `rgb(${r}, ${g}, ${b})`;
-  }
-  
 
+    return `rgb(${r}, ${g}, ${b})`;
+  }
 }
