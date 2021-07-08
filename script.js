144c8181927b8158535ca7d91eec1eb6ca25f03d
Elithon Silva <elithonsilva@gmail.com>
2021-07-08 20:40:50 -0300

    Correçao de comentario, foi resolvido o requisito-07, o 06 ainda esta em aberto

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const colorBlack = document.querySelector('.black');
 const colorYellow = document.querySelector('.yellow');
 const colorMagenta = document.querySelector('.magenta');
 const colorCyan = document.querySelector('.cyan');
-const section = document.querySelector('#pixel-board');
+const sectionMatriz = document.querySelector('#pixel-board');
 
 function oneClick(event) {
-  let selectedClass = document.querySelector('.selected');
+  const selectedClass = document.querySelector('.selected');
   selectedClass.classList.remove('selected');
   event.target.classList.add('selected');
 }
