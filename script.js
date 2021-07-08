efc33ade857c6554c88798a84410714e1bb5a0fb
Kauan Carvalho <kauancarvalho987@gmail.com>
2021-07-08 17:47:38 -0300

    requisito 07

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
-window.onload = function() {
-  const blackColor = document.getElementById('colorOne') 
-  blackColor.classList.add('selected')
+window.onload = function () {
+    const blackColor = document.getElementById('black')
+    blackColor.classList.add('selected')
 }
 
 const base = 5;
 const pixBoard = document.getElementById('pixel-board');
+const divWithSelected = document.querySelector('.selected');
+let divsWithColor = document.querySelectorAll('.color');
+
 
 function createPixBoard() {
-  for (let index = 1; index <= base; index += 1) {
-    const line = document.createElement('div');
-    pixBoard.appendChild(line);
-  for (let secondIndex = 0; secondIndex < base; secondIndex += 1) {
-    const pixel = document.createElement('div');
-    pixel.className = 'pixel';
-    line.appendChild(pixel);
+    for (let index = 1; index <= base; index += 1) {
+        const line = document.createElement('div');
+        pixBoard.appendChild(line);
+        for (let secondIndex = 0; secondIndex < base; secondIndex += 1) {
+            const pixel = document.createElement('div');
+            pixel.className = 'pixel';
+            line.appendChild(pixel);
         }
     }
 }
-createPixBoard()
+createPixBoard();
 
-function selectedClass() {
-    
+function assignSelected(color) {
+ let selected = document.querySelector('.selected')
+ selected.classList.remove('selected')
+ color.target.classList.add('selected')
 }
+
+function classSelected() {
+    for (let index = 0; index < divsWithColor.length; index += 1) {
+        divsWithColor[index].addEventListener('click', assignSelected);
+    }
+}
+classSelected()
+
