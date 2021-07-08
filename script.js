06926437411bcb6c8c46cd1ddf02171097040677
Leomar Linhares <leomar_linhares@hotmail.com>
2021-07-08 19:13:53 -0300

    Cumpre o requisito 9 :rocket:

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ function paint(event) {
 }
 
 function randomHexColorGenerator() {
-  const hexCharacters = 'abcdef1234567890'
+  const hexCharacters = 'abcdef1234567890';
   let hexColorCode = '#';
-  for (let index = 0; index < 6; index++) {
+  for (let index = 0; index < 6; index += 1) {
     hexColorCode += hexCharacters.charAt(Math.floor(Math.random() * 16));
   }
   return hexColorCode;
-};
+}
 
 function createColorPalette() {
   const paletteColor = document.getElementById('color-palette');
@@ -65,3 +65,25 @@ function newBoard() {
 
 createColorPalette();
 createPixelBoard();
+
+document.getElementById('clear-board').addEventListener('click', newBoard);
+
+/*
+  Descrição da function randomHexColorGenerator():
+  A função retorna uma string contendo uma cor aleatória em hexadecimal.
+
+  ### --- Linha por linha --- ###
+  Linha 12: Eu crio a função que não recebe parâmetro
+  Linha 13: Declaro uma variável constante com uma string contendo os caracteres que aparecem em um valor de cor hexadecimal;
+  Linha 14: Declaro uma variável que eu possa modificar contento apenas a string com a cerquilha. Essa variável vai receber mais caracteres;
+  Linha 15: Declaro um for loop com limite de 6 ações para atender ao formato de uma cor em hexadecimal;
+  Linha 16: Nessa linha eu digo para pegar um caractere aleatório de hexCharacters. O Math.floor é para transformar em inteiro levando em
+            consideração o piso. Math.random gera um valor aleatório e, quando multiplicado por 16, ele incluí 0 e excluí 16. Como eu tenho
+            Math.floor sendo executado, esse número vai de 0 à 15. Esse é intervalo de índices da string hexCharacters. O charAt vai usar esse
+            valor para selecionar o caractere da string hexCharacters;
+  Linha 17: Fecho a execução do for;
+  Linha 18: Retorno hexColorCode, pois vou precisar desse retorno para definir as cores na função createColorPalette.
+
+  Referência acerca do charAt e geração de caracteres aleatórios a partir de uma string:
+  https://www.webtutorial.com.br/funcao-para-gerar-uma-string-aleatoria-random-com-caracteres-especificos-em-javascript/
+*/
