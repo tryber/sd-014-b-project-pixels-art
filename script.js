9cca0573c45755fe88022930698057da7c860f82
Israel S. Nazareth <israel.fab@hotmail.com>
2021-07-08 16:52:35 -0300

    Requisito 07

---
+++ script.js
@@ -0,0 +1,15 @@
+const black = document.querySelector('.black');
+const green = document.querySelector('.green');
+const blue = document.querySelector('.blue');
+const yellow = document.querySelector('.yellow');
+
+function selectedColor(event) {
+  let elemento = document.querySelector('.selected');
+  elemento.classList.remove('selected')
+  event.target.classList.add('selected');
+}
+
+black.addEventListener('click', selectedColor);
+green.addEventListener('click', selectedColor);
+blue.addEventListener('click', selectedColor);
+yellow.addEventListener('click', selectedColor);
