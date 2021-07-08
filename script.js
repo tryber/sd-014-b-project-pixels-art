61cdcef5cfe9b0ab87d864a77c25e2ff91c95357
natalia <mellomartins.naty@gmail.com>
2021-07-08 20:02:30 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
-window.onload = function() {
+    window.onload = function () {
     
-    let boardWidth = 5
-    lines = 5
-
-    /*Funções para construir o quadro de pixels inspirada nos arquivos da dinâmica do bloco 5.4*/
-    function fillBoard(lines) {
-        for(let index = 0; index < lines.length; index += 1) {
-          fillLine(lines[index]);
-          controlRight += 1;
-          controlLeft -= 1;
-        }
-      }
-
-    function createBox(className) {
-        let box = document.createElement("div");
-        box.className = className;
-        return box;
-      }
-
-    function constructLine(divLine) {
-      for (let index = 1; index <= boardWidth; index += 1) {
-        pixel.appendChild(createBox("box"));
-        }
-      }
+    const black = document.getElementById('preto');
+    const purple = document.getElementById('roxo');
+    const blue = document.getElementById('azul');
+    const pink = document.getElementById('rosa');
+    
+    function paintBrush(event) {
+        const previousColor = document.querySelector('.selected');
+        previousColor.classList.remove('selected');
+        event.target.classList.add('selected');        
+    } 
+    
+    black.addEventListener('click', paintBrush);
+    purple.addEventListener('click', paintBrush);
+    blue.addEventListener('click', paintBrush);
+    pink.addEventListener('click', paintBrush);   
 
 }
