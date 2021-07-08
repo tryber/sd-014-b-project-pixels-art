41f3578cf10669c0c9fa396dc1617248b0d0a2e7
Everson Elias Gonçalves de Oliveira <eversoneliasg@gmail.com>
2021-07-08 17:45:08 -0300

    Testando classe .selected em branco

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-/*window.onload = function() {
-  blackBox = document.getElementById('blackBox');
-  blackBoxColor = window.getComputedStyle(blackBox, null).getPropertyValue("background-value");
+window.onload = function () {
+  
+  let blackBox = document.getElementById('blackBox');
+  
+  let cornBox = document.getElementById('cornFlowerBox');
+  
+  let hotBox = document.getElementById('hotPinkBox');
+  
+  let tiffanyBox = document.getElementById('tiffanyBlueBox');
 
-}*/
+  blackBox.addEventListener('click', pickColor);
+// cornBox.addEventListener('click', pickColor);
+// hotBox.addEventListener('click', pickColor);
+// tiffanyBox.addEventListener('click', pickColor);
+
+  function pickColor(event) {
+    let boxColor = event.target.style.backgroundColor;
+  }
+
+  let pixelsArray = document.querySelectorAll('.pixel');
+
+  for (let index = 0; index < pixelsArray.length; index += 1) {
+    pixelsArray[index].addEventListener('click', changeToSelectedColor);
+  }
+
+  function changeToSelectedColor(event) {
+    let pickedColor = document.querySelector('.selected');
+    style = window.getComputedStyle(pickedColor);
+    event.target.style.backgroundColor = style.getPropertyValue('background-color');
+  }
+
+}
