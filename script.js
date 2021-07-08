7b15ddff209cf8fa5c5431d9f1fb951a3ded1157
Elithon Silva <elithonsilva@gmail.com>
2021-07-08 20:43:22 -0300

    correcao do erro - sectionMatriz is assigned a value but never used.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const colorBlack = document.querySelector('.black');
 const colorYellow = document.querySelector('.yellow');
 const colorMagenta = document.querySelector('.magenta');
 const colorCyan = document.querySelector('.cyan');
-const sectionMatriz = document.querySelector('#pixel-board');
+const matriz = document.querySelector('#pixel-board');
 
 function oneClick(event) {
   const selectedClass = document.querySelector('.selected');
