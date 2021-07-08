ddc5fb74ee6b639ad4ef2fb39c9da535a1af882a
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-07-08 18:11:44 -0300

    Altera algumas coisas pedidas pelo lint

--- script.js
+++ script.js
@@ -19,12 +19,11 @@ function selectedColor() {
 selectedColor();
 
 document.addEventListener('click', function (event) {
-  console.log(event);
   if (event.target.classList.contains('pixel')) {
     event.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
   }
 }, false);
-//Acima foi ultilizado o conceito de event bubbling contido no link https://gomakethings.com/attaching-multiple-elements-to-a-single-event-listener-in-vanilla-js/
+// Acima foi ultilizado o conceito de event bubbling contido no link https://gomakethings.com/attaching-multiple-elements-to-a-single-event-listener-in-vanilla-js/
 
 const resetButton = document.getElementById('clear-board');
 resetButton.addEventListener('click', function () {
