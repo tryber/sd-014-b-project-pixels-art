4de3fba98138bc1ab663955ef743e0969cae7100
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-07-08 17:29:53 -0300

    Requisito #8

--- script.js
+++ script.js
@@ -11,19 +11,27 @@ for (let index = 0; index < trChindren.length; index += 1) {
   trChindren[index].setAttribute('style', colors[index]);
 }
 
-window.onload = function () {
-  const color = document.querySelector('.color');
-  color.classList.add('selected');
+const color = document.querySelector('.color');
+color.classList.add('selected');
 
-  function selectedChange(event) {
-    const colorsSelected = document.querySelector('.selected');
-    colorsSelected.classList.remove('selected');
-    event.target.classList.add('selected');
-  }
+const pixels = document.querySelectorAll('.pixel');
 
-  const divColors = document.querySelectorAll('.color');
+for (let index = 0; index < pixels.length; index++) {
+  pixels[index].addEventListener('click', function (event) {
+    let colorSelect = document.querySelector('.selected').style.background;
+    event.target.setAttribute('style', 'background:' + colorSelect);
+  });
+}
 
-  for (let index = 0; index < divColors.length; index += 1) {
-    divColors[index].addEventListener('click', selectedChange);
-  }
-};
+function selectedChange(event) {
+  const colorsSelected = document.querySelector('.selected');
+  colorsSelected.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+
+const divColors = document.querySelectorAll('.color');
+
+for (let index = 0; index < divColors.length; index += 1) {
+  const div = divColors[index];
+  div.addEventListener('click', selectedChange);
+}
