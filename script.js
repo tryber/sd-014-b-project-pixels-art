1d8c7c56515b01149f2c907263e219d2a9a17811
Renata <renatateixeira.est@gmail.com>
2021-07-08 18:50:51 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ document.querySelector('#title').innerHTML = "Paleta de Cores";
 //Requisito 3:
 //console.log(document.querySelector('.color'))
 document.querySelector('.color').style.backgroundColor = 'black';
+
+//Requisito 4:
+document.querySelectorAll('.pixel').style.backgroundColor = 'blue';
+//console.log(document.querySelectorAll('.pixel'));
