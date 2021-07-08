72e5bdd200f78d0fb47ec6a4bfc5d891fc6ae833
Henrique Almeida <joaoh.almeidaaraujo@gmail.com>
2021-07-08 16:01:36 -0300

    Requisitos 4 e 5

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ createColor('rgb(250, 0, 0)');
 createColor('rgb(10, 50, 149)');
 
 createColor('rgb(0, 250, 075)');
+
+function createPixelBoard() {
+  const setBoardWidth = 5;
+  const setBoardHeight = 5;
+  for (let i = 0; i < setBoardHeight; i += 1) {
+    for (let i2 = 0; i2 < setBoardWidth; i2 += 1) {
+      const createPixel = document.createElement('div');
+      const getBoard = document.querySelector('#pixel-board');
+      const pixel = getBoard.appendChild(createPixel);
+      pixel.style.backgroundColor = 'white';
+      pixel.style.border = '1px solid black';
+      pixel.setAttribute('class', 'pixel');
+    }
+  }
+}
+
+createPixelBoard();
