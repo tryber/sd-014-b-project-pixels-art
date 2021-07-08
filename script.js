e01e1b735b02162c0f6258b45b6d9a605302f195
Mathluiz23 <matheus_luizs@hotmail.com>
2021-07-08 18:08:41 -0300

    Requisito #2 refeito

--- script.js
+++ script.js
@@ -10,6 +10,3 @@ color3.style.backgroundColor = "green";
 let color4 = document.getElementsByClassName('color')[3];
 color4.style.backgroundColor = "yellow";
 
-let color5 = document.getElementsByClassName('color')[4];
-color5.style.backgroundColor = "violet";
-
