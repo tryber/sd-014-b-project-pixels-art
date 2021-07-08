ca1be5625309029f4da3bab2ed836fed8c9e7a84
Thyara <thyaranonato@outlook.com>
2021-07-08 16:01:15 -0300

    Adiciona requisito #5

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Função para criar o pixel-board
-let pixelBoard = document.querySelector('#pixel-board');
+const pixelBoard = document.querySelector('#pixel-board');
 
 function pixelBoardCreat(lines, columns) {
   for (let column = 1; column <= columns; column += 1) {
-    let pixelColumns = document.createElement('div');
+    const pixelColumns = document.createElement('div');
     pixelColumns.classList.add('pixel');
     pixelBoard.appendChild(pixelColumns);
   }
@@ -11,7 +11,7 @@ function pixelBoardCreat(lines, columns) {
 
 function pixelLinesCreat(lines) {
   for (let line = 1; line <= lines; line += 1) {
-    let pixelLine = document.createElement('div');
+    const pixelLine = document.createElement('div');
     pixelBoard.appendChild(pixelLine);
     pixelBoardCreat(pixelLine, lines);
   }
