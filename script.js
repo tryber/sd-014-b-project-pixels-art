da9521c5ccef43a2d87fcd1cdea96e77393a0fca
Cecilia Cacais <ceci.cacais@gmail.com>
2021-07-08 17:32:58 -0300

    requisito 6

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 
 let tabelaDePixels = document.getElementById('pixel-board');
+let tabelaDeCores = document.getElementsByClassName('color');
+
+let primeiraCor = document.querySelector('.color');
+primeiraCor.classList.add('selected');
 
 for (let index = 0; index < 25; index +=1){
     let pixelsDaTabela = document.createElement('div');
     pixelsDaTabela.className='pixel';
     tabelaDePixels.appendChild(pixelsDaTabela);
 }
+
+
+
+
