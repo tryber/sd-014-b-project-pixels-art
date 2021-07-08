9bd9e2a54633f34e2d2463dc2f69a22ed0b4ed8c
Elton Rodrigues <el_dantas1999@hotmail.com>
2021-07-08 17:42:12 -0300

    Requisito #7

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
-window.onload = function () {
-  let numberOfBlocks = 4;
-  createDivs();
+// window.onload = function () {
+//   let numberOfBlocks = 4;
+//   createDivs();
 
-  function createDivs() {
-    for (let index = 0; index < numberOfBlocks; index += 1) {
-      let divs = document.createElement('div');
-      divs.className = 'color';
-      divs.setAttribute('id', 'test');
-      document.querySelector('#color-palette').appendChild(divs);
-    }
-  }
-};
+//   function createDivs() {
+//     for (let index = 0; index < numberOfBlocks; index += 1) {
+//       let divs = document.createElement('div');
+//       divs.className = 'color';
+//       divs.setAttribute('id', 'test');
+//       document.querySelector('#color-palette').appendChild(divs);
+//     }
+//   }
+// };
+let black = document.querySelector('#black');
+black.addEventListener('click', function () {
+  let element = document.querySelector('.selected');
+  element.classList.remove('selected');
+  black.className = 'color selected';
+});
+
+let blue = document.querySelector('#blue');
+blue.addEventListener('click', function () {
+  let element = document.querySelector('.selected');
+  element.classList.remove('selected');
+  blue.className = 'color selected';
+});
+
+let green = document.querySelector('#green');
+green.addEventListener('click', function () {
+  let element = document.querySelector('.selected');
+  element.classList.remove('selected');
+  green.className = 'color selected';
+});
+
+let yellow = document.querySelector('#yellow');
+yellow.addEventListener('click', function () {
+  let element = document.querySelector('.selected');
+  element.classList.remove('selected');
+  yellow.className = 'color selected';
+});
