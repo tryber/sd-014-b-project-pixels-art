c649b9760fb2df3d9c75ba4ea12c73181f32fef6
Nicolas Johnson <nicolas-sousa2011@hotmail.com>
2021-07-08 20:52:39 -0300

    Tentativa de limpar o eslint erros.

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-window.onload = function (){
+window.onload = function () {
   const coresPalet = document.getElementById('color-palette').children;
-  const pixelArt = document.getElementsByClassName('pixel'); 
+  const pixelArt = document.getElementsByClassName('pixel');
   const clearBtn = document.getElementById('clear-board');
-  const pixelBoard = document.getElementById('pixel-board');
-
-  clearBtn.addEventListener('click', clearPixels);
   // Função que adiciona a classe SELECTED na cor selecionada e remove da outra.
-  function selectColor (event){
+  function selectColor(event) {
     // 1° passo um Loop em todas as cores e removo a classe de todos
-    for (let index in coresPalet) {
+    for (let index = 0; index < coresPalet.length; index += 1) {
       if (coresPalet[index].classList) {
         coresPalet[index].classList.remove('selected');
       }
@@ -19,28 +16,23 @@ window.onload = function (){
     }
   }
   // Função que identifica a cor seleciona e pinta o pixel que recebeu o clique.
-  function colore (event){
-    let corSelecionada = document.querySelector('.selected');
-    let cordaVez = window.getComputedStyle(corSelecionada, null).getPropertyValue('background-color');
+  function colore(event) {
+    const corSelecionada = document.querySelector('.selected');
+    const cordaVez = window.getComputedStyle(corSelecionada, null).getPropertyValue('background-color');
     event.target.style.backgroundColor = cordaVez;
   }
   // Cria um loop para adicionar um listener em todos os pixels.
-  for (let index = 0; index < pixelArt.length; index += 1){
+  for (let index = 0; index < pixelArt.length; index += 1) {
     pixelArt[index].addEventListener('click', colore);
   }
   // Cria um Loop para adicionar um listener em todos as cores da Paleta de cores.
   for (let index = 0; index < coresPalet.length; index += 1) {
     coresPalet[index].addEventListener('click', selectColor);
   }
-  function clearPixels () {
+  function clearPixels() {
     for (let index = 0; index < pixelArt.length; index += 1) {
       pixelArt[index].style.backgroundColor = 'white';
     }
   }
+  clearBtn.addEventListener('click', clearPixels);
 };
-
-
-// 1° adicionar um listener em todos os pixels click chama a função colore.
-// 2° função que colore.
-  // 1° identifica a cor da variavel corSelecionada
-  // 2° pinta o pixel que recebel o clique.
