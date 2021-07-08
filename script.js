0b14134c54bd3a35c3ae8479a8b328eeef3b8daa
Vinicius <vinicius@DESKTOP-NBO50A7.localdomain>
2021-07-08 18:46:06 -0300

    Requisito #7 - Adicionando classe Selected

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function addBlocks() {
   const elementId = document.querySelector('#pixel-board');
   for (let iLine = 0; iLine < lines; iLine += 1) {
     for (let i = 0; i < colunms; i += 1) {
-      let divs = document.createElement('div');
+      const divs = document.createElement('div');
       divs.className = 'pixel';
       elementId.appendChild(divs);
     }
@@ -12,11 +12,29 @@ function addBlocks() {
 }
 addBlocks();
 
-function defineBlackColor(color) { 
-  let colorBlack = document.querySelectorAll('.pixel');
+function defineBlackColor(color) {
+  const colorBlack = document.querySelectorAll('.pixel');
   for (let i = 0; i < colorBlack.length; i += 1) {
     colorBlack[i].style.backgroundColor = color;
     colorBlack[i].className += ' selected';
   }
 }
 defineBlackColor('black');
+
+function selectColor() {
+  const colorSelected = document.querySelectorAll('.color');
+  for (let i = 0; i < colorSelected.length; i += 1) {
+    colorSelected[i].addEventListener('click', function () {
+      if (!colorSelected[i].classList.contains('selected')) {
+        for (let index = 0; index < colorSelected.length; index += 1) {
+          colorSelected[index].classList.contains('selected');
+          if (colorSelected[index].classList.contains('selected')) {
+            colorSelected[index].classList.remove('selected');
+          }
+        }
+        colorSelected[i].classList.add('selected');            
+      } else colorSelected[i].classList.remove('selected');
+    })
+  }
+}
+selectColor();
