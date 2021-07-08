ac1a96c8134d0a2e0522ad08d1b3c93a3789d687
Leomar Linhares <leomar_linhares@hotmail.com>
2021-07-08 17:31:29 -0300

    Refatora a forma como a paleta é gerada e cumpre o requisito 8 :rocket:

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+function createColorPalette() {
+  const paletteColor = document.getElementById('color-palette');
+
+  for (let index = 0; index < 4; index += 1) {
+    const color = document.createElement('div');
+    color.className = 'color';
+    color.addEventListener('click', colorSelector)
+
+    switch (index) {
+      case 0:
+        color.style.backgroundColor = 'black';
+        color.classList.add('selected');
+        break;
+
+      case 1:
+        color.style.backgroundColor = 'red';
+        break;
+
+      case 2: 
+        color.style.backgroundColor = 'green';
+        break;
+
+      case 3:
+        color.style.backgroundColor = 'blue';
+        break;
+      
+      default:
+        break;
+    }
+
+    paletteColor.appendChild(color);
+  }
+}
+
 function createPixelBoard() {
   const pixelBoard = document.getElementById('pixel-board');
 
@@ -8,6 +42,7 @@ function createPixelBoard() {
     for (let subIndex = 0; subIndex < 5; subIndex += 1) {
       const pixel = document.createElement('div');
       pixel.className = 'pixel';
+      pixel.addEventListener('click', paint)
       line.appendChild(pixel);
     }
     pixelBoard.appendChild(line);
@@ -19,12 +54,10 @@ function colorSelector(event) {
   event.target.classList.add('selected');
 }
 
-function turnPaletteSelectable() {
-  const colors = document.getElementsByClassName('color');
-  for (const color of colors) {
-    color.addEventListener('click', colorSelector);
-  }
+function paint(event) {
+  const selectedColor = document.querySelector('.selected').style.backgroundColor;
+  event.target.style.backgroundColor = selectedColor;
 }
 
+createColorPalette();
 createPixelBoard();
-turnPaletteSelectable();
