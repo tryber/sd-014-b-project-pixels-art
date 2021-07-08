3cefe8b33445cad475cbbc8701de6047ebe7ea42
Thiago Frozzi <thiagofrozzi@hotmail.com>
2021-07-08 18:58:20 -0300

    Requisito 8 pronto

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-window.onload 
+const firstDiv = document.getElementById('black');
+const secondDiv = document.getElementById('yellow');
+const thirdDiv = document.getElementById('red');
+const forDiv = document.getElementById('blue');
+
+let box = document.querySelector('#pixel-board');
+    box.addEventListener("click",printColor);
+
+
+
+function selectColor(event){
+    let color = document.querySelector('.selected');
+    color.classList.remove('selected');
+    event.target.classList.add('selected');
+}
+
+firstDiv.addEventListener("click",selectColor)
+secondDiv.addEventListener("click",selectColor)
+thirdDiv.addEventListener("click",selectColor)
+forDiv.addEventListener("click",selectColor)
+
+
+function printColor(event){
+    let boxprinted = document.querySelector('.selected')
+        
+    event.target.style.backgroundColor = boxprinted.id;
+}
+
