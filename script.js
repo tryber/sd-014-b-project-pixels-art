7145726a2ea679f0749f3abe8a76e9c831f0e346
Adilson Gabriel <adilsongb.rabelo@gmail.com>
2021-07-08 16:08:54 -0300

    Requisito 2 e 3 :white_check_mark:

---
+++ script.js
@@ -0,0 +1,11 @@
+
+window.onload = carregarPaleta;
+
+function carregarPaleta() {
+
+  document.querySelectorAll('.color')[0].style.backgroundColor = 'rgb(0,0,0)';
+  document.querySelectorAll('.color')[1].style.backgroundColor = 'rgb(227,113,113)';
+  document.querySelectorAll('.color')[2].style.backgroundColor = 'rgb(55,154,234)';
+  document.querySelectorAll('.color')[3].style.backgroundColor = 'rgb(226,232,81)';
+
+}
