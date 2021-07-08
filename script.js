7c25697f818e42d84bfec237b70b3da89c94fed8
brenoMatias <matiasbreno19@gmail.com>
2021-07-08 16:42:11 -0300

    Requisito 1

---
+++ script.js
@@ -0,0 +1,6 @@
+// Requisito 1
+const header = document.createElement('h1');
+header.innerText = 'Paleta de Cores';
+header.id = 'title';
+document.getElementById('mainContent').appendChild(header);
+
