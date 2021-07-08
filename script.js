5e2f89a6c4b3bc0ebcb959419f8ebd6d7c1a4e0c
Kelner Roberto <kelner250@hotmail.com>
2021-07-08 17:15:12 -0300

    requisito #4

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const secaoPaleta = document.getElementById('color-palette');
 const arrColor = ['black', 'red', 'green', 'blue'];
 
-function criaPaleta (){
+function criaPaleta(){
     for (let index = 0; index < 4; index += 1){
         let palleteBox = document.createElement('div');
         palleteBox.className = 'color';
@@ -14,4 +14,24 @@ function criaPaleta (){
 }
 criaPaleta();
 
+//----------------------------------------------------------------------
+
+//Cria quadrado de pixels brancos
+
+const secaoPixelBoard = document.getElementById('pixel-board');
+
+function criaPixelBoard() {
+    for (let index = 0; index < 5; index +=1) {
+        let pixelBoard = document.createElement('div');
+        secaoPixelBoard.appendChild(pixelBoard);
+        for (let i = 0; i < 5; i += 1){
+            let pixelLine = document.createElement('div');
+            pixelLine.className = 'pixel';
+            pixelLine.style.backgroundColor = 'white';
+            pixelLine.style.border = '1px solid black';
+            pixelBoard.appendChild(pixelLine);
+        }
+    }
+}
+criaPixelBoard();
 
