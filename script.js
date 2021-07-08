b4b356bb499d77faf4b3e1f5a61a5876d703304d
eli <leonardohcds@gmail.com>
2021-07-08 18:19:12 -0300

    requirement 1 to 7

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-// const selectedColor = document.querySelector('.selected');
+const palette = document.querySelectorAll('.color');
+const black = document.querySelector('.black');
+const red = document.querySelector('.red');
+const orange = document.querySelector('.orange');
+const teal = document.querySelector('.teal');
 
-// selectedColor.addEventListener('click', function (event) {
-//     event.target.
-// })
+
+function takeColor(event){
+    let selected = document.querySelector('.selected');
+    selected.classList.remove('selected');
+    event.target.classList.add('selected'); 
+}
+
+black.addEventListener('click', takeColor);
+red.addEventListener('click', takeColor);
+orange.addEventListener('click', takeColor);
+teal.addEventListener('click', takeColor);
