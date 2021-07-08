22c9fdfb78c14781b9492473443cb652c99b228a
dhenycruz <dheniarley.ds@gmail.com>
2021-07-08 19:48:46 -0300

    Requisito #4.

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ colors[0].setAttribute('style', 'background-color: black');
 colors[1].setAttribute('style', 'background-color: green');
 colors[2].setAttribute('style', 'background-color: yellow');
 colors[3].setAttribute('style', 'background-color: pink');
+
+const lineUser = 5;
+const columnUser = 5;
+
+function createLine(number) {
+  for (let col = 1; col <= number; col += 1) {
+    const divLine = document.createElement('div');
+    divLine.setAttribute('class', 'tr');
+    document.querySelector('#table').appendChild(divLine);
+  }
+}
+
+function fillCells(cells, paramLines) {
+  for (let line = 0; line < paramLines.length; line += 1) {
+    for (let col = 1; col <= cells; col += 1) {
+      const div = document.createElement('div');
+      div.setAttribute('class', 'pixel td');
+      paramLines[line].appendChild(div);
+    }
+  }
+}
+
+createLine(lineUser);
+const lines = document.querySelectorAll('.tr');
+fillCells(columnUser, lines);
