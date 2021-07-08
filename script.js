01270ad0580087ef39a9b8154513d970ea91f61b
Leomar Linhares <leomar_linhares@hotmail.com>
2021-07-08 16:09:46 -0300

    Ajusta o código às exigências do lint :rocket:

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function createPixelBoard() {
-  let pixelBoard = document.getElementById('pixel-board');
+  const pixelBoard = document.getElementById('pixel-board');
 
-  for (let index = 0; index < 5; index++) {
+  for (let index = 0; index < 5; index += 1) {
     const line = document.createElement('div');
     line.className = 'line';
 
-    for (let index = 0; index < 5; index++) {
+    for (let subIndex = 0; subIndex < 5; subIndex += 1) {
       const pixel = document.createElement('div');
       pixel.className = 'pixel';
       line.appendChild(pixel);
