dd3fed128f4a23dfff0db6ef44fbe50e52de1d17
lucas <mr.pine97@gmail.com>
2021-07-08 15:28:55 -0300

    Requisito #2 e coincidentemente #3

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 function h1Creation () {
-  let bodySelection = document.querySelector('body')
-  let titleCreation = document.createElement('h1')
-  titleCreation.innerText = 'Paleta de Cores'
-  titleCreation.id = 'title'
-  bodySelection.appendChild(titleCreation)
+  let bodySelection = document.querySelector('body');
+  let titleCreation = document.createElement('h1');
+  titleCreation.innerText = 'Paleta de Cores';
+  titleCreation.id = 'title';
+  bodySelection.appendChild(titleCreation);
+}
+function paletteCreation () {
+  let bodySelection = document.querySelector('body');
+  let divCreation = document.createElement('div')
+  divCreation.id = 'color-palette'
+  bodySelection.appendChild(divCreation)
+}
+function colors (colorDesired) {
+  let divSelection = document.querySelector('#color-palette')
+  let divCreation = document.createElement('div')
+  divCreation.classList.add('color')
+  divCreation.id = colorDesired
+  divSelection.appendChild(divCreation)
 }
 
 h1Creation()
+paletteCreation()
+colors('black')
+colors('yellow')
+colors('red')
+colors('green')
