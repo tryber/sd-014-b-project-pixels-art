28bd6d6c9986196bac0af7c398581eda6c61e22a
Mariana Ferreira <marianadesouzaferreira01@gmail.com>
2021-07-08 17:33:00 -0300

    requisito 3

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ window.onload = function () {
 
     for (let colors of paletaDeCores) {
         if (paletaDeCores[0] == colors) {
-         colors.style.backgroundColor = 'yellow';
+            colors.style.backgroundColor = 'black';
         }
         else if (paletaDeCores[1] == colors) {
-         colors.style.backgroundColor = 'blue';
+            colors.style.backgroundColor = 'blue';
         }
         else if (paletaDeCores[2] == colors) {
-         colors.style.backgroundColor = 'green';
+            colors.style.backgroundColor = 'purple';
         }
         else if (paletaDeCores[3] == colors) {
-         colors.style.backgroundColor = 'red';
+            colors.style.backgroundColor = 'pink';
         }
     }
 }
