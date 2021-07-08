463db18d511d972e5227354f2efab5918b4e332c
Gabriel Takaki <gabriel.f.takaki@gmail.com>
2021-07-08 16:05:53 -0300

    primeiros passos

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('#color1').style.backgroundColor = 'black';
+document.querySelector('#color2').style.backgroundColor = 'red';
+document.querySelector('#color3').style.backgroundColor = 'orange';
+document.querySelector('#color4').style.backgroundColor = 'yellow';
