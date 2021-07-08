607f63cade9dd157d373f933ff50bc3250a99342
Jucélio <juceneon@gmail.com>
2021-07-08 17:50:16 -0300

    Alguns erros do ESlint resolvidos sobre identação

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-let colorPalette = document.querySelectorAll('.color');
-let pixels = document.querySelectorAll('.pixel');
-let clearButton = document.querySelector('#clear-board');
+const colorPalette = document.querySelectorAll('.color');
+const pixels = document.querySelectorAll('.pixel');
+const clearButton = document.querySelector('#clear-board');
 
-for (let colors of colorPalette) {
-    colors.addEventListener('click', function (event) {
-        for (let color of colorPalette) {
-            color.classList.remove('selected');
-        }
-        event.target.classList.add('selected')
+for (const colors of colorPalette) {
+  colors.addEventListener('click', function (event) {
+    for (let color of colorPalette) {
+      color.classList.remove('selected');
+    }
+  event.target.classList.add('selected')
     })
 }
-let selected = document.querySelector('.selected');
-for (let pixel of pixels) {
-    pixel.addEventListener('click', function (event) {
-        let selectedColor = document.querySelector('.selected').classList[1];
-        event.target.classList.remove(event.target.classList[1]);
-        event.target.classList.add(selectedColor);
-    })
+
+for (const pixel of pixels) {
+  pixel.addEventListener('click', function (event) {
+    const selectedColor = document.querySelector('.selected').classList[1];
+    event.target.classList.remove(event.target.classList[1]);
+    event.target.classList.add(selectedColor);
+  })
 }
 
 clearButton.addEventListener('click', function () {
-    for (let pixel of pixels) {
-        pixel.classList.remove(pixel.classList[1]);
-        pixel.classList.add('white')
-    }
-})
+  for (let pixel of pixels) {
+    pixel.classList.remove(pixel.classList[1]);
+    pixel.classList.add('white')
+}
+});
