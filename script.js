9b78d736a3f17cdeade3a132f71d9bea42b8ac58
Denis Espanhol <denisgfespanhol@gmail.com>
2021-07-08 19:07:21 -0300

    Sétimo requisito do Project Pixels Art.

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ let blackColor = document.querySelector('.black');
 window.addEventListener('load', function() {
   blackColor.classList.add('selected')
 })
+
+let colors = document.querySelectorAll('.color');
+let divColors = document.body.children[1];
+
+function selectedPallet(event) {
+  for (let index = 0; index < colors.length; index += 1) {
+    if (colors[index].classList.length === 3) {
+      event.target.parentElement.children[index].classList.remove('selected');
+    }
+    if (colors[index].classList.length === 2) {
+      event.target.classList.add('selected');
+    }
+  }
+}
+
+divColors.addEventListener('click', selectedPallet);
