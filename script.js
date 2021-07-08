97880f7e6ba5fca97fee03ef9bbf526b52883d7a
viniciussantana <viniciusantana@yahoo.com.br>
2021-07-08 16:39:25 -0300

    Requisito #8

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const color = document.querySelectorAll('.color');
+const pixel = document.querySelectorAll('.pixel');
 
 function selected(event) {
   const select = document.querySelector('.selected');
@@ -7,4 +8,13 @@ function selected(event) {
 }
 for (let index = 0; index < color.length; index += 1) {
   color[index].addEventListener('click', selected);
+}
+
+function pixelColor(pixels) {
+  const select = document.querySelector('.selected');
+  const cor = select.id;
+  pixels.target.style.backgroundColor = cor;
+}
+for (let index = 0; index < pixel.length; index += 1) {
+  pixel[index].addEventListener('click', pixelColor);
 }
