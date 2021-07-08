3cc51ad186b0fbf22209972ad91d5f2eac96b8da
Jucélio <juceneon@gmail.com>
2021-07-08 17:29:29 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let colorPalette = document.querySelectorAll('.color');
+let pixels = document.querySelectorAll('.pixel');
+let clearButton = document.querySelector('#clear-board');
+
 for (let colors of colorPalette) {
     colors.addEventListener('click', function (event) {
         for (let color of colorPalette) {
@@ -7,3 +10,18 @@ for (let colors of colorPalette) {
         event.target.classList.add('selected')
     })
 }
+let selected = document.querySelector('.selected');
+for (let pixel of pixels) {
+    pixel.addEventListener('click', function (event) {
+        let selectedColor = document.querySelector('.selected').classList[1];
+        event.target.classList.remove(event.target.classList[1]);
+        event.target.classList.add(selectedColor);
+    })
+}
+
+clearButton.addEventListener('click', function () {
+    for (let pixel of pixels) {
+        pixel.classList.remove(pixel.classList[1]);
+        pixel.classList.add('white')
+    }
+})
