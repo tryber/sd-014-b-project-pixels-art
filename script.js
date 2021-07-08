7db49da983d0ee67b44b3c96b832784b4aa129b4
Paulo França <paulovinicius.franca@gmail.com>
2021-07-08 17:19:07 -0300

    insere a cor inicial branca para os pixels e altera o tamanho para 40px

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let pixelTableSize = 5;
+const pixelTableSize = 5;
 
-function createPixelBoard () {
-  let getBody = document.querySelector('body');
+function createPixelBoard() {
+  const getBody = document.querySelector('body');
   console.log(getBody);
-  let pixelBoard = document.createElement('section');
+  const pixelBoard = document.createElement('section');
   pixelBoard.id = 'pixel-board';
   getBody.appendChild(pixelBoard);
 }
@@ -12,13 +12,13 @@ createPixelBoard();
 
 // A ideia de usar criar uma linha para depois associar o conjunto de pixels, veio da resposta no https://stackoverflow.com/questions/29229523/how-and-why-to-use-display-table-cell-css
 
-function createPixels () {
-  for (index = 0; index < pixelTableSize; index +=1) {
+function createPixels() {
+  for (let index = 0; index < pixelTableSize; index += 1) {
     let boardLine = document.createElement('div');
-    boardLine.className = 'board-line'
+    boardLine.className = 'board-line';
     document.querySelector('#pixel-board').appendChild(boardLine);
-    for (secondIndex = 1; secondIndex <= pixelTableSize; secondIndex += 1) {
-      let pixel = document.createElement('div');
+    for (let secondIndex = 1; secondIndex <= pixelTableSize; secondIndex += 1) {
+      const pixel = document.createElement('div');
       pixel.className = 'pixel';
       document.querySelectorAll('.board-line')[index].appendChild(pixel);    
     }
@@ -26,4 +26,3 @@ function createPixels () {
 }
 
 createPixels();
-
