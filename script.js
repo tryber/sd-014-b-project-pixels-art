5ec845376f9844ba1d145295dbbca7bcd3d72da4
enzo <enzo@pop-os.localdomain>
2021-07-08 19:16:16 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-window.onload = function () {
-  const selectBlack = document.getElementsByClassName('color')[0];
-  selectBlack.classList.add('selected');
-};
-const title = document.querySelector('#title');
-title.innerHTML = 'Paleta de Cores';
-
-function setColor() {
-  const colorPalette = document.querySelectorAll('.color');
-  colorPalette[0].style.backgroundColor = 'Black';
-  colorPalette[1].style.backgroundColor = 'Blue';
-  colorPalette[2].style.backgroundColor = 'Yellow';
-  colorPalette[3].style.backgroundColor = 'Red';
-}
-setColor();
-
-function selectColor () {
-    const palettColors = document.querySelectorAll('.color');
-    
-}
+const black = document.getElementById('#black');
+const blue = document.getElementById('#blue');
+const yellow = document.getElementById('#yellow');
+const red = document.getElementById('#red');
+const selectedColor = document.getElementsByClassName('selected');
