6268547d7d177d2dc6b8f842bdba6c8d68dd63d0
Gustavo <gus.inacio@hotmail.com>
2021-07-08 17:35:10 -0300

    Requisito #7

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 let black = document.querySelector('.black');
 black.classList.add('selected');
+const purple = document.querySelector('.purple');
+const yellow = document.querySelector('.yellow');
+const green = document.querySelector('.green');
+
+
+let selectedElement = document.querySelector('.selected');
+function handleChangeSelected (event) {
+    const selectedElement = document.querySelector('.selected');
+    selectedElement.classList.remove('selected');
+    event.target.classList.add('selected');
+}
+purple.addEventListener('click', handleChangeSelected);
+yellow.addEventListener('click', handleChangeSelected);
+green.addEventListener('click', handleChangeSelected)
+black.addEventListener('click', handleChangeSelected);
+
 
     
