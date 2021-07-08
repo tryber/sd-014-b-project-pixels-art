db7e4bd9742c1e61fb0a4c0c2ec5afa305e26a89
Arturo Vaine <arturo.vaine@gmail.com>
2021-07-08 18:19:03 -0300

    Update.

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-window.onload = function () {
-    document.getElementById('black').classList.add('selected');
+window.onload = function start () {
+    document.body.getElementById('black').classList.add('selected');
+    document.body.getElementById('color1').style.backgroundColor = 'red';
     // Requisito 6: 6 - Defina a cor preta como cor inicial. Ao carregar a página, a cor preta já deve estar selecionada para pintar os pixels
-}
+};
+/*
+var randomColor = Math.floor(Math.random()*16777215).toString(16);
+console.log(Math.floor(Math.random()*16777215).toString(16));
 
+function randomColor () {
+    document.getElementById('color1').style.backgroundColor = rgb(225,258,215);
+    document.getElementById('color2').style.backgroundColor = randomColor;
+    document.getElementById('color3').style.backgroundColor = randomColor;
+}*/
 
-// var randomColor = Math.floor(Math.random()*16777215).toString(16);
+function clearBoard() {
+    document.body.getElementById('color1').style.backgroundColor = "red";
+    document.body.getElementById('color2').style.backgroundColor = "white";
+    document.body.getElementById('color3').style.backgroundColor = "white";
+};
