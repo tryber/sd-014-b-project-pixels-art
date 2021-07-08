c7278873d076730411e22ef7f6bfa7ef0015c1ee
enzo <enzo@pop-os.localdomain>
2021-07-08 20:45:40 -0300

    Revisão do código

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const black = document.getElementById('black');
-const blue = document.getElementById('blue');
-const yellow = document.getElementById('yellow');
-const red = document.getElementById('red');
-const mainColor = document.getElementsByClassName('selected');
+// const black = document.getElementById('black');
+// const blue = document.getElementById('blue');
+// const yellow = document.getElementById('yellow');
+// const red = document.getElementById('red');
+// const mainColor = document.getElementsByClassName('selected');
 
 
-function changeClassColor (event) {
-    const selectedColor = event.target
-    if (selectedColor !== mainColor) {
-        mainColor.classList.remove('selected')
-        selectedColor.classList.add('selected')
-     }
-}
-changeClassColor();
+// function changeClassColor (event) {
+//     const selectedColor = event.target
+//     if (selectedColor !== mainColor) {
+//         mainColor.classList.remove('selected')
+//         selectedColor.classList.add('selected')
+//      }
+// }
+// changeClassColor();
 
-black.addEventListener('click', changeClassColor)
-blue.addEventListener('click', changeClassColor)
-yellow.addEventListener('click', changeClassColor)
-red.addEventListener('click', changeClassColor)
+// black.addEventListener('click', changeClassColor)
+// blue.addEventListener('click', changeClassColor)
+// yellow.addEventListener('click', changeClassColor)
+// red.addEventListener('click', changeClassColor)
