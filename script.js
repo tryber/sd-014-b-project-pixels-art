6bc6e1e02cd2643b77ab4c0c7d78fa3c659c6a0a
samsantosb <samsantosb@outlook.com>
2021-07-08 19:04:22 -0300

    ajudas do meu amigo glauco

--- script.js
+++ script.js
@@ -13,19 +13,14 @@ window.onload = function () {
       element.style.backgroundColor = 'green';
     }
     element.addEventListener('click', function (event) {
-      //adiciona a classe 'selected' ao elemento clicado, e retira de outro que esteja com a classe
       for (let i = 0; i < colorsPallete.length; i += 1) {
         colorsPallete[i].classList.remove('selected');
         event.target.classList.add('selected');
+        for (let i = 1; i <= 25; i += 1) {
+          pixel.classList.add('pixel');
+        }
       }
     });
-  }
-
-  let pixelBoard = document.querySelector('#pixel-board');
-
-  for (let i = 1; i <= 25; i += 1) {
-    pixel.classList.add('pixel');
-    pixelBoard.appendChild(pixel);
   }
 };
 
@@ -38,7 +33,6 @@ pixelBoard.addEventListener('click', function (event) {
 
 const clear = () => {
   const totalDeleteLesgoTrybers = document.querySelector('#clear-board');
-
   totalDeleteLesgoTrybers.addEventListener('click', () => {
     const pxl = document.querySelectorAll('.pixel');
     for (let index = 0; index < pxl.length; index += 1) {
