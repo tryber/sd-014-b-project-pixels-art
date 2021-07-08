a02cb5f93aa61298439a410fdce7da7c01abb541
mahnetti <mariah.manetti@gmail.com>
2021-07-08 18:10:29 -0300

    Desafio 4

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
+// Quando a página carregar vai executar essa função (colocar cor nos quadrados)
 window.onload = function () {
-let colors = document.getElementsByClassName('color');
-
-for (let i = 0; i < colors.length; i += 1){
-  if (i === 0){
-    colors[0].style.backgroundColor = 'black';
+  let colors = document.getElementsByClassName('color');
+//variavel colors com o caminho do elemento
+  for (let i = 0; i < colors.length; i += 1) {
+    if (i === 0) {
+      colors[0].style.backgroundColor = 'black';
+    }
+    if (i === 1) {
+      colors[1].style.backgroundColor = '#AB4E68';
+    }
+    if (i === 2) {
+      colors[2].style.backgroundColor = '#533745';
+    }
+    if (i === 3) {
+      colors[3].style.backgroundColor = '#9D9171';
+    }
   }
-  if (i === 1){
-    colors[1].style.backgroundColor = '#AB4E68';
+  let pixelBoard = document.getElementById ('pixel-board');
+  for (let i = 0; i < 25; i += 1) {
+    let elemento = document.createElement('div');
+    elemento.classList.add('pixel');
+    pixelBoard.appendChild(elemento);
   }
-  if (i === 2){
-    colors[2].style.backgroundColor = '#533745';
-  }
-  if (i === 3){
-    colors[3].style.backgroundColor = '#9D9171';
-  }
-
-}
 }
