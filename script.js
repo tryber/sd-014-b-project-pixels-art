9181aa98e777f172de346eda8a4d2a42ac2ccbb7
Nataniel Santos <nathan.santos159@hotmail.com>
2021-07-08 17:09:26 -0300

    requisito 8

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ function selectColor(selectedColor){
   }
   selectedColor.target.className += " selected"
 }
+
+
+let pixels = document.querySelectorAll('.pixel')
+for (let pixel of pixels) {
+  pixel.addEventListener('click', paintPixel)
+}
+function paintPixel(pixel){
+  let selectedColor = document.querySelector('.selected')
+  style = window.getComputedStyle(selectedColor) // referencia: https://stackoverflow.com/questions/25238153/how-to-get-background-color-property-value-in-javascript/25238247
+  // a linha acima basicamente atribui a variavel style todo o css referente ao elemento passado como parametro, no caso selectedColor
+  pixel.target.style.backgroundColor = style.backgroundColor // atribuindo a cor selecionada à cor de fundo do pixel clicado
+}
