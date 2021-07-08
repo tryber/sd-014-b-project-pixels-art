8f77bec1a0fec732127cf49443165ff87ea0c5f2
Ana-Alice-Honorio <ana.alice.honorio@gmail.com>
2021-07-08 18:16:34 -0300

    requisito 6 select

---
+++ script.js
@@ -0,0 +1,6 @@
+let pegaPreto = document.getElementsByClassName('color');
+pegaPreto[0].classList.add('selected');
+function criaGrid() {
+  const alturaGrid = 5;
+  const larguraGrid = 5;
+  const grid = document.getElementById('pixel-board');
