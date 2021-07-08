1f0da033fa08ffca451f18b5b8a6b597264b6217
Everson Elias Gonçalves de Oliveira <eversoneliasg@gmail.com>
2021-07-08 17:56:55 -0300

    Requisito #7

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 window.onload = function () {
   
-  let blackBox = document.getElementById('blackBox');
+  const blackBox = document.getElementById('blackBox');
   
-  let cornBox = document.getElementById('cornFlowerBox');
+  const cornBox = document.getElementById('cornFlowerBox');
   
-  let hotBox = document.getElementById('hotPinkBox');
+  const hotBox = document.getElementById('hotPinkBox');
   
-  let tiffanyBox = document.getElementById('tiffanyBlueBox');
+  const tiffanyBox = document.getElementById('tiffanyBlueBox');
 
   blackBox.addEventListener('click', pickColor);
-// cornBox.addEventListener('click', pickColor);
-// hotBox.addEventListener('click', pickColor);
-// tiffanyBox.addEventListener('click', pickColor);
+  cornBox.addEventListener('click', pickColor);
+  hotBox.addEventListener('click', pickColor);
+  tiffanyBox.addEventListener('click', pickColor);
 
   function pickColor(event) {
-    let boxColor = event.target.style.backgroundColor;
+    let previousSelected = document.querySelector('.selected');
+    previousSelected.classList.remove('selected');
+    event.target.classList.add('selected');
   }
 
-  let pixelsArray = document.querySelectorAll('.pixel');
+  const pixelsArray = document.querySelectorAll('.pixel');
 
   for (let index = 0; index < pixelsArray.length; index += 1) {
     pixelsArray[index].addEventListener('click', changeToSelectedColor);
@@ -25,7 +27,7 @@ window.onload = function () {
 
   function changeToSelectedColor(event) {
     let pickedColor = document.querySelector('.selected');
-    style = window.getComputedStyle(pickedColor);
+    let style = window.getComputedStyle(pickedColor);
     event.target.style.backgroundColor = style.getPropertyValue('background-color');
   }
 
