85f072c44620a6066d6cfee3bbfb98aa2adbf092
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-07-08 15:16:36 -0300

    Requisito #2

---
+++ script.js
@@ -0,0 +1,9 @@
+let td1 = document.querySelector('tr').children[0];
+let td2 = document.querySelector('tr').children[1];
+let td3 = document.querySelector('tr').children[2];
+let td4 = document.querySelector('tr').children[3];
+
+td1.style.backgroundColor = 'rgb(125, 231, 85)';
+td2.style.backgroundColor = 'rgb(51, 200, 170)';
+td3.style.backgroundColor = 'rgb(189, 85, 211)';
+td4.style.backgroundColor = 'rgb(52, 20, 89)';
