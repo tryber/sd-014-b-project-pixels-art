6a4dee7b5f3dc4a134488a9b2b75aed59a518d85
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-07-08 16:50:42 -0300

    Lint e estilizacao

--- script.js
+++ script.js
@@ -1,63 +1,52 @@
 /* eslint-disable max-lines-per-function */
-window.onload = function() {
+window.onload = function () {
   const sectionPalette = document.getElementById('color-palette');
-  const clearButton = document.getElementById('clear-board');
-  
+  const clearButton = document.getElementById('clear-board');  
   for (let i = 1; i <= 4; i += 1) {
-    let divPalette = document.createElement('div');
+    const divPalette = document.createElement('div');
     divPalette.className = 'color';
-    sectionPalette.appendChild(divPalette);    
+    sectionPalette.appendChild(divPalette);
   }
+  const divsPalette = document.querySelectorAll('div');
+  const colors = ['black', 'green', 'blue', 'red'];
+  const divsIds = ['first-palette', 'second-palette', 'third-palette', 'fourth-palette'];
 
-  let divsPalette = document.querySelectorAll('div');  
-  let colors = ['black','green','blue','red'];
-  let divsIds = ['first-palette','second-palette','third-palette','fourth-palette'];
-
-  for (let i = 0; i < divsPalette.length; i +=1) {
+  for (let i = 0; i < divsPalette.length; i += 1) {
     divsPalette[i].style.backgroundColor = colors[i];
     divsPalette[i].id = divsIds[i];
   }
-
-  divsPalette[0].classList.add('selected');
-  
-  
-  let miniFrame = document.querySelectorAll('td');
-
+  divsPalette[0].classList.add('selected');  
+  const miniFrame = document.querySelectorAll('td');
   for (let i = 0; i < miniFrame.length; i += 1) {
-    miniFrame[i].className = 'pixel';    
+    miniFrame[i].className = 'pixel';
   }
-  
-
-  clearButton.addEventListener('click',function() {
+  clearButton.addEventListener('click', function clearBoard() {
     for (let i = 0; i < miniFrame.length; i += 1) {
       miniFrame[i].style.backgroundColor = 'white';      
     }
-  })
+  });
 
   function changeSelectedPalette (event) {
     let selectedElement = document.querySelector('.selected');
     selectedElement.classList.remove('selected');
     event.target.classList.add('selected');
   }
+  const firstPalette = document.getElementById('first-palette');
+  const secondPalette = document.getElementById('second-palette');
+  const thirdPalette = document.getElementById('third-palette');
+  const fourthPalette = document.getElementById('fourth-palette');
 
-  let firstPalette = document.getElementById('first-palette');
-  let secondPalette = document.getElementById('second-palette');
-  let thirdPalette = document.getElementById('third-palette');
-  let fourthPalette = document.getElementById('fourth-palette');
+  firstPalette.addEventListener('click', changeSelectedPalette);
+  secondPalette.addEventListener('click', changeSelectedPalette);
+  thirdPalette.addEventListener('click', changeSelectedPalette);
+  fourthPalette.addEventListener('click', changeSelectedPalette);
 
-  firstPalette.addEventListener('click',changeSelectedPalette);
-  secondPalette.addEventListener('click',changeSelectedPalette);
-  thirdPalette.addEventListener('click',changeSelectedPalette);
-  fourthPalette.addEventListener('click',changeSelectedPalette);
-
-  function coloringFrame () {
+  function coloringFrame() {
     let table = document.getElementById('pixel-board');
     table.addEventListener('click',function (event) {
       let selectedElement = document.querySelector('.selected');
       event.target.style.backgroundColor = selectedElement.style.backgroundColor;
-    })
+    });
   }
   coloringFrame();
-
-
 }
