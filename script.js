7f77e4b89e8cc79402534accba7ffd6e1353378b
Riba Jr <ribastudio@gmail.com>
2021-07-08 16:40:07 -0300

    Correção de alguns requisitos e alinhamentos do CSS, início das anotações do raciocínio lógico para desenvolver os requisitos

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 window.onload = function() {
+  let boardColor = 
+
+
+}
+
+document.querySelector('')
+
+// função "clickColorSelect", que irá adicionar um addeventlistener para quando clicar no mouse, gravar a cor que está no elemento clicado, para ser inserido do elemento que será clicado após
+
+// deve existir uma função que verifica se o primeiro elemento está na cor preta, para depois randomizar as outras cores
+
+// função para limitar o tamanho mínimo e máximo do board
+
+// função que gera as cores da paleta aleatoriamente
+
+// função que captura a entrada de valores do input e a partir dele, gera a tabela com largura e altura definidos. Ex.: se digitar "7", a tabela terá 7 linhas e 7 colunas;
+
+function generateTable(value) {
+  let tableValue = document.getElementById('board-size');
+  let tableNewSize = tableValue.innerText;
+}
+
+//função que limpa toda a tabela, baseado no exercício da pirâmide
+function erasePixelTable() {
   
-
-
 }
