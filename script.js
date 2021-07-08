06c005bd90f78ad7aeb8b5284947216bbf7266ac
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-07-08 17:07:44 -0300

    Requisito 8 - Clicar em um pixel dentro do quadro após selecionar uma cor na paleta faz com que o pixel seja preenchido com a cor selecionada.

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
-const blackPixel = document.getElementById('black');
-const orangeRedPixel = document.getElementById('orange-red');
-const bluePixel = document.getElementById('blue');
-const greenPixel = document.getElementById('green');
-
-function selectedColor() {
-  function changeSelected(event) {
-    let divSelected = document.querySelector('.selected');
-    divSelected.classList.remove('selected');
-    event.target.classList.add('selected');
+window.onload = function() {
+  const blackPixel = document.getElementById('black');
+  const orangeRedPixel = document.getElementById('orange-red');
+  const bluePixel = document.getElementById('blue');
+  const greenPixel = document.getElementById('green');
+  
+  function selectedColor() {
+    function changeSelected(event) {
+      const divSelected = document.querySelector('.selected');
+      divSelected.classList.remove('selected');
+      event.target.classList.add('selected');
+    }
+  
+    blackPixel.addEventListener('click', changeSelected);
+    orangeRedPixel.addEventListener('click', changeSelected);
+    bluePixel.addEventListener('click', changeSelected);
+    greenPixel.addEventListener('click', changeSelected);
   }
+  
+  selectedColor();
 
-  blackPixel.addEventListener('click', changeSelected);
-  orangeRedPixel.addEventListener('click', changeSelected);
-  bluePixel.addEventListener('click', changeSelected);
-  greenPixel.addEventListener('click', changeSelected);
+  document.addEventListener('click', function(event) {
+    console.log(event);
+    if (event.target.classList.contains('pixel')) {
+      event.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
+    }
+  }, false);
+  //Acima foi ultilizado o conceito de event bubbling contido no link https://gomakethings.com/attaching-multiple-elements-to-a-single-event-listener-in-vanilla-js/
 }
-
-selectedColor();
