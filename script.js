fea03f5192da8ea211388ab936c34a7d38d27931
lucas <mr.pine97@gmail.com>
2021-07-08 20:53:16 -0300

    Requisito #8 + refatoração

--- script.js
+++ script.js
@@ -18,13 +18,73 @@ function colors (colorDesired) {
   if (colorDesired === 'black') {
     divCreation.classList.add('selected')
   }
-  divCreation.id = colorDesired
+  divCreation.classList.add(colorDesired)
   divSelection.appendChild(divCreation)
 }
+
+function setPixelColor(evnt) {
+  evnt.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
+}
+function abc () {
+  document.querySelector('#pixel-board').classList.add('bioca')
+}
+
+function tableContainer () {
+  let bodySelection = document.querySelector('body');
+  let divCreation = document.createElement('div')
+  divCreation.id = 'pixel-board'
+  bodySelection.appendChild(divCreation)
+}
+function pixelDivs () {
+  let pixelBoardSelection = document.querySelector('#pixel-board');
+  let divCreation = document.createElement('div')
+  divCreation.classList.add('tr')
+  pixelBoardSelection.appendChild(divCreation)
+}
+function pixelsX (index) {
+  let divCreation = document.createElement('div')
+  divCreation.classList.add('pixel')
+  let pixelBoardSelection = document.querySelectorAll('.tr')
+  pixelBoardSelection[index].appendChild(divCreation)
+}
+function pixelTable () {
+  tableContainer()
+pixelDivs()
+pixelDivs()
+pixelDivs()
+pixelDivs()
+pixelDivs()
+pixelsX(0)
+pixelsX(0)
+pixelsX(0)
+pixelsX(0)
+pixelsX(0)
+pixelsX(1)
+pixelsX(1)
+pixelsX(1)
+pixelsX(1)
+pixelsX(1)
+pixelsX(2)
+pixelsX(2)
+pixelsX(2)
+pixelsX(2)
+pixelsX(2)
+pixelsX(3)
+pixelsX(3)
+pixelsX(3)
+pixelsX(3)
+pixelsX(3)
+pixelsX(4)
+pixelsX(4)
+pixelsX(4)
+pixelsX(4)
+pixelsX(4)
+}
+
 function swapClass(evnt) {
-  let classSelection = document.querySelector('.selected');// procuro a class tech
-  classSelection.classList.remove('selected');//removo a class tech
-  evnt.target.classList.add('selected');//adiciono a class tech ao elemento que eu cliquei
+  let classSelection = document.querySelector('.selected');// procuro a class 
+  classSelection.classList.remove('selected');//removo a class 
+  evnt.target.classList.add('selected');//adiciono a class ao elemento que eu cliquei
 }
 
 h1Creation()
@@ -33,4 +93,11 @@ colors('black')
 colors('yellow')
 colors('red')
 colors('green')
+pixelTable()
+function setPixelColor (event){
+  let selection = document.querySelector('.selected')
+  let selectionColor = window.getComputedStyle(selection).getPropertyValue("background-color")
+  event.target.style.backgroundColor = selectionColor
+}
 document.querySelector('#color-palette').addEventListener('click', swapClass)
+document.querySelector('#pixel-board').addEventListener('click', setPixelColor)
