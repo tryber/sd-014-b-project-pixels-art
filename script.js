1f40b31555051e7b7008d73dcdcca6d96bafa05e
Grasiela Gomes <produtoradama@gmail.com>
2021-07-08 17:44:01 -0300

    clear button added

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-window.onload = () => {
-  function createPixel(value) {
-    const pixelBoard = document.querySelector('#pixel-board');
-    for (let i = 0; i < value; i += 1) {
-      const boardItem = document.createElement('div');
-      boardItem.className = 'pixel';
-      pixelBoard.appendChild(boardItem);
-    }
+function createPixel(value) {
+  const pixelBoard = document.querySelector('#pixel-board');
+  for (let i = 0; i < value; i += 1) {
+    const boardItem = document.createElement('div');
+    boardItem.className = 'pixel';
+    pixelBoard.appendChild(boardItem);
   }
-  createPixel(5 ** 2);
+}
+createPixel(5 ** 2);
 
-  const selectBlack = document.querySelector('.black');
-  selectBlack.classList.add('selected');
-};
+const selectBlack = document.querySelector('.black');
+selectBlack.classList.add('selected');
+
+function setPixelColor(color) {
+  document.addEventListener('click', (event) => {
+    if (event.target.classList.contains('pixel')) {
+      event.target.style.backgroundColor = color;
+    }
+  }, false);
+}
+setPixelColor('yellow');
+
+function clearColors() {
+  const pixels = document.querySelectorAll('.pixel');
+  const button = document.querySelector('#clear-board');
+  button.addEventListener('click', () => {
+    pixels.forEach((pixel) => {
+      pixel.style.backgroundColor = 'white';
+    });
+  });
+}
+clearColors();
