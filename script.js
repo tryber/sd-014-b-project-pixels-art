7e7cbd023136f022b644372ce1a27f78545b3401
Rafael Moraes - Turma 14 - Tribo B <rafael1moraes1rocha@gmail.com>
2021-07-08 18:28:47 -0300

    Projeto Pixels Art :rocket: requisito 9

--- script.js
+++ script.js
@@ -5,15 +5,25 @@ window.onload = function () {
     let colorClassSelected = document.querySelector('.selected');
     colorClassSelected.classList.remove('selected');
     event.target.classList.add('selected');
-    console.log(document.querySelector('.selected'))
+    console.log(document.querySelector('.selected'));
   });
 
   let tablePixel = document.querySelector('#pixel-board');
 
   tablePixel.addEventListener('click', function changePixelColor(event) {
     let colorClassSelected = document.querySelector('.selected');
-    event.target.style.backgroundColor = colorClassSelected.id
-    console.log(colorSelected)
-  })
+    event.target.style.backgroundColor = colorClassSelected.id;
+    console.log(colorClassSelected.id);
+  });
 
+  let button = document.querySelector('#clear-board');
+
+  button.addEventListener('click', function clearBoard() {
+    let pixelCells = document.querySelectorAll('.pixel');
+    for (let index = 0; index < pixelCells.length; index +=1 ) {
+      pixelCells[index].style.backgroundColor = 'white'
+    }
+  });
 }
+
+
