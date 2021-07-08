69253068aa59245b08246f67e33c0dea43a2fa01
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-07-08 17:54:00 -0300

    Requisito 7 Completo

--- script.js
+++ script.js
@@ -22,49 +22,33 @@ let yellowPalete = document.querySelector('#yellow');
 redPalete.addEventListener('click', changeSelectRed);
 
 function changeSelectRed() {
-  if (redPalete.classList.value === ('color selected')) {
-    redPalete.classList.remove('selected');
-  } else {
-    blackPalete.classList.remove('selected');
-    bluePalete.classList.remove('selected');
-    yellowPalete.classList.remove('selected');
-    redPalete.classList.add('selected');
-  }
+  blackPalete.classList.remove('selected');
+  bluePalete.classList.remove('selected');
+  yellowPalete.classList.remove('selected');
+  redPalete.classList.add('selected');
 }
 
 blackPalete.addEventListener('click', changeSelectBlack);
 function changeSelectBlack() {
-  if (blackPalete.classList.value === ('color selected')) {
-    blackPalete.classList.remove('selected');
-  } else {
-    redPalete.classList.remove('selected');
-    bluePalete.classList.remove('selected');
-    yellowPalete.classList.remove('selected');
-    blackPalete.classList.add('selected');
-  }
+  redPalete.classList.remove('selected');
+  bluePalete.classList.remove('selected');
+  yellowPalete.classList.remove('selected');
+  blackPalete.classList.add('selected');
 }
 
 bluePalete.addEventListener('click', changeSelectBlue);
 function changeSelectBlue() {
-  if (bluePalete.classList.value === ('color selected')) {
-    bluePalete.classList.remove('selected');
-  } else {
-    redPalete.classList.remove('selected');
-    blackPalete.classList.remove('selected');
-    yellowPalete.classList.remove('selected');
-    bluePalete.classList.add('selected');
-  }
+  redPalete.classList.remove('selected');
+  blackPalete.classList.remove('selected');
+  yellowPalete.classList.remove('selected');
+  bluePalete.classList.add('selected');
 }
 
 yellowPalete.addEventListener('click', changeSelectYellow);
 function changeSelectYellow() {
-  if (yellowPalete.classList.value === ('color selected')) {
-    yellowPalete.classList.remove('selected');
-  } else {
-    redPalete.classList.remove('selected');
-    blackPalete.classList.remove('selected');
-    bluePalete.classList.remove('selected');
-    yellowPalete.classList.add('selected');
-  }
+  redPalete.classList.remove('selected');
+  blackPalete.classList.remove('selected');
+  bluePalete.classList.remove('selected');
+  yellowPalete.classList.add('selected');
 }
 
