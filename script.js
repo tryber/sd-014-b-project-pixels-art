1b5385697b920866505f931636800a2d56368265
Vitor Lima <vitorliman@gmail.com>
2021-07-08 17:50:05 -0300

    #Requisito 3

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function generateRandomColor() {
   let randomColor = document.querySelectorAll('.color');
-  for(let index = 0; index < randomColor.length; index += 1) {
+  for (let index = 1; index < randomColor.length; index += 1) {
     let random = () => [Math.floor(Math.random() * 255)];
     let color = `rgb(${random()}, ${random()}, ${random()})`;
     randomColor[index].style.background = color;
+    randomColor[0].style.background = rgb(0,0,0);
   }
 }
 generateRandomColor();
