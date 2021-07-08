acfaa68c89971519218da12d5e24dc4fbc8b0ba6
Lucas Accurcio <lucas.accurcio@gmail.com>
2021-07-08 18:52:21 -0300

    Requisito 04

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ for (let i = 0; i < 4; i += 1) {
   boxPalheta.innerText = nroDoQuadro;
   containerPalhetas.appendChild(boxPalheta);
 }
+
+// Quadro branco
+const containerWhiteBoard = document.querySelector('#pixel-board');
+for (let i = 0; i < 5; i += 1) {
+  const lineBox = document.createElement('div');
+/*   lineBox.className = 'pixel';
+  lineBox.innerText = i + 1; */
+  containerWhiteBoard.appendChild(lineBox);
+  for (let ii = 0; ii < 5; ii += 1) {
+    const lineBox2 = document.createElement('div');
+    lineBox2.className = 'pixel';
+    //lineBox2.innerText = ii + 1;
+    lineBox2.style.color = 'white';
+    containerWhiteBoard.appendChild(lineBox2);
+  }
+}
