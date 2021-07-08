14ecf4005653a66053e9b8d2fb85b40af90fd7fb
Elithon Silva <elithonsilva@gmail.com>
2021-07-08 20:49:22 -0300

    Requisito-07 Finalizado

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const colorBlack = document.querySelector('.black');
 const colorYellow = document.querySelector('.yellow');
 const colorMagenta = document.querySelector('.magenta');
 const colorCyan = document.querySelector('.cyan');
-const matriz = document.querySelector('#pixel-board');
 
 function oneClick(event) {
   const selectedClass = document.querySelector('.selected');
