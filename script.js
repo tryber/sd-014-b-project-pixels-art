438a7f603d7578bf208ffa9f9f0162c9efba43ee
Israel S. Nazareth <israel.fab@hotmail.com>
2021-07-08 20:04:57 -0300

    Requisito 08

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // Requisito 07
-const black = document.querySelector('.black');
-const green = document.querySelector('.green');
-const blue = document.querySelector('.blue');
-const yellow = document.querySelector('.yellow');
+const black = document.querySelector('#black');
+const green = document.querySelector('#green');
+const blue = document.querySelector('#blue');
+const yellow = document.querySelector('#yellow');
+const pixels = document.querySelector('#pixel-board');
 
 function selectedColor(event) {
   const elemento = document.querySelector('.selected');
@@ -16,3 +17,8 @@ blue.addEventListener('click', selectedColor);
 yellow.addEventListener('click', selectedColor);
 
 // Requisito 08
+function changeBgColorPixel(event) {
+  const getColor = document.querySelector('.selected');
+  event.target.style.backgroundColor = getColor.id;
+};
+pixels.addEventListener('click', changeBgColorPixel)
