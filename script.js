de98e7554e93cd7b413041fb128895d26ca5becf
viniciussantana <viniciusantana@yahoo.com.br>
2021-07-08 17:04:03 -0300

    Requisito #9

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const color = document.querySelectorAll('.color');
 const pixel = document.querySelectorAll('.pixel');
+const button = document.getElementById('clear-board');
 
 function selected(event) {
   const select = document.querySelector('.selected');
@@ -10,11 +11,18 @@ for (let index = 0; index < color.length; index += 1) {
   color[index].addEventListener('click', selected);
 }
 
-function pixelColor(pixels) {
+function pixelColor(event) {
   const select = document.querySelector('.selected');
   const cor = select.id;
-  pixels.target.style.backgroundColor = cor;
+  event.target.style.backgroundColor = cor;
 }
 for (let index = 0; index < pixel.length; index += 1) {
   pixel[index].addEventListener('click', pixelColor);
 }
+
+function clearButton() {
+  for (let index = 0; index < pixel.length; index += 1) {
+    pixel[index].style.backgroundColor = 'white';
+  }
+}
+button.addEventListener('click', clearButton);
