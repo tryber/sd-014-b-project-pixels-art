77639a41c92bd0f83085856b788d82ffe27caba7
Levi Manoel <levimanoel.deob@gmail.br>
2021-07-08 16:02:40 -0300

    Requisitos 1 ao 4

---
+++ script.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+  createPixelBoard();
+}
+
+function createPixelBoard() {
+  for(let index = 0; index < 5; index += 1){
+    let tableRow = document.createElement('div');
+    tableRow.className = 'pixel tr';
+    document.querySelector('#pixel-board').appendChild(tableRow);
+    for(let indexB = 0; indexB < 5; indexB += 1){
+      let tableCell = document.createElement('div');
+      tableCell.className = 'pixel td'
+      document.querySelectorAll('.tr')[index].appendChild(tableCell);
+    }
+  }
+
+}
