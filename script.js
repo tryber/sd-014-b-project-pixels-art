4f21e1ec6b9a13a7f2b501a7c770203507cab3fb
samsantosb <samsantosb@outlook.com>
2021-07-08 19:14:26 -0300

    correçoes que meu amigão glauco me ajudou xD

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 window.onload = function () {
-  let colorsPallete = document.querySelectorAll('.color');
+  let c = document.querySelectorAll('.color');
 
-  for (let element of colorsPallete) {
-    if (colorsPallete[0] === element) {
-      element.style.backgroundColor = 'black';
-      element.classList.add('selected');
-    } else if (colorsPallete[1] === element) {
-      element.style.backgroundColor = 'grey';
-    } else if (colorsPallete[2] === element) {
-      element.style.backgroundColor = 'orange';
-    } else if (colorsPallete[3] === element) {
-      element.style.backgroundColor = 'pink';
+  for (let i of c) {
+    if (c[0] === i) {
+      i.style.backgroundColor = 'black';
+      i.classList.add('selected');
+    } else if (c[1] === i) {
+      i.style.backgroundColor = 'grey';
+    } else if (c[2] === i) {
+      i.style.backgroundColor = 'orange';
+    } else if (c[3] === i) {
+      i.style.backgroundColor = 'pink';
     }
-    element.addEventListener('click', function (event) {
-      for (let i = 0; i < colorsPallete.length; i += 1) {
-        colorsPallete[i].classList.remove('selected');
+    addEventListener('click', function (event) {
+      //percorre removendo todos os selecionados e selciona o seu
+      {
+        classList.remove('selected');
         event.target.classList.add('selected');
         for (let i = 1; i <= 25; i += 1) {
           pixel.classList.add('pixel');
@@ -24,19 +25,18 @@ window.onload = function () {
   }
 };
 
-let pixelBoard = document.querySelector('#pixel-board');
-pixelBoard.addEventListener('click', function (event) {
-  let alreadySelected =
-    document.querySelector('.selected').style.backgroundColor;
-  event.target.style.backgroundColor = alreadySelected;
+let pboard = document.querySelector('#pixel-board');
+pboard.addEventListener('click', function (event) {
+  let selectedd = document.querySelector('.selected').style.backgroundColor;
+  event.target.style.backgroundColor = selectedd;
 });
 
 const clear = () => {
   const totalDeleteLesgoTrybers = document.querySelector('#clear-board');
   totalDeleteLesgoTrybers.addEventListener('click', () => {
     const pxl = document.querySelectorAll('.pixel');
-    for (let index = 0; index < pxl.length; index += 1) {
-      pxl[index].style.backgroundColor = 'white';
+    for (let j = 0; j < pxl.length; j += 1) {
+      pxl[j].style.backgroundColor = 'white';
     }
   });
 };
