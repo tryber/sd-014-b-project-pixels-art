40e814d462bb50c2a67354f2fe62006517523042
Grasiela Gomes <produtoradama@gmail.com>
2021-07-08 15:27:19 -0300

    pixel board added

---
+++ script.js
@@ -0,0 +1,13 @@
+window.onload = () => {
+  function createPixel(value) {
+    const pixelBoard = document.querySelector('#pixel-board');
+    for (let i = 0; i < value; i += 1) {
+      const boardItem = document.createElement('div');
+      boardItem.className = 'pixel';
+      pixelBoard.appendChild(boardItem);
+    }
+  }
+  createPixel(5 ** 2);
+
+  const selectBlack = document.querySelector('.black');
+};
