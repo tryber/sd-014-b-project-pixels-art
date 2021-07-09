f769050cc642f3ff4c21dcceb1cc297abf593b06
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-07-08 22:34:34 -0300

    Requesito número 03 e 04.

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-let paletteOne = document.getElementsByClassName('color')[0];
+const paletteOne = document.getElementsByClassName('color')[0];
 paletteOne.style.background = 'black';
 
-let paletteTwo = document.getElementsByClassName('color')[1];
+const paletteTwo = document.getElementsByClassName('color')[1];
 paletteTwo.style.background = 'yellow';
 
-let paletteTree = document.getElementsByClassName('color')[2];
+const paletteTree = document.getElementsByClassName('color')[2];
 paletteTree.style.background = 'red';
 
-let paletteFour = document.getElementsByClassName('color')[3];
+const paletteFour = document.getElementsByClassName('color')[3];
 paletteFour.style.background = 'blue';
-
