b4ee6f4ab292574655c55a4799353eca8a17d4b7
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-07-09 00:36:38 -0300

    organizando a lógica para limpar os pixel todos de fundo branco

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const colorTwo = document.querySelector('#pink');
 const colorTree = document.querySelector('#green');
 const colorFour = document.querySelector('#purple');
 const whiteBox = document.querySelector('#pixel-board');
+const botao = document.querySelector('#clear-board');
+const arrayPixel = document.getElementsByClassName('pixel');
 
 function selectColor(event) {
   const corSelecionada = document.querySelector('.selected');
@@ -16,11 +18,13 @@ colorFour.addEventListener('click', selectColor);
 
 function colorir(event) {
   const corSelecionada = document.querySelector('.selected').id;
-  event.target.style.backgroundColor = corSelecionada;
+  const mudaCor = event.target;
+  mudaCor.style.backgroundColor = corSelecionada;
 }
 whiteBox.addEventListener('click', colorir);
 
-function clear(event) {
-  const botao = document.querySelector('#clear-board');
-
+function clear(event) {    
+  event.target.classList.add('.fundo-branco');    
 }
+botao.addEventListener('click', clear);
+console.log(arrayPixel);
