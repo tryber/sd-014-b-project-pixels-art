e7646cc475a2276e96e982478cb35d35c7849545
Nataniel Santos <nathan.santos159@hotmail.com>
2021-07-09 10:49:27 -0300

    requisito 11

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-window.onload = function(){
+window.onload = function () {
   btnBoardSize.addEventListener('click', setBoardSize);
   btnClearPixels.addEventListener('click', clearGrade);
-for (let color of colors) {
-  color.addEventListener('click', selectColor);
-}
-updatePixels()
-setBoardSize(5);
-}
-function updatePixels(){
+
+  for (let color of colors) {
+    color.addEventListener('click', selectColor);
+  }
+
+  updatePixels();
+  setBoardSize(5);
+};
+
+function updatePixels() {
   pixels = document.querySelectorAll('.pixel'); //atualizando os pixels porque a grade foi alterada
   for (let pixel of pixels) {
     pixel.addEventListener('click', paintPixel);
   }
 }
+
 let colors = document.getElementsByClassName('color');
 let pixels = document.querySelectorAll('.pixel');
 let btnClearPixels = document.getElementById('clear-board');
@@ -20,15 +24,23 @@ let inputBoardSize = document.getElementById('board-size');
 let btnBoardSize = document.getElementById('generate-board');
 let pixelBoardElement = document.querySelector('#pixel-board');
 
-
 function setBoardSize(number) {
-  number === 5
+  if(inputBoardSize.value < 5){
+    inputBoardSize.value = 5;
+  } else if(inputBoardSize.value > 50){
+    inputBoardSize.value = 50;
+  }
+
+  number === 5 
     ? (pixelBoardSize = number)
     : (pixelBoardSize = inputBoardSize.value);
-    while (pixelBoardElement.firstChild) { // referencia https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
-      pixelBoardElement.removeChild(pixelBoardElement.lastChild); //remove os filhos da table, ou seja, reinicializa a grade
-    }
-    if (pixelBoardSize >= 5 && pixelBoardSize <= 50) {
+
+    
+    if (pixelBoardSize !== "") {
+      while (pixelBoardElement.firstChild) {
+        // referencia https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
+        pixelBoardElement.removeChild(pixelBoardElement.lastChild); //remove os filhos da table, ou seja, reinicializa a grade, deixando-a vazia
+      }
       for (let i = 0; i < pixelBoardSize; i++) {
         let trElement = document.createElement('tr');
         for (let j = 0; j < pixelBoardSize; j++) {
@@ -36,16 +48,13 @@ function setBoardSize(number) {
           tdElement.className = 'pixel';
           trElement.appendChild(tdElement);
         }
-        pixelBoardElement.appendChild(trElement);
-      }
-      clearGrade()
-      updatePixels() //atualizando os pixels porque a grade foi alterada
-    } else {
-      alert('Board inválido!');
+      pixelBoardElement.appendChild(trElement);
     }
-    if(pixelBoardSize === ''){
-      alert('Board inválido!');
-    }
+    clearGrade();
+    updatePixels(); //atualizando os pixels porque a grade foi alterada
+  } else {
+    alert('Board inválido!');
+  }
 }
 
 function selectColor(selectedColor) {
@@ -64,10 +73,8 @@ function paintPixel(pixel) {
   pixel.target.style.backgroundColor = style.backgroundColor; // atribuindo a cor selecionada à cor de fundo do pixel clicado
 }
 
-
 function clearGrade() {
   for (let pixel of pixels) {
     pixel.style.backgroundColor = 'white';
   }
 }
-
