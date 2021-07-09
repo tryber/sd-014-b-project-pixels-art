02635c1ef78f2857d3e5ec6ace882a55076c659a
mf <marcelo.cunha1@outlook.com>
2021-07-09 15:34:50 -0300

    resolved issue 4

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-function getDivsColor () {
+function getDivsColor() {
   const getDivs = document.querySelectorAll('.color');
   getDivs[0].classList.add('black');
   getDivs[1].classList.add('red');
@@ -7,9 +6,18 @@ function getDivsColor () {
   getDivs[3].classList.add('green');
 }
 
-
+function creadPixelBoard() {
+  const div = document.createElement('div');
+  div.setAttribute('id', 'pixel-board');
+  document.querySelector('body').appendChild(div)
+  for (let i = 0; i < 25; i++) {
+    const y = document.createElement('div')
+    y.setAttribute('class', 'pixel');
+    document.querySelector('#pixel-board').appendChild(y);
+  }
+}
 
 window.onload = function () {
   getDivsColor()
+  creadPixelBoard()
 }
-
