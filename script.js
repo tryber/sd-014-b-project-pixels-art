f8975989432791cdfcb36c3705b88f57378a96f4
Henrique Almeida <joaoh.almeidaaraujo@gmail.com>
2021-07-09 10:42:52 -0300

    preparação para o requisito 10

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const colorPalette = document.querySelector('#color-palette');
 const pixelBoard = document.querySelector('#pixel-board');
 const clearButton = document.querySelector('#clear-board');
+// const sizeInput = document.querySelector('#board-size');
+// const generateBoardButton = document.querySelector('#generate-board');
 
 function createColor(rgbString) {
   const createElement = document.createElement('div');
@@ -9,14 +11,6 @@ function createColor(rgbString) {
   color.style.border = '1px solid black';
   color.setAttribute('class', 'color');
 }
-
-createColor('rgb(0, 0, 0)');
-
-createColor('rgb(250, 0, 0)');
-
-createColor('rgb(10, 50, 149)');
-
-createColor('rgb(0, 250, 075)');
 
 function createPixelBoard() {
   const setBoardWidth = 5;
@@ -32,14 +26,10 @@ function createPixelBoard() {
   }
 }
 
-createPixelBoard();
-
 function defaultColor() {
   const colors = document.querySelectorAll('.color');
   colors[0].classList.add('black', 'selected');
 }
-
-defaultColor();
 
 function selectColor(event) {
   const colors = document.querySelectorAll('.color');
@@ -49,14 +39,11 @@ function selectColor(event) {
   }
 }
 
-colorPalette.addEventListener('click', selectColor);
-
 function paintPixel(event) {
   const selectedColor = document.querySelector('.selected').style.backgroundColor;
+  //  eslint-disable-next-line no-param-reassign
   event.target.style.backgroundColor = selectedColor;
 }
-
-pixelBoard.addEventListener('click', paintPixel);
 
 function clearBoard() {
   const pixels = document.querySelectorAll('.pixel');
@@ -64,5 +51,21 @@ function clearBoard() {
     pixels[i].style.backgroundColor = 'white';
   }
 }
+
+createColor('rgb(0, 0, 0)');
+
+createColor('rgb(250, 0, 0)');
+
+createColor('rgb(10, 50, 149)');
+
+createColor('rgb(0, 250, 075)');
+
+createPixelBoard();
+
+defaultColor();
+
+colorPalette.addEventListener('click', selectColor);
+
+pixelBoard.addEventListener('click', paintPixel);
 
 clearButton.addEventListener('click', clearBoard);
