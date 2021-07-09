08bd1112b9db40506b926a9b9cd1e30f7db5e0de
Rodrigo Cavalheiro <oldcavalheiro@gmail.com>
2021-07-09 10:14:24 -0300

    8 e 9 resolvidos

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-let pixel = document.querySelectorAll('.pixel');
-let selected = document.querySelector('.selected');
+let pixel = document.querySelector('#pixel-board');
+// bug const select = document.querySelector('.selected'); bug bug bug
 
 let black = document.querySelector('#black');
 let red = document.querySelector('#red');
 let blue = document.querySelector('#blue');
 let orange = document.querySelector('#orange');
 
-
+// selecionando as cores
 function selectColor(event) {
+  // usar variavel select declarada buga tudo bug bug bug
   document.querySelector('.selected').classList.remove('selected');
   event.target.classList.add('selected');
 }
@@ -17,14 +18,22 @@ red.addEventListener('click', selectColor);
 blue.addEventListener('click', selectColor);
 orange.addEventListener('click', selectColor);
 
-/*
-document.addEventListener('click', function() {
-  console.log(pixel);
-  
-  
-  for (let index = 0; index < pixel.length; index += 1) {
-    pixel[index].style.backgroundColor = 'black';
+// aplicando as cores no pixel selecionado getComputedStyle currentStyle
+/* Unless you have directly defined the backgroundColor on the element itself
+   you have to use getComputedStyle() "stackoverflow" 
+   https://stackoverflow.com/questions/10556185/style-backgroundcolor-is-an-empty-string-in-javascript*/
+function selectPixel(event) {
+  event.target.style.backgroundColor = getComputedStyle(document.querySelector('.selected')).backgroundColor;
+}
+
+document.querySelector('#pixel-board').addEventListener('click', selectPixel);
+
+// botão clear
+let button = document.querySelector('#clear-board');
+let allPixels = document.querySelectorAll('.pixel');
+function clear() {
+  for (let index = 0; index < allPixels.length; index += 1){
+    allPixels[index].style.backgroundColor = 'white';
   }
-
-});*/
-
+}
+button.addEventListener('click', clear);
