46b99fa30195cf9dc3a97e2d530ac48f621390fb
Gustavo <gus.inacio@hotmail.com>
2021-07-09 00:04:57 -0300

    Requisito #9

--- script.js
+++ script.js
@@ -17,16 +17,44 @@ green.addEventListener('click', handleChangeSelected)
 black.addEventListener('click', handleChangeSelected);
 
 let getPixel = document.getElementsByClassName('pixel')
-for(let index = 0; index < getPixel.length; index +=1){
+for (let index = 0; index < getPixel.length; index += 1) {
     getPixel[index].addEventListener('click', setColor);
 }
-function setColor(event){
+function setColor(event) {
     let getColor = document.querySelector('.selected')
     let getBgColor = window.getComputedStyle(getColor).getPropertyValue("background-color")
     event.target.style.backgroundColor = getBgColor
 }
+function createCleanButton(name) {
+    let buttonContainer = document.getElementById('cleaner')
+    let button = document.createElement('button')
+    let buttonId = 'clear-board';
 
-/* Rerencias 
+    button.innerHTML = name;
+    button.id = buttonId;
+    buttonContainer.appendChild(button);
+}
+createCleanButton('Limpar');
+
+let cleanButton = document.getElementById("clear-board")
+cleanButton.addEventListener('click', cleanColors);
+function cleanColors() {
+    let bgWhite = document.querySelectorAll(".pixel")
+    for (let index = 0; index < bgWhite.length; index += 1) {
+        bgWhite[index].style.backgroundColor = "rgb(255,255,255)"
+    }
+}
+cleanColors();
+
+
+
+
+
+
+
+
+
+/* Rerencias
 https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
 
 https://www.w3schools.com/jsref/met_cssstyle_getpropertyvalue.asp*/
