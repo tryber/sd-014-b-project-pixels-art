9b20ff8496de0097ab9c7c63f03c1d673b274ad6
vinicius-mns <vinicius_mns@hotmail.com>
2021-07-09 00:08:23 -0300

    refatorando / comentando codgo

--- script.js
+++ script.js
@@ -1,52 +1,44 @@
-let select = document.querySelector('#color-palette').children
 
-let corUm = select[0].addEventListener('click', preto)
-
-let corDois = select[1].addEventListener('click', vermelho)
-
-let corTres = select[2].addEventListener('click', amarelo)
-
-let corQuatro = select[3].addEventListener('click', verde)
-
-function preto() {
-    select[0].classList.add ('selected')
-    select[1].classList.remove ('selected')
-    select[2].classList.remove ('selected')
-    select[3].classList.remove ('selected')
+//criando a paleta de cores
+for (let index = 0; index < 4; index +=1) {
+    let collorPalette = document.createElement('div');
+    collorPalette.className = 'color'
+    document.querySelector('#color-palette').appendChild(collorPalette)
 }
-function vermelho() {
-    select[0].classList.remove ('selected')
-    select[1].classList.add ('selected')
-    select[2].classList.remove ('selected')
-    select[3].classList.remove ('selected')
-}
-function amarelo() {
-    select[0].classList.remove ('selected')
-    select[1].classList.remove ('selected')
-    select[2].classList.add ('selected')
-    select[3].classList.remove ('selected')
-}
-function verde() {
-    select[0].classList.remove ('selected')
-    select[1].classList.remove ('selected')
-    select[2].classList.remove ('selected')
-    select[3].classList.add ('selected')
-}
+// adicionando classes com as respectivas cores
+// adicinando `selected` primeira div que contem a cor black
+let color = document.querySelectorAll('.color')
+    color[0].classList.add ('black', 'selected')
+    color[1].classList.add ('red')
+    color[2].classList.add ('yellow')
+    color[3].classList.add ('green')
+// limpa todas as classes `selected` das divs do (#color-palette) 
+    document.querySelector('#color-palette').addEventListener ('click', clearClass)
+        function clearClass() {
+            for ( let index = 0; index < color.length; index += 1) {
+                color[index].classList.remove ('selected')
+            }
+        }
+// adicinando a classe `selected` a div clickada 
+//usando o setTimeout para que a remocao nao entre em conflito com a adicao
+    for ( let index = 0; index < color.length; index += 1) {
+        color[index].addEventListener ('click', selecionar)
+        function selecionar() {
+            setTimeout(function () {
+                color[index].classList.add ('selected');
+            },10);
+        }
+    }
+    //setTimeout https://www.youtube.com/watch?v=RXWAZ0C_mds&ab_channel=AkshaySaini
+
+// nota : a remocao de todas as classes acontece ao mesmo tempo que a classe é adicionada a div determinada(clicada), entao usei o setTimeout para que a remocao aconteca primeio, e a adicao aconteca dps
 let setColor = window.getComputedStyle(document.querySelector('.selected')).getPropertyValue("background-color")
-
 let a = document.querySelectorAll('.pixel')
-
-
-
     for (let index = 0; index < a.length; index += 1) {
         a[index].addEventListener ('click', c)
-
         function c() {
-
             let setColor = window.getComputedStyle(document.querySelector('.selected')).getPropertyValue("background-color")
-
             a[index].style.backgroundColor = setColor;
-
         }
     }
 
