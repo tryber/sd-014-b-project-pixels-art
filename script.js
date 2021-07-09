7179968f34b00ad407eeae1bbb6772af6f90ebd7
Mathluiz23 <matheus_luizs@hotmail.com>
2021-07-08 22:26:10 -0300

    Requisito #7

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+window.onload = function() {
+
 let color1 = document.getElementsByClassName('color')[0];
 color1.style.backgroundColor = "black";
 
@@ -10,3 +12,18 @@ color3.style.backgroundColor = "green";
 let color4 = document.getElementsByClassName('color')[3];
 color4.style.backgroundColor = "yellow";
 
+color1.addEventListener("click" , recebeClick);
+color2.addEventListener("click" , recebeClick);
+color3.addEventListener("click" , recebeClick);
+color4.addEventListener("click" , recebeClick);
+
+function recebeClick(event) {
+  for (index = 0; index < 5; index +=1){
+    let corSelect = document.querySelector('.color');
+    if (corSelect[index] === document.querySelector('.selected')){
+      corSelect = corSelect[index].classList.remove('.selected');
+    } 
+  }
+  event.target.classList.add('.selected');
+}
+}
