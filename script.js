13a6c766ca70e7806b729924e0131841c447360c
miguel-alexandre <sauloj@protonmail.com>
2021-07-09 20:13:10 -0300

    Projeto pronto VII

--- script.js
+++ script.js
@@ -99,3 +99,14 @@ generateBoard.addEventListener('click', function () {
     buildBoard(number);
   }
 });
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.key === 'Enter') {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("generate-board").click();
+  }
+});
