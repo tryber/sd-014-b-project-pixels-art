ca7d4710029f756ca6879d7c5a98afda48678d8e
S4bot4 <sabotamail@gmail.com>
2021-07-09 13:19:20 -0300

    Cumprido requisitos 5 e 6

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ initialColors[0].style.backgroundColor = 'black';
 initialColors[1].style.backgroundColor = 'yellow';
 initialColors[2].style.backgroundColor = 'red';
 initialColors[3].style.backgroundColor = 'green';
+ 
