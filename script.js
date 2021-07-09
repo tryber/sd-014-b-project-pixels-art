05b011fec0e51c291c0e8381e6fee28cfce1815a
Adilson Gabriel <adilsongb.rabelo@gmail.com>
2021-07-09 10:06:11 -0300

    Corrigindo bugs :white_check_mark:

--- script.js
+++ script.js
@@ -21,13 +21,22 @@ function colorPixel(event) {
   event.target.style.backgroundColor = colorSelected;
 }
 
+function resetPalette() {
+  let pixels = document.querySelectorAll('.pixel');
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].style.backgroundColor = 'white';
+  }
+}
+
 function eventAdd() {
   let botoesCores = document.querySelectorAll('.color');
   let pixels = document.querySelectorAll('.pixel');
+  let botaoReset = document.getElementById('clear-board');
   for (let i = 0; i < botoesCores.length; i += 1) {
     botoesCores[i].addEventListener('click', selectColor);
   }
   for (let i = 0; i < pixels.length; i += 1) {
     pixels[i].addEventListener('click', colorPixel);
   }
+  botaoReset.addEventListener('click', resetPalette);
 }
