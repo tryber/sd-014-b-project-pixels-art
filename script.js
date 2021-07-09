6ca6234f1a5194c3bb81a88a6e089a095390fed5
samsantosb <samsantosb@outlook.com>
2021-07-08 21:40:55 -0300

    syntax changes ad css

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
+const query = document.querySelector.bind(document);
+const queryAll = document.querySelectorAll.bind(document);
 /*outras refs *
 Tabelas Display Flex: https://www.maujor.com/tutorial/anti-heroi-css-display-table.php
 Flexbox: https://www.w3schools.com/css/css3_flexbox.asp
 Javascript Arrow Functions: https://www.w3schools.com/js/js_arrow_function.asp/
 */
-
 //refs: script do aluno Glauco.
 //seleciona a palheta, aidicona um evento, seleciona a classe selected, no clique, remove do antigo e adiciona no novo.
 const select = () => {
-  document.querySelector('#color-palette').addEventListener('click', (e) => {
-    document.querySelector('.selected').classList.remove('selected');
+  query('#color-palette').addEventListener('click', (e) => {
+    query('.selected').classList.remove('selected');
     e.target.classList.add('selected');
   });
 };
 //com a função fill pegamos a cor selecionada na select e preenchemos um pixel presente no pixel board.
 const fill = () => {
-  document.querySelector('#pixel-board').addEventListener('click', (e) => {
-    e.target.style.backgroundColor =
-      document.querySelector('.selected').style.backgroundColor;
+  query('#pixel-board').addEventListener('click', (e) => {
+    e.target.style.backgroundColor = query('.selected').style.backgroundColor;
   });
 };
 //essa função limpa tudo - ref: https://www.codegrepper.com/code-examples/javascript/javascript+change+color+for+class+name
 const clear = () => {
-  document.querySelector('#clear-board').addEventListener('click', () => {
-    const pxl = document.querySelectorAll('.pixel');
+  query('#clear-board').addEventListener('click', () => {
+    const pxl = queryAll('.pixel');
     for (let i = 0; i < pxl.length; i += 1) {
       pxl[i].style.backgroundColor = 'white';
     }
