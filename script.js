b23fdc0ce1fe4092d55367b6c6157fffcb76110d
enzo <enzo@pop-os.localdomain>
2021-07-09 14:14:57 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -4,17 +4,22 @@ const yellow = document.getElementById('yellow');
 const red = document.getElementById('red');
 
 
-
-function changeClassColor (event) {
-    const mainColor = document.querySelector('.selected');
-    const selectedColor = event.target
-    if (selectedColor !== mainColor) {
-        mainColor.classList.remove('selected');
-        selectedColor.classList.add('selected');
-     }
+function changeClassColor(event) {
+  const mainColor = document.querySelector('.selected');
+  const selectedColor = event.target;
+  mainColor.classList.remove('selected');
+  selectedColor.classList.add('selected');
 }
 
 black.addEventListener('click', changeClassColor);
 blue.addEventListener('click', changeClassColor);
 yellow.addEventListener('click', changeClassColor);
 red.addEventListener('click', changeClassColor);
+
+const pixel = document.querySelectorAll('.pixel');
+for (let index = 0; index < pixel.length; index += 1){
+  pixel[index].addEventListener('click', function(event){
+    let color = document.querySelector('.selected');
+    event.target.style.backgroundColor = color.id
+  })
+} 
