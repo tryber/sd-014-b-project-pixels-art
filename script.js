883e73c07e272a6bbcd763c422fb734069c67812
bruholanda <brunaholandacunha7@gmail.com>
2021-07-08 21:58:05 -0300

    Requirement #7; Functions to add and remove the class selected;

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-window.onload = function () {
-  const loaded = document.getElementById('black').classList.add('selected');
+window.onload = document.getElementById('black').classList.add('selected');
+
+function clickRed () {
+  document.querySelector('#black').classList.remove('selected');
+  document.querySelector('#blue').classList.remove('selected');
+  document.querySelector('#green').classList.remove('selected');
+  document.querySelector('#red').classList.add('selected');
 }
 
+function clickBlack () {
+  document.querySelector('#red').classList.remove('selected');
+  document.querySelector('#blue').classList.remove('selected');
+  document.querySelector('#green').classList.remove('selected');
+  document.querySelector('#black').classList.add('selected');
+}
+
+function clickBlue () {
+  document.querySelector('#black').classList.remove('selected');
+  document.querySelector('#red').classList.remove('selected');
+  document.querySelector('#green').classList.remove('selected');
+  document.querySelector('#blue').classList.add('selected');
+}
+
+function clickGreen () {
+  document.querySelector('#black').classList.remove('selected');
+  document.querySelector('#blue').classList.remove('selected');
+  document.querySelector('#red').classList.remove('selected');
+  document.querySelector('#green').classList.add('selected');
+}
