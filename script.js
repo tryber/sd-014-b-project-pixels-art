9fc80f1271edf83bd81943bd03b7708760f984e3
Bruno Trindade <brunotrndd@gmail.com>
2021-07-09 00:09:30 -0300

    Requisito 8 e 9

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-window.onload = function () {
-  document.getElementById('black').classList.add('selected');
-};
+document.getElementById('black').classList.add('selected');
 
 const palletColors = document.querySelectorAll('.color');
+
+function checkPallet(array) {
+  const colorArray = array;
+  const color = ['black', 'blue', 'red', 'yellow'];
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] === palletColors[index]) {
+      colorArray[index].style.backgroundColor = color[index];
+    }
+  }
+}
+checkPallet(palletColors);
 
 function selectColor(event) {
   for (let index = 0; index < palletColors.length; index += 1) {
@@ -12,5 +21,25 @@ function selectColor(event) {
 }
 
 for (let index = 0; index < palletColors.length; index += 1) {
-  palletColors[index].addEventListener('click', selectColor, false);
+  palletColors[index].addEventListener('click', selectColor);
 }
+
+const pixels = document.querySelectorAll('.pixel');
+function paintPixel(event) {
+  const evento = event;
+  const selectedColor = document.querySelector('.selected').style.backgroundColor;
+  evento.target.style.backgroundColor = selectedColor;
+}
+
+for (let index = 0; index < pixels.length; index += 1) {
+  pixels[index].addEventListener('click', paintPixel);
+}
+
+function clearBoard() {
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].style.backgroundColor = 'white';
+  }
+}
+
+const clearButton = document.getElementById('clear-board');
+clearButton.addEventListener('click', clearBoard);
