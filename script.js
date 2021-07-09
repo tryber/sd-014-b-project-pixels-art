73d9a1ea5a110874e3b3c1845739d099c97a9366
Jean Marcel <jeanvanzella91@gmail.com>
2021-07-09 15:51:29 -0300

    ate a 6

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
-document.getElementsByClassName('color')[1].style.backgroundColor = 'red';
-document.getElementsByClassName('color')[2].style.backgroundColor = 'green';
-document.getElementsByClassName('color')[3].style.backgroundColor = 'blue';
+
+let corPreto = document.getElementsByClassName('color')[0]
+corPreto.style.backgroundColor = 'black';
+let corVermelho = document.getElementsByClassName('color')[1]
+corVermelho.style.backgroundColor = 'red';
+let corVerde = document.getElementsByClassName('color')[2]
+corVerde.style.backgroundColor = 'green';
+let corAzul = document.getElementsByClassName('color')[3]
+corAzul.style.backgroundColor = 'blue';
+
+corPreto.classList.add('selected');
