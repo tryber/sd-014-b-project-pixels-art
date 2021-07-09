1e577211c04f8dfa12b9a0d7cb7a8669a3ab788a
samsantosb <samsantosb@outlook.com>
2021-07-08 21:45:32 -0300

    sintaxe do JS melhorada

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+//helpers pra reduzir o tamanho da sintaxe.
 const query = document.querySelector.bind(document);
-const queryAll = document.querySelectorAll.bind(document);
+const queries = document.querySelectorAll.bind(document);
 /*outras refs *
 Tabelas Display Flex: https://www.maujor.com/tutorial/anti-heroi-css-display-table.php
+ainda referente a tabelas: https://stackoverflow.com/questions/29229523/how-and-why-to-use-display-table-cell-css
 Flexbox: https://www.w3schools.com/css/css3_flexbox.asp
 Javascript Arrow Functions: https://www.w3schools.com/js/js_arrow_function.asp/
 */
 //refs: script do aluno Glauco.
+
 //seleciona a palheta, aidicona um evento, seleciona a classe selected, no clique, remove do antigo e adiciona no novo.
 const select = () => {
   query('#color-palette').addEventListener('click', (e) => {
@@ -22,7 +25,7 @@ const fill = () => {
 //essa função limpa tudo - ref: https://www.codegrepper.com/code-examples/javascript/javascript+change+color+for+class+name
 const clear = () => {
   query('#clear-board').addEventListener('click', () => {
-    const pxl = queryAll('.pixel');
+    const pxl = queries('.pixel');
     for (let i = 0; i < pxl.length; i += 1) {
       pxl[i].style.backgroundColor = 'white';
     }
