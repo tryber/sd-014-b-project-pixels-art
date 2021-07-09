b2c6ee2349c881523886ca480dc8f8057210e650
Cezar <cezarguimaraes@gmail.com>
2021-07-09 12:01:30 -0300

    requisito 8 motherf...yeah \../

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let colorList = document.querySelectorAll(".color");
+let pixelList = document.querySelectorAll(".pixel");
 
 function addSelected(event){
    let getSelected = document.querySelector(".selected");
@@ -8,4 +9,14 @@ function addSelected(event){
 
 for(let index = 0; index < colorList.length; index +=1){
    colorList[index].addEventListener("click", addSelected);
+}
+
+function addColor(event){
+   let getSelected = document.querySelector(".selected");
+   let bgcSelected = getSelected.id
+   event.target.style.backgroundColor = bgcSelected;
+}
+
+for(let index = 0; index < pixelList.length; index +=1){
+   pixelList[index].addEventListener("click", addColor);
 }
