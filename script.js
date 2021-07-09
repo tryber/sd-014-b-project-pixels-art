c9ab08ef8940176214afad72c698826281393b40
bruholanda <brunaholandacunha7@gmail.com>
2021-07-09 13:01:58 -0300

    Requirement #9

--- script.js
+++ script.js
@@ -1,29 +1,63 @@
-window.onload = document.getElementById('black').classList.add('selected');
+window.onload = function teste() {
+  let select = document.getElementById('black');
+  select.classList.add('selected');
+}
 
-function clickRed () {
+function clickRed() {
   document.querySelector('#black').classList.remove('selected');
   document.querySelector('#blue').classList.remove('selected');
   document.querySelector('#green').classList.remove('selected');
-  document.querySelector('#red').classList.add('selected');
+  let select = document.querySelector('#red').classList.add('selected');
+  alert('vermelho');
 }
 
-function clickBlack () {
+function clickBlack() {
   document.querySelector('#red').classList.remove('selected');
   document.querySelector('#blue').classList.remove('selected');
   document.querySelector('#green').classList.remove('selected');
-  document.querySelector('#black').classList.add('selected');
+  let select = document.querySelector('#black').classList.add('selected');
+  alert('preto');
 }
 
-function clickBlue () {
+function clickBlue() {
   document.querySelector('#black').classList.remove('selected');
   document.querySelector('#red').classList.remove('selected');
   document.querySelector('#green').classList.remove('selected');
-  document.querySelector('#blue').classList.add('selected');
+  let select = document.querySelector('#blue').classList.add('selected');
+  alert('azul');
 }
 
-function clickGreen () {
+function clickGreen() {
   document.querySelector('#black').classList.remove('selected');
   document.querySelector('#blue').classList.remove('selected');
   document.querySelector('#red').classList.remove('selected');
-  document.querySelector('#green').classList.add('selected');
+  let select = document.querySelector('#green').classList.add('selected');
+  alert('verde');
 }
+
+const pixels = document.querySelectorAll('.pixel');
+
+function paint() {
+  
+}
+
+function cleaning() {
+  for (let index = 0; index < pixels.length; index++) {
+    pixels[index].style.backgroundColor = 'white';
+  }
+}
+
+const clickB = document.getElementById('black');
+clickB.addEventListener('click', clickBlack);
+
+const clickR = document.getElementById('red');
+clickR.addEventListener('click', clickRed);
+
+const clickBlu = document.getElementById('blue');
+clickBlu.addEventListener('click', clickBlue);
+
+const clickG = document.getElementById('green');
+clickG.addEventListener('click', clickGreen);
+
+const clean = document.getElementById('clear-board');
+clean.addEventListener('click', cleaning);
