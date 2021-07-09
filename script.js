87f16ec702d879568ff1ddb6e6d43d49e3d3063e
samsantosb <samsantosb@outlook.com>
2021-07-09 11:46:49 -0300

    consegui reduzir ainda mais o código

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-const q = document.querySelector.bind(document)
+const q = document.querySelector.bind(document);
 const qll = document.querySelectorAll.bind(document);
-const select = () => {
-  q('#color-palette').addEventListener('click', (e) => {
-    q('.selected').classList.remove('selected');
-    e.target.classList.add('selected');
-  });
-};
+const select = () => q('#color-palette').addEventListener('click', (e) => {
+  q('.selected').classList.remove('selected');
+  e.target.classList.add('selected');
+});
 const fill = () => q('#pixel-board').addEventListener('click', (e) =>
   e.target.style.backgroundColor = q('.selected').style.backgroundColor);
 const clear = () => q('#clear-board').addEventListener('click', () => {
-  for (const pxl of qll('.pixel')) pxl.style.backgroundColor = 'white';
+  for (const p of qll('.pixel')) p.style.backgroundColor = 'white';
 });
-select(); fill(); clear();
+select(), fill(), clear();
+
 /*Referencias do trabalho, fontes de estudo etc:
 
 Tabelas Display Flex: https://www.maujor.com/tutorial/anti-heroi-css-display-table.php
@@ -25,9 +24,9 @@ how to select divs https://medium.datadriveninvestor.com/how-to-select-all-div-e
 
 ____Funções____ fiz o máximo para reduzi-las e usar uma sintaxe mais moderna ___
 
-select: essa função é acionada com o click, quando clica-se remove selected do black e adiciona a classe selected a cor do clique.
+(s)elect: essa função é acionada com o click, quando clica-se remove selected do black e adiciona a classe selected a cor do clique.
 
-fill: essa função é acionada no click, ela preenche um target contido no pixelboard, aplicando o backgroundColor da cor selecionada.
+(f)ill: essa função é acionada no click, ela preenche um target contido no pixelboard, aplicando o backgroundColor da cor selecionada.
 
-clear: essa função é acionada no click, ela pega tods os pixels, percorre eles aplicando a cor white.
+(c)lear: essa função é acionada no click, ela pega tods os pixels, percorre eles aplicando a cor white.
 */
