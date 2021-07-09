9521963bff983b6e109d6ac0bc344ad4456fb4f6
Rodrigo Cavalheiro <oldcavalheiro@gmail.com>
2021-07-09 10:27:02 -0300

    resolvido diversos erros de eslint

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-let pixel = document.querySelector('#pixel-board');
 // bug const select = document.querySelector('.selected'); bug bug bug
 
-let black = document.querySelector('#black');
-let red = document.querySelector('#red');
-let blue = document.querySelector('#blue');
-let orange = document.querySelector('#orange');
+const black = document.querySelector('#black');
+const red = document.querySelector('#red');
+const blue = document.querySelector('#blue');
+const orange = document.querySelector('#orange');
 
 // selecionando as cores
 function selectColor(event) {
@@ -18,21 +17,21 @@ red.addEventListener('click', selectColor);
 blue.addEventListener('click', selectColor);
 orange.addEventListener('click', selectColor);
 
-// aplicando as cores no pixel selecionado getComputedStyle currentStyle
+// 8. aplicando as cores no pixel selecionado
 /* Unless you have directly defined the backgroundColor on the element itself
-   you have to use getComputedStyle() "stackoverflow" 
-   https://stackoverflow.com/questions/10556185/style-backgroundcolor-is-an-empty-string-in-javascript*/
+   you have to use getComputedStyle() "stackoverflow"
+   https://stackoverflow.com/questions/10556185/style-backgroundcolor-is-an-empty-string-in-javascript */
 function selectPixel(event) {
   event.target.style.backgroundColor = getComputedStyle(document.querySelector('.selected')).backgroundColor;
 }
 
 document.querySelector('#pixel-board').addEventListener('click', selectPixel);
 
-// botão clear
-let button = document.querySelector('#clear-board');
-let allPixels = document.querySelectorAll('.pixel');
+// 9. botão clear
+const button = document.querySelector('#clear-board');
+const allPixels = document.querySelectorAll('.pixel');
 function clear() {
-  for (let index = 0; index < allPixels.length; index += 1){
+  for (let index = 0; index < allPixels.length; index += 1) {
     allPixels[index].style.backgroundColor = 'white';
   }
 }
