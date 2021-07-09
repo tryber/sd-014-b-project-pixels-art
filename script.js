daf778645bb72a68ace5c3122d29eeb9cfe8f2bf
Victor Martins <victor.martins.duarte@gmail.com>
2021-07-09 13:05:15 -0300

    Dúvida sanada com o Iago, sobre um código para o requisito #7, e atualização do novo código para o requisito #7

--- script.js
+++ script.js
@@ -3,31 +3,28 @@
 //   let pathColor = document.querySelectorAll('.color');
 //   for (let content of pathColor) {
 //     content.addEventListener('click', function (event) {
-//       if (content.classList == 'selected') {
+//       if (content === event.target) {
 //         content.classList.remove('selected');
-//       }
-//       event.target.classList.add('selected');
+//       } else {
+//         event.target.classList.add('selected');
+//       }  
 //     })
 //     console.log(content.classList)
 //     console.log(content);
 //   }
 // }; changeClass();
-// *** Preciso de ajuda pra entender por que não está funcionando o 'remove' da function acima.
+// *** Preciso de ajuda pra entender por que não está funcionando o 'remove' da function acima. 
+// Monitoria do Iago - Conclusão: É necessário outro 'for' dentro do eventListener, porque ele só entra na parte do 'if' quando o evento 'click' acontece, daí confere se tem 'selected' e acaba atribuindo, ou retirando desse mesmo clicado, e depois o 'for' volta a percorrer, mas não entra no eventListener, porque não tem novos 'clicks'.
 
 function deleteSelected() {
   document.querySelector('.selected').classList.remove('selected')
 }
-function changeClass() {
-  let pathColor = document.querySelectorAll('.color');
-  for (let content of pathColor) {
-    content.addEventListener('click', function (event) {
-      deleteSelected();
-      if (event.target.classList != "selected") {
-        event.target.classList.add('selected');
-      } 
-    })   
-  }
-}; changeClass();
+for (let content of document.querySelectorAll('.color')) {
+  content.addEventListener('click', function (event) {
+    deleteSelected();
+    event.target.classList.add('selected');
+  })   
+}
 
 // *Requisito 8*
 const pixelBlocks = document.querySelectorAll('.pixel');
