e247a962fc8b5f564d5201dcb92e19835eb3fa0f
Mateus Gambaro <magambaro@hotmail.com>
2021-07-08 23:56:03 -0300

    Requisito #8

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.onload = function () {
 
+
 const black = document.getElementById('black');
 const green = document.getElementById('green');
 const red = document.getElementById('red');
@@ -14,6 +15,29 @@ function clickColor(event) {
 black.addEventListener('click', clickColor);
 green.addEventListener('click', clickColor);
 red.addEventListener('click', clickColor);
-blue.addEventListener('click', clickColor);   
+blue.addEventListener('click', clickColor); 
+
+document.querySelector(".pixel").classList.add("pixel-selected");
+
+let pixelSelected = document.querySelectorAll(".pixel");
+for (let index = 0; index < pixelSelected.length; index += 1) {
+  pixelSelected[index].addEventListener("click", pickPixel);
+  pixelSelected[index].addEventListener("click", changeColor);
+};
+function pickPixel(event) {
+  let pixelClick = document.querySelector(".pixel-selected");
+  pixelClick.classList.remove("pixel-selected");
+  event.target.classList.add("pixel-selected");
+}
+
+let changeColorPixel = document.querySelector(".pixel-selected")
+changeColorPixel.addEventListener("click", changeColor);
+
+
+function changeColor () {
+  let paleta = document.querySelector(".selected").id;
+  let pixel = document.querySelector(".pixel-selected")
+  pixel.style.backgroundColor = paleta;
+}
 
 }
