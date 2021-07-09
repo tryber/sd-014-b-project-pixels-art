226741fc25cd675137296f60ec80e71b1f7078a3
Vitor Luquini <vitorluquini2013@hotmail.com.br>
2021-07-09 15:45:25 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -7,4 +7,18 @@ function CreatePixelBoard(value) {
   }
 }
 
-CreatePixelBoard(25);
+function selectColor(event) {
+  const selected = document.querySelector('.selected');
+  selected.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+
+function Main() {
+  CreatePixelBoard(25);
+  const paletas = document.querySelectorAll('.color');
+  for (let i = 0; i < paletas.length; i += 1) {
+    paletas[i].addEventListener('click', selectColor);
+  }
+}
+
+Main();
