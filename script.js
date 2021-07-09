523fbe6e49dad0dd861d96d74313bce14f759d1c
samsantosb <samsantosb@outlook.com>
2021-07-09 01:03:37 -0300

    acho que por hoje é só

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-//helpers.
 const query = document.querySelector.bind(document);
 const queryAll = document.querySelectorAll.bind(document);
-//script
 const select = () => {
   query('#color-palette').addEventListener('click', (e) => {
     query('.selected').classList.remove('selected');
@@ -21,11 +19,11 @@ const clear = () =>
     for (let pxl of p) pxl.style.backgroundColor = 'white';
   });
 select(), fill(), clear();
-
 /*Referencias do trabalho
 Tabelas Display Flex: https://www.maujor.com/tutorial/anti-heroi-css-display-table.php
 ainda referente a tabelas: https://stackoverflow.com/questions/29229523/how-and-why-to-use-display-table-cell-css
 Flexbox: https://www.w3schools.com/css/css3_flexbox.asp
 Javascript Arrow Functions: https://www.w3schools.com/js/js_arrow_function.asp/
 operações com classlist https://developer.mozilla.org/pt-BR/docs/Web/API/Element/classList
+change backgrounds with js https://stackoverflow.com/questions/197748/how-do-i-change-the-background-color-with-javascript
 */
