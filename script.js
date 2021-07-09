45cd9082c925c1d5d09d07540558225cba7e16e1
Rafael Veiga <rafaelveig@hotmail.com>
2021-07-09 11:11:36 -0300

    requisito 8 completo

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ const greenDiv = document.getElementById('green');
 const caixaPixel = document.querySelector('#pixel-board');
 const corSelecionada = document.querySelector('.selected');
 const caixinhaPixel = document.querySelector('.pixel');
+const botaoLimpar = document.getElementById('#clear-board')
 
-
-function changeSelectedColor (event) {
-  let color = document.querySelector('.selected');
+function changeSelectedColor(event) {
+  const color = document.querySelector('.selected');
   color.classList.remove('selected');
   event.target.classList.add('selected');
 }
@@ -19,13 +19,14 @@ redDiv.addEventListener('click', changeSelectedColor);
 greenDiv.addEventListener('click', changeSelectedColor);
 
 function paintSquare(event){
-  event.target.style.backgroundColor = document.querySelector('.selected').id;
+  event.target.style.backgroundColor = document.querySelector ('.selected').id;
 }
-caixaPixel.addEventListener('click', paintSquare) 
+caixaPixel.addEventListener('click', paintSquare); 
 
-function createClearButton(){
-  let buttonID = document.querySelector('#clear-board');
-  let buttonIcon = document.createElement('button');
-  buttonIcon.id = buttonID;
- }
- 
+// function clearButton(){
+//   let caixaPintada = document.querySelectorAll('.pixel');
+//   for (let i=0; i=24; i++){
+//     caixaPintada[i].style.backgroundColor = 'white';
+//   }
+//  }
+//  botaoLimpar.addEventListener('click', clearButton)
