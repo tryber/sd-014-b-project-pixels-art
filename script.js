bde8f3e55d34de75c414b711dba0d2fd21ebf64f
samsantosb <samsantosb@outlook.com>
2021-07-09 11:14:47 -0300

    funçoes assincronas e redução da sintaxe

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const q = document.querySelector.bind(document);
 const qll = document.querySelectorAll.bind(document);
 const select = () => {
-  q('#color-palette').addEventListener('click', (e) => {
-    q('.selected').classList.remove('selected');
+  q('#color-palette').addEventListener('click', async (e) => {
+    await q('.selected').classList.remove('selected');
     e.target.classList.add('selected');
   });
 };
-const fill = () => q('#pixel-board').addEventListener('click', (e) =>
-  (e.target.style.backgroundColor = q('.selected').style.backgroundColor));
+const fill = () => q('#pixel-board').addEventListener('click', async (e) =>
+  await (e.target.style.backgroundColor = q('.selected').style.backgroundColor));
 const clear = () => q('#clear-board').addEventListener('click', () => {
-  const p = qll('.pixel');
-  for (let pxl of p) pxl.style.backgroundColor = 'white';
+  for (let p of qll('.pixel')) p.style.backgroundColor = 'white';
 });
 select(), fill(), clear();
 /*Referencias do trabalho
