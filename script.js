be96c4b7eabc5d08ea9ac88af6e048254482df88
Cezar <cezarguimaraes@gmail.com>
2021-07-09 11:21:29 -0300

    requisito 7 done

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let colorList = document.querySelectorAll(".color");
 
 function addSelected(event){
-    let getSelected = document.querySelector(".selected");
-    getSelected.classList.remove(".delected");
-    event.target.classList.add(".selected");
+   let getSelected = document.querySelector(".selected");
+   getSelected.classList.remove("selected");
+   event.target.classList.add("selected");
 }
 
-for(let index = 0; index < color.length; index =+1){
-    colorList[index].addEventListener("click", addSelected);
+for(let index = 0; index < colorList.length; index +=1){
+   colorList[index].addEventListener("click", addSelected);
 }
