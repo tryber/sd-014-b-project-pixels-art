820fc9b81153c0ddcf6d52ce2fd51c7b7d820269
Leomar Linhares <leomar_linhares@hotmail.com>
2021-07-08 23:57:58 -0300

    Atende às exigências do lint :rocket:

--- scripts/moreColors.js
+++ scripts/moreColors.js
@@ -1,5 +1,5 @@
 const newColorSelector = document.querySelector('#new-color');
-newColorSelector.addEventListener('change', function (event) {
-  let selected = document.querySelector('.selected');
+newColorSelector.addEventListener('change', (event) => {
+  const selected = document.querySelector('.selected');
   selected.style.backgroundColor = event.target.value;
-})
+});
