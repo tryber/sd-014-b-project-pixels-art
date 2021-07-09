60d2327fa37708c793e32435d6627b2cd5a98269
Adilson Gabriel <adilsongb.rabelo@gmail.com>
2021-07-08 22:52:27 -0300

    Requisito 6 - 9 :white_check_mark:

--- script.js
+++ script.js
@@ -1,11 +1,56 @@
+/* Variaveis globais */
 
-window.onload = carregarPaleta;
+let corSelected = 'rgb(0,0,0)';
 
-function carregarPaleta() {
+/* ********* */
 
-  document.querySelectorAll('.color')[0].style.backgroundColor = 'rgb(0,0,0)';
-  document.querySelectorAll('.color')[1].style.backgroundColor = 'rgb(227,113,113)';
-  document.querySelectorAll('.color')[2].style.backgroundColor = 'rgb(55,154,234)';
-  document.querySelectorAll('.color')[3].style.backgroundColor = 'rgb(226,232,81)';
+window.onload = carregarApp;
+
+function carregarApp() {
+
+  let botoesCores = document.querySelectorAll('.color');
+  let pixels = document.querySelectorAll('.pixel');
+  let botaoReset = document.getElementById('clear-board');
+  
+  /* Cores inicias */
+
+  botoesCores[0].style.backgroundColor = 'rgb(0,0,0)';
+  botoesCores[1].style.backgroundColor = 'rgb(227,113,113)';
+  botoesCores[2].style.backgroundColor = 'rgb(55,154,234)';
+  botoesCores[3].style.backgroundColor = 'rgb(226,232,81)';
+
+  /* Eventos de seleção do botão e da cor */
+  for (let i = 0; i < botoesCores.length; i += 1) {
+
+    botoesCores[i].addEventListener('click', function () {
+      
+      for (let k = 0; k < botoesCores.length; k += 1) {
+        botoesCores[k].classList.remove('selected');  
+      }
+
+      botoesCores[i].classList.add('selected');
+
+      corSelected = window.getComputedStyle(botoesCores[i], null).getPropertyValue("background-color");
+      console.log(corSelected);
+
+    })
+
+  }
+
+  /* Eventos de pintura na tabela */
+  for (let i = 0; i < pixels.length; i += 1) {
+
+    pixels[i].addEventListener('click', function () {
+      pixels[i].style.backgroundColor = corSelected;
+    })
+
+  }
+
+  /* Ferramentas - Botão de resetar tudo pra branco */
+  botaoReset.addEventListener('click', function () {
+    for (let i = 0; i < pixels.length; i += 1) {
+      pixels[i].style.backgroundColor = 'rgb(255,255,255)';
+    }
+  })
 
 }
