7ce3439fa9f3110753947bd45e39f646930ed32e
samsantosb <samsantosb@outlook.com>
2021-07-09 01:24:44 -0300

    continuando no desafio da sintaxe minimalista

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
-const query = document.querySelector.bind(document);
-const queryAll = document.querySelectorAll.bind(document);
+const q = document.querySelector.bind(document);
+const qll = document.querySelectorAll.bind(document);
 const select = () => {
-  query('#color-palette').addEventListener('click', (e) => {
-    query('.selected').classList.remove('selected');
+  q('#color-palette').addEventListener('click', (e) => {
+    q('.selected').classList.remove('selected');
     e.target.classList.add('selected');
   });
 };
-const fill = () =>
-  query('#pixel-board').addEventListener(
-    'click',
-    (e) =>
-      (e.target.style.backgroundColor =
-        query('.selected').style.backgroundColor)
-  );
-const clear = () =>
-  query('#clear-board').addEventListener('click', () => {
-    const p = queryAll('.pixel');
-    for (let pxl of p) pxl.style.backgroundColor = 'white';
-  });
+const fill = () => q('#pixel-board').addEventListener('click', (e) =>
+  (e.target.style.backgroundColor = q('.selected').style.backgroundColor));
+const clear = () => q('#clear-board').addEventListener('click', () => {
+  const p = qll('.pixel');
+  for (let pxl of p) pxl.style.backgroundColor = 'white';
+});
 select(), fill(), clear();
 /*Referencias do trabalho
 Tabelas Display Flex: https://www.maujor.com/tutorial/anti-heroi-css-display-table.php
@@ -26,4 +20,11 @@ Flexbox: https://www.w3schools.com/css/css3_flexbox.asp
 Javascript Arrow Functions: https://www.w3schools.com/js/js_arrow_function.asp/
 operações com classlist https://developer.mozilla.org/pt-BR/docs/Web/API/Element/classList
 change backgrounds with js https://stackoverflow.com/questions/197748/how-do-i-change-the-background-color-with-javascript
+
+
+select: essa função é acionada com o click, quando clica-se remove selected do black e adiciona a classe selected a cor do clique.
+
+fill: essa função é acionada no click, ela preenche um target contido no pixelboard, aplicando o backgroundColor da cor selecionada.
+
+clear: essa função é acionada no click, ela pega tods os pixels, percorre eles aplicando a cor white.
 */
