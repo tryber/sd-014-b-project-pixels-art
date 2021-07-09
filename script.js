65f02b7bbe6b20cce12523288922eb564c655fc4
Juliana Braga <juliana4braga@gmail.com>
2021-07-08 21:51:18 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -9,11 +9,6 @@ function makeFramePixel() {
 }
 */
 
-let pixel = document. querySelectorAll("#pixel-board td");
-for (let index = 0; index < pixel.length; index += 1) {
-  pixel[index].classList.add("pixel");
-}
-
 document.getElementById("pixel-board").style.marginLeft = "10px";
 
 document.getElementById("black").classList.add("selected");
@@ -33,5 +28,30 @@ function pickColor (event) {
   let colorWithSelected = document.querySelector(".selected");
   colorWithSelected.classList.remove("selected");
   event.target.classList.add("selected");
-  console.log(event.target);
 }
+
+// Cria função para fazer armezanar o pixel que está selecionado (mesma lógica da pickColor) 
+
+document.querySelector(".pixel").classList.add("pixel-selected");
+
+let pixelSelected = document.querySelectorAll(".pixel");
+for (let index = 0; index < pixelSelected.length; index += 1) {
+  pixelSelected[index].addEventListener("click", pickPixel);
+  pixelSelected[index].addEventListener("click", changeColor);
+};
+function pickPixel(event) {
+  let pixelClick = document.querySelector(".pixel-selected");
+  pixelClick.classList.remove("pixel-selected");
+  event.target.classList.add("pixel-selected");
+}
+
+let changeColorPixel = document.querySelector(".pixel-selected")
+changeColorPixel.addEventListener("click", changeColor);
+
+
+function changeColor () {
+  let paleta = document.querySelector(".selected").id;
+  let pixel = document.querySelector(".pixel-selected")
+  pixel.style.backgroundColor = paleta;
+}
+
