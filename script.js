9c968fd0e93daa644fcfe6e50407d8a7886c1d0b
DevJunior21 <junior-wmw@hotmail.com>
2021-07-09 16:44:02 -0400

    adicionado o requisito 8, manipulação da classe selected

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-function pintar (){
-    
-    
+let blackcolor = document.getElementsByClassName('color')[0] // elemento da cor preto
+let bluecolor = document.getElementsByClassName('color')[1] // elemento da cor azul
+let yellowcolor = document.getElementsByClassName('color')[2] // cor amarela
+let redcolor = document.getElementsByClassName('color') [3] // cor vermelha
 
+function pintapinto(event){
+    let select = document.querySelector('.selected')
+    select.classList.remove('selected')
+    event.target.classList.add('selected')
 }
-    
+blackcolor.addEventListener('click', pintapinto)
+bluecolor.addEventListener('click', pintapinto)
+yellowcolor.addEventListener('click', pintapinto)
+redcolor.addEventListener('click', pintapinto)
+
+
+
+
 
