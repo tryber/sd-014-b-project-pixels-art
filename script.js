52da51ec045cadafeebf23a89bf92dfaed108ff6
mf <marcelo.cunha1@outlook.com>
2021-07-09 16:16:44 -0300

    resolved issue 6

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getDivsColor() {
   const getDivs = document.querySelectorAll('.color');
-  getDivs[0].classList.add('black');
+  getDivs[0].classList.add('black', 'selected');
   getDivs[1].classList.add('red');
   getDivs[2].classList.add('blue');
   getDivs[3].classList.add('green');
