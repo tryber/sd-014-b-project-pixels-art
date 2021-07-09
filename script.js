66906a4ab5704a8b8536bba2ee05b02ef1f439ae
Sergio_Rodrigues <rodrigues.sergiofilho@gmail.com>
2021-07-08 21:05:13 -0300

    Requisito #9

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-let divs = document.getElementsByClassName('color');
-for (let index of divs) {
-    index.addEventListener('click', selectColor);
-}
-
+//#7
 function selectColor(event) {
   let color = document.querySelector('.selected');
   color.classList.remove('selected');
   event.target.classList.add('selected');
 }
 
+let divs = document.getElementsByClassName('color');
+for (let index of divs) {
+    index.addEventListener('click', selectColor);
+}
+//#8
 function setColor(event) {
   let color = document.querySelector('.selected');
   event.target.style.backgroundColor = color.id;
@@ -16,3 +17,14 @@ function setColor(event) {
 
 let table = document.getElementById('pixel-board');
 table.addEventListener('click', setColor);
+
+//#9
+function clearTable() {
+  let pixel = document.getElementsByClassName('pixel');
+  for (let index of pixel) {
+    index.style.backgroundColor = 'white';
+  }
+}
+
+let button = document.getElementById('clear-board');
+button.addEventListener('click', clearTable);
