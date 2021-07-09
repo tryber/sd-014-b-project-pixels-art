5d603a2791db725a490aa47c0bae2e3169535a9b
dsmarcelo <celoygm@gmail.com>
2021-07-08 22:04:48 -0300

    #2-5

---
+++ script.js
@@ -0,0 +1,7 @@
+const colorPalette = document.getElementById('color-palette')
+const pixelBoard = document.getElementById('pixel-board')
+const box1 = document.getElementById('box1');
+const box2 = document.getElementById('box2');
+const box3 = document.getElementById('box3');
+const box4 = document.getElementById('box4');
+
