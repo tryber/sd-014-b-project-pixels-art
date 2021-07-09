2c45c0e672b4c429e48332d7c40b640653d26b32
miguel-alexandre <sauloj@protonmail.com>
2021-07-09 18:53:56 -0300

    Projeto pronto VI

--- script.js
+++ script.js
@@ -1,115 +1,101 @@
-let boardSizeLoaded = 5;
+const divColors = document.querySelector('#color-palette');
+const color1 = document.querySelector('.color1');
+const color2 = document.querySelector('.color2');
+const color3 = document.querySelector('.color3');
+const boardSize = document.querySelector('#board-size');
+const generateBoard = document.querySelector('#generate-board');
+const cleaner = document.querySelector('#clear-board');
+const board = document.querySelector('#pixel-board');
+let selectedDiv = document.querySelector('.selected');
+let selectedColor;
 
-function removeBoard() {
-  const pixelBoard = document.getElementsByClassName('pixel-board')[0];
-  pixelBoard.innerHTML = '';
-  
+//  FUNCTIONS
+//  Função responsável por remover classe 'selected' da cor selecionada no momento
+function clearSelection() {
+  selectedDiv.classList.remove('selected');
 }
-
-// add width and height in pixelboard
-function addPropertyInPixelBoard(size) {
-  const pixelBoard = document.getElementsByClassName('pixel-board')[0];
-
-  pixelBoard.style.width = `${40 * size}px`;
-  pixelBoard.style.height = `${40 * size}px`;
+//  Função responsável por adicionar classe 'selected' ao elemento selecionado
+function addSelection(target) {
+  target.classList.add('selected');
+  selectedDiv = document.querySelector('.selected');
 }
-
-// add property in div
-function addProperty(div) {
-  div.id = 'pixel';
-  div.className = 'pixel';
-  div.style.backgroundColor = 'white';
-  div.addEventListener('click', function (event) {
-    event.target.style.backgroundColor = colorCurrent;
-  });
+//  Função Responsável por pegar a cor da div selecionada
+function getColor() {
+  const property = window.getComputedStyle(selectedDiv, null).getPropertyValue('background-color');
+  return property;
 }
-
-// generate board
-function generateBoard() {
-  let boardSize = boardSizeLoaded * boardSizeLoaded;
-
-  const pixelBoard = document.getElementsByClassName('pixel-board')[0];
-
-  // add width and height in pixelboard
-  addPropertyInPixelBoard(boardSizeLoaded);
-
-  for (let index = 1; index <= boardSize; index += 1) {
-    let div =document.createElement('div');
-    addProperty(div);
-    pixelBoard.appendChild(div);
-  }
+//  Função responsável por pintar pixel
+function paint(target) {
+  target.style.backgroundColor = selectedColor;
 }
-generateBoard();
-
-const inputBoard = document.getElementById('board-size');
-const btn = document.getElementById('generate-board');
-btn.addEventListener('click', function () {
-  if (inputBoard.value == '') {
-    alert('Board inválido!');
-  } else {
-    boardSizeLoaded = inputBoard.value;
-    if (inputBoard.value > 50) boardSizeLoaded = 50;
-    if (inputBoard.value < 5) boardSizeLoaded = 5;
-    removeBoard();
-    generateBoard();
-  }
-});
-
-const colorsPallet = {
- 
-  color1: '#000000', //Preto
-  color2: '#F11D00', //Vermelho
-  color3: '#2FC18C', //Verde
-  color4: '#54BCEA', //Azul
-  
-}
-
-let colorCurrent = colorsPallet.color1;
-
-const colors = document.getElementsByClassName('color');
-
-let selectedCurrent = document.getElementsByClassName('selected')[0];
-
-function removeClassSelected() {
-  let classes = selectedCurrent.className.split(' ');
-  selectedCurrent.className = `${classes[0]} ${classes[1]}`;
-}
-
-function addClassSelected(colorButton) {
-  colorButton.className += ' selected';
-  colorCurrent = colorsPallet[`${colorButton.id}`];
-  selectedCurrent = colorButton;
-}
-
-for (let index = 0; index < colors.length; index += 1) {
-  colors[index].addEventListener('click', function (event) {
-    if (selectedCurrent) removeClassSelected();
-    addClassSelected(event.target);
-  });
-}
-
-const pixels = document.getElementsByClassName('pixel');
-
-// limpar board
-const clearBoard = document.getElementById('clear-board');
-clearBoard.addEventListener('click', function () {
+//  Função responsável por limpar a board
+function clearBoard() {
+  const pixels = document.querySelectorAll('.pixel');
   for (let index = 0; index < pixels.length; index += 1) {
-    pixels[index].style.backgroundColor = 'white';
-    
+    pixels[index].style.backgroundColor = '';
   }
+}
+//  Função responsável por criar rows da matriz
+function buildRow(number) {
+  const divRow = document.createElement('div');
+  divRow.className = 'row';
+  for (let index = 0; index < number; index += 1) {
+    const divCell = document.createElement('div');
+    divCell.className = 'pixel';
+    divRow.appendChild(divCell);
+  }
+  board.appendChild(divRow);
+}
+//  Função responsável por construir board
+function buildBoard(number) {
+  board.innerHTML = '';
+  for (let index = 0; index < number; index += 1) {
+    buildRow(number);
+  }
+}
+function generateColor() { // Inspirado em código achado no seguint url (https://stackoverflow.com/questions/1152024/best-way-to-generate-a-random-color-in-javascript/1152508)
+  const first = Math.floor(Math.random() * 256);
+  const second = Math.floor(Math.random() * 256);
+  const third = Math.floor(Math.random() * 256);
+  return `rgb(${first}, ${second}, ${third})`;
+}
+//  DEFINIÇÕES PÓS FUNÇÕES
+selectedColor = getColor(selectedDiv);
 
+//  EVENTOS
+//  Gerando cores
+window.onload = function () {
+  color1.style.backgroundColor = generateColor();
+  color2.style.backgroundColor = generateColor();
+  color3.style.backgroundColor = generateColor();
+};
+//  Adicionando evento de clique nas cores da paleta
+divColors.addEventListener('click', function (event) {
+  clearSelection();
+  addSelection(event.target);
+  selectedColor = getColor();
 });
-
-let input = document.getElementById("board-size");
-
-// Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function(event) {
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.key === 'Enter') {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    document.getElementById("generate-board").click();
+//  Adicionando evento de clique na pixel-board
+board.addEventListener('click', function (event) {
+  paint(event.target);
+});
+// Adicionando evento de clique no botão limpar
+cleaner.addEventListener('click', function () {
+  clearBoard();
+});
+//  Adicionando evento de clique no botão generate board
+generateBoard.addEventListener('click', function () {
+  let number = 0;
+  if (boardSize.value === '') {
+    alert('Board inválido!');
+  } else if (boardSize.value < 5) {
+    number = 5;
+    buildBoard(number);
+  } else if (boardSize.value > 50) {
+    number = 50;
+    buildBoard(number);
+  } else {
+    number = boardSize.value;
+    buildBoard(number);
   }
 });
-
