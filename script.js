65174fe41c68cc8da9b0fc349c1073ee1a3cf913
samsantosb <samsantosb@outlook.com>
2021-07-09 12:00:20 -0300

    mais reduções, comentários etc

--- script.js
+++ script.js
@@ -13,11 +13,11 @@ select(), fill(), clear();
 
 /*Referencias do trabalho, fontes de estudo etc:
 
-Tabelas Display Flex: https://www.maujor.com/tutorial/anti-heroi-css-display-table.php
-ainda referente a tabelas: https://stackoverflow.com/questions/29229523/how-and-why-to-use-display-table-cell-css
+Tables Display Flex: https://www.maujor.com/tutorial/anti-heroi-css-display-table.php
+-: https://stackoverflow.com/questions/29229523/how-and-why-to-use-display-table-cell-css
 Flexbox: https://www.w3schools.com/css/css3_flexbox.asp
 Javascript Arrow Functions: https://www.w3schools.com/js/js_arrow_function.asp/
-operações com classlist https://developer.mozilla.org/pt-BR/docs/Web/API/Element/classList
+classlist https://developer.mozilla.org/pt-BR/docs/Web/API/Element/classList
 change backgrounds with js https://stackoverflow.com/questions/197748/how-do-i-change-the-background-color-with-javascript
 assync fuctions https://developers.google.com/web/fundamentals/primers/async-functions#:~:text=If%20you%20use%20the%20async,the%20rejected%20value%20is%20thrown.
 how to select divs https://medium.datadriveninvestor.com/how-to-select-all-div-elements-on-a-page-using-javascript-9b2cd16af740#:~:text=How%20to%20Select%20All%20Elements%20with%20JavaScript,name%2C%20i.e.%20.&text=Calling%20document.
