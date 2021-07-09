6fb6db0b4388c8c9f581805e8343770a0c83e9c3
FelipeMartins <mfelipeluiz99@gmail.com>
2021-07-09 15:16:02 -0300

    Reafatoraçao para passar no crypres, Requisito #6 e #7

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-/* let classPixel = document.getElementsByClassName('pixel');
+const blackColor = document.querySelector('.black');
+const redColor = document.querySelector('.red');
+const greenColor = document.querySelector('.green');
+const blueColor = document.querySelector('.blue');
 
-classPixel.addeventlistener('click' function (mudaCor){
+blackColor.addEventListener('click', escolheCor);
+redColor.addEventListener('click', escolheCor);
+greenColor.addEventListener('click', escolheCor);
+blueColor.addEventListener('click', escolheCor);
 
-}) */
-function cores() {}
+function escolheCor(event) {
+  const selected = document.querySelector('.selected');
+  selected.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+
+let tabela = document.querySelectorAll('#pixel-board');
+let tagPixel = document.getElementsByClassName('pixel');
+function pintaCor() {
+  for (let i = 0; i < tagPixel.length; i++) {
+    let cor = tagPixel[i];
+    console.log(cor);
+    console.log(tagPixel);
+    cor.addEventListener('click', function () {
+      let selected = document.querySelector('.selected');
+      cor.style.backgroundColor = selected.style.backgroundColor;
+    });
+  }
+}
+pintaCor();
