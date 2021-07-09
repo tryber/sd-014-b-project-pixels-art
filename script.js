9a7f5d83fa6b92d6b0138c38fa12a63287d3ddac
S4bot4 <sabotamail@gmail.com>
2021-07-09 01:07:13 -0300

    Cumprido requisito 2 e 3

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-const paletteColors = document.getElementsByClassName('color');
-paletteColors
-console.log(paletteColors);
+let initialColors = document.getElementsByClassName('color');
+initialColors[0].style.backgroundColor = 'black';
+initialColors[1].style.backgroundColor = 'yellow';
+initialColors[2].style.backgroundColor = 'red';
+initialColors[3].style.backgroundColor = 'green';
