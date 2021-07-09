51c2c8748e40cca88695f3c534a4b1adbafb3427
dsmarcelo <celoygm@gmail.com>
2021-07-09 17:18:35 -0300

    Requisitos 6 e 7

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const colorPalette = document.getElementById('color-palette')
-const pixelBoard = document.getElementById('pixel-board')
-const box1 = document.getElementById('box1');
-const box2 = document.getElementById('box2');
-const box3 = document.getElementById('box3');
-const box4 = document.getElementById('box4');
+// const colorPalette = document.getElementById('color-palette');
+// const pixelBoard = document.getElementById('pixel-board');
+// document.onload = function addSelectedBox1() {
+//   const box1 = document.getElementById('box1');
+//   box1.classList.add = 'selected';
+// };
 
+const paletteColors = document.querySelectorAll('.color');
+console.log(paletteColors.length);
+
+function addClassSelected(event) {
+  const lastColor = document.querySelector('.selected');
+  lastColor.classList.remove('selected');
+  event.target.classList.add('selected');
+  console.log(event);
+}
+
+for (let i = 0; i < paletteColors.length; i += 1) {
+  paletteColors[i].addEventListener('click', addClassSelected);
+}
+
+function getColor () {
+  let currentPalette = document.querySelector('.selected')
+  let currentColor = window.getComputedStyle(currentPalette).getPropertyValue("background-color")
+}
