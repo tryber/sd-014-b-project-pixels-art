0607721a32bf64af62fecb2fdde0cbe037ee399f
Vitor Lima <vitorliman@gmail.com>
2021-07-09 12:59:11 -0300

    #Requisito 9

--- script.js
+++ script.js
@@ -9,9 +9,26 @@ function generateRandomColor() {
 }
 generateRandomColor();
 
+let color = 'black';
 const board = document.querySelector('#color-palette');
 board.addEventListener('click', function(event) {
   const colorSelected = document.querySelector('.selected');
   colorSelected.classList.remove('selected');
   event.target.classList.add('selected');
+  color = event.target.style.background;
+})
+
+const colorTable = document.querySelector('#pixel-board');
+colorTable.addEventListener('click', function(event) {
+  if (event.target.classList.value.includes('pixel')) {
+    event.target.style.background = color;
+  }
+})
+
+const button = document.querySelector('#clear-board');
+button.addEventListener('click', function() {
+  let pixel = document.querySelectorAll('.pixel');
+  for (let index = 0; index < pixel.length; index += 1) {
+    pixel[index].style.background = 'white';
+  }
 })
