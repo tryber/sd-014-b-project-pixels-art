63097f3ce262fd70762ed05daa23c66d3e0f8d68
Thiago Gomez <thiagogomez2906@hotmail.com>
2021-07-09 10:54:32 -0300

    correçao 1

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 let linha =document.querySelectorAll(".line");
-
+var brek=document.createElement("br");
+let cet=linha[index];
 selectedCor();
 readline(linha);
-function readline(linha){
-    for(let index=0;index<linha.length;index+=1){
-        addpixel(linha[index]);
-    }
-}
-function addpixel(divLine){
-    for (let v=1;v<=25;v++){ 
-        divLine.appendChild(createPixel("pixel"));
-        if(v%5==0){
-            divLine.createElement(<br></br>);
-        }
-    }
-}
 function createPixel(className){
     let pixel=document.createElement("div");
     pixel.className=className;
     return pixel;
 }
+function addpixel(cet){
+    for (let v=1;v<=5;v++){ 
+        cet.appendChild(createPixel("pixel"));
+        }
+    }
+
+function readline(linha){
+    for(let index=0;index<linha.length;index+=1){
+        addpixel(linha[index]);
+       linha.appendChild(brek);
+    }
+}
+
