fe8f3c8eb5f7c49f5a6980fc24cadfd63a15add8
Fran Ribeiro <ribeirofrv@gmail.com>
2021-07-09 23:18:27 -0300

    Requisitos 7 e 8 - Testando...

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-window.onload = () => {
-  document.getElementById('black').classList.add('selected');
+// Define a cor preta como cor inicial.
+document.getElementById('black').classList.add('selected');
+// Clicar em uma das cores da paleta, faz com que ela seja selecionada e utilizada para preencher os pixels no quadro.
+const palette = document.getElementById('color-palette');
+const pickColor = document.getElementsByClassName('color');
+
+palette.addEventListener('click', (event) => {
+  for (let key = 0; key <= pickColor.length; key += 1) {
+    pickColor[key].classList.remove('selected');
+    event.target.classList.add('selected');
+  }
+});
+// Clicar em um pixel dentro do quadro após selecionar uma cor na paleta, faz com que o pixel seja preenchido com a cor selecionada.
+const toColor = () => {
+  const coloringBoard = document.getElementById('pixel-board');
+  coloringBoard.addEventListener('click', (event) => {
+    const selectColor = document.querySelector('.selected');
+    const currentColor = window.getComputedStyle(selectColor).getPropertyValue('background-color');
+    event.target.style.backgroundColor = currentColor;
+  });
 };
+toColor();
