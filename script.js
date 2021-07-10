d8e677008b8248553d6c4ff8a4139cb18459a04c
perchespierri <perchespierri@gmail.com>
2021-07-10 10:23:50 -0300

    mais mudanças do que eu consigo lembrar

--- script.js
+++ script.js
@@ -1,45 +1,81 @@
+function gridGenerator(tamanhoGrid) {
+  // Cria o grid de pixels usando uma tabela
+  let pixelBoard = document.getElementById('pixel-board');
+  let createTable = document.createElement('table');
+  pixelBoard.appendChild(createTable);
+  for (let i = 1; i <= tamanhoGrid; i += 1) {
+    let row = document.createElement('tr');
+    createTable.appendChild(row);
+    for (let j = 1; j <= tamanhoGrid; j += 1) {
+      let column = document.createElement('th');
+      column.className = 'pixel';
+      row.appendChild(column);
+    }
+  }
+}
+gridGenerator(5);
+
 function colorGenerator() {
-// Definir cores aleatoriamente (REF. [1])
-// O primeiro quadrado (posição 0) é sempre preto
+  // Define cores aleatoriamente (REF. [1])
+  // O primeiro quadrado (posição 0) é sempre preto
   for (let i=1; i <= 3; i+=1){
-    let x = Math.floor(Math.random() * 256);
-    let y = Math.floor(Math.random() * 256);
-    let z = Math.floor(Math.random() * 256);
-    let color = "rgb(" + x + "," + y + "," + z + ")";
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let color = "rgb(" + r + "," + g + "," + b + ")";
     let square = document.getElementById('color-palette').children[i];
     square.style.backgroundColor = color;
-    }
-// Não permitir a cor branca nem cores repetidas    
+  }
+  // Não permite a cor branca nem cores repetidas: 
   let colors = document.querySelectorAll('.color');
   for (let j=1; j<=3; j+=1){
     for (let k=0; k<=3; k+=1){
       if (colors[j] === 'white' || colors[j] === colors[k]){
         colorGenerator();
-    } else {
-        break;
-   }
+    } 
+      else {
+      break;
+    }
   } 
  }
 }
 colorGenerator();
 
-function gridGenerator(tamanhoGrid) {
-// Criar um grid 5x5
-  const pixelBoard = document.getElementById('pixel-board');
-  const newTable = document.createElement('table');
-  pixelBoard.appendChild(newTable);
-  for (let i = 1; i <= tamanhoGrid; i += 1) {
-    const row = document.createElement('tr');
-    newTable.appendChild(row);
-    for (let j = 1; j <= tamanhoGrid; j += 1) {
-      const column = document.createElement('th');
-      column.className = 'pixel';
-      row.appendChild(column);
+function getColor() {
+  // Retorna a cor clicada pelo usuário
+  let colorPalette = document.querySelector('#color-palette');
+  let colors = document.getElementsByClassName('color');
+  colorPalette.addEventListener('click', (event) => {
+    for (let j=0; j<colors.length; j++){
+      if (colors[j].classList.contains('selected')){
+        colors[j].classList.remove('selected');
+      }
     }
- }
+    for(let i=0; i<colors.length; i++){
+      if (colors[i] === event.target){
+        colors[i].classList.add('selected');
+        let newColor = colors[i].style.backgroundColor;
+        console.log(newColor);
+        return newColor;
+      }
+    }
+  });
 }
-gridGenerator(5);
+getColor();
 
+function paintGrid() {
+  // Pinta os pixels
+  let pixelBoard = document.querySelector('#pixel-board');
+  let selectedColor = document.querySelector('.selected');
+  let pixels = document.getElementsByClassName('pixel');
+  pixelBoard.addEventListener('click', (event) => {
+    for (let i=0; i<pixels.length; i++){
+        getColor();
+        pixels[i].event.target.style.backgroundColor = 'newColor';
+    }
+ });
+}
+paintGrid();
 
 // REFERÊNCIAS JAVASCRIPT:
 // [1] GERADOR DE COR ALEATÓRIA: https://www.w3resource.com/javascript-exercises/javascript-math-exercise-40.php
