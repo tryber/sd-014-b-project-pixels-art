17b22150d1a909a1ae4bdc8ad1d52718f591f743
Fran Ribeiro <ribeirofrv@gmail.com>
2021-07-10 00:13:05 -0300

    Requisito 9 - Um quase... ta bugado, mas to upando

--- script.js
+++ script.js
@@ -2,9 +2,8 @@
 document.getElementById('black').classList.add('selected');
 // Clicar em uma das cores da paleta, faz com que ela seja selecionada e utilizada para preencher os pixels no quadro.
 const palette = document.getElementById('color-palette');
-const pickColor = document.getElementsByClassName('color');
-
 palette.addEventListener('click', (event) => {
+  const pickColor = document.getElementsByClassName('color');
   for (let key = 0; key <= pickColor.length; key += 1) {
     pickColor[key].classList.remove('selected');
     event.target.classList.add('selected');
@@ -13,6 +12,7 @@ palette.addEventListener('click', (event) => {
 // Clicar em um pixel dentro do quadro após selecionar uma cor na paleta, faz com que o pixel seja preenchido com a cor selecionada.
 const toColor = () => {
   const coloringBoard = document.getElementById('pixel-board');
+
   coloringBoard.addEventListener('click', (event) => {
     const selectColor = document.querySelector('.selected');
     const currentColor = window.getComputedStyle(selectColor).getPropertyValue('background-color');
@@ -20,3 +20,12 @@ const toColor = () => {
   });
 };
 toColor();
+// Botão que, ao ser clicado, limpa o quadro preenchendo a cor de todos seus pixels com branco.
+const clearBoard = () => {
+  const pixels = document.getElementsByClassName('pixel');
+
+  for (let key = 0; key < pixels.length; key += 1) {
+    pixels[key].style.backgroundColor = 'white';
+  }
+};
+clearBoard();
