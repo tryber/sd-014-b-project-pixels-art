b790be5d0d552f0bc60b6c0346c0beb8d32ebf87
S4bot4 <sabotamail@gmail.com>
2021-07-10 00:29:48 -0300

    adicionado background image, estilização do botãode limpar e do H1

--- script.js
+++ script.js
@@ -3,4 +3,17 @@ initialColors[0].style.backgroundColor = 'black';
 initialColors[1].style.backgroundColor = 'yellow';
 initialColors[2].style.backgroundColor = 'red';
 initialColors[3].style.backgroundColor = 'green';
- 
+// Requisito 7
+function removeColor(event) {
+  const remColor = document.querySelector('.selected');
+  remColor.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+
+function selectedColor() {
+  const deletedColor = document.querySelectorAll('.color');
+  for (let index = 0; index < deletedColor.length; index += 1) {
+    deletedColor[index].addEventListener('click', removeColor)
+    console.log(deletedColor[index].addEventListener('click', removeColor));
+  }
+}
