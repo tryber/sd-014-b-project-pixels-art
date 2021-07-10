101a66d8ce2f6cc06a6041decda69bcc54dddc3d
perchespierri <perchespierri@gmail.com>
2021-07-10 12:58:25 -0300

    não tá tudo bem, cara.

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-function gridGenerator(tamanhoGrid) {
-  // Cria o grid de pixels usando uma tabela
-  let pixelBoard = document.getElementById('pixel-board');
-  let createTable = document.createElement('table');
-  pixelBoard.appendChild(createTable);
-  for (let i = 1; i <= tamanhoGrid; i += 1) {
-    let row = document.createElement('tr');
-    createTable.appendChild(row);
-    for (let j = 1; j <= tamanhoGrid; j += 1) {
-      let column = document.createElement('th');
-      column.className = 'pixel';
-      row.appendChild(column);
-    }
-  }
-}
-gridGenerator(5);
-
 function colorGenerator() {
   // Define cores aleatoriamente (REF. [1])
   // O primeiro quadrado (posição 0) é sempre preto
@@ -41,44 +24,23 @@ function colorGenerator() {
 }
 colorGenerator();
 
-
-function getColor() {
-  // Retorna a cor clicada pelo usuário
-  let colors = document.getElementsByClassName('color');
-  const colorPalette = document.querySelector('#color-palette');
-  colorPalette.addEventListener('click', (event) => {
-    for (let j=0; j<colors.length; j++){
-      if (colors[j].classList.contains('selected')){
-        colors[j].classList.remove('selected');
-      }
+function gridGenerator(tamanhoGrid) {
+  // Cria o grid de pixels usando uma tabela
+  let pixelBoard = document.getElementById('pixel-board');
+  let createTable = document.createElement('table');
+  pixelBoard.appendChild(createTable);
+  for (let i = 1; i <= tamanhoGrid; i += 1) {
+    let row = document.createElement('tr');
+    createTable.appendChild(row);
+    for (let j = 1; j <= tamanhoGrid; j += 1) {
+      let column = document.createElement('th');
+      column.className = 'pixel';
+      row.appendChild(column);
     }
-    for(let i=0; i<colors.length; i++){
-      if (colors[i] === event.target){
-        colors[i].classList.add('selected');
-        let newColor = colors[i].style.backgroundColor;
-        console.log(newColor);
-        return newColor
-      }
-    }
-  });
-}
-getColor();
-
-function paintGrid() {
-  // Pinta os pixels
-  let newColor = getColor();
-  // Pintar ao clicar:
-  let pixels = document.querySelectorAll('.pixel');
-  let pixelBoard = document.querySelector('#pixel-board');
-  for (let i = 0; i < pixels.length; i++){
-    pixelBoard.addEventListener('click', (event) => {
-        if (event.target === pixels[i]){
-          pixels[i].style.backgroundColor = newColor;
-        }
-    });
   }
 }
-paintGrid();
+gridGenerator(5);
+
 
 // REFERÊNCIAS JAVASCRIPT:
 // [1] GERADOR DE COR ALEATÓRIA: https://www.w3resource.com/javascript-exercises/javascript-math-exercise-40.php
