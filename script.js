3159afa17e169389ee0efd56896cca3dce49b008
Arturo Vaine <arturo.vaine@gmail.com>
2021-07-11 13:11:05 -0300

    Update.

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 // 1 - Adicione à página o título "Paleta de Cores". OK
+// 2 - Adicione à página uma paleta de quatro cores distintas. - OK
 
+// 6 - Defina a cor preta como cor inicial.
+
+document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
+document.getElementsByClassName('color')[0].className = 'selected';
+
+// 9 - Crie um botão que, ao ser clicado, limpa o quadro preenchendo a cor de todos seus pixels com branco.
+
+const clearButton = document.getElementById('clear-board'); // Atribuir elemento do botão clear-board à variável clearButton
+
+function clearBoard() { // Adicionar evento 'click' para botão acionar função anônima
+  document.querySelectorAll('.pixel').forEach((element) => {
+    const pixel = element;
+    pixel.style.backgroundColor = 'white'; // Setar backgroundcolor em branco para cada pixel
+  });
+}
+
+clearButton.addEventListener('click', clearBoard);
+
+// 12 - Faça com que as cores da paleta sejam geradas aleatoriamente ao carregar a página.
 // Função abaixo baseada em: https://stackoverflow.com/questions/27406377/javascript-generate-random-number-except-some-values
 function numRandom() {
   const num = Math.floor(Math.random() * 16777215);
@@ -20,21 +40,3 @@ window.onload = function start() {
   document.getElementById('color2').style.backgroundColor = secondColor;
   document.getElementById('color3').style.backgroundColor = thirdColor;
 };
-
-// 6 - Defina a cor preta como cor inicial.
-
-document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
-document.getElementsByClassName('color')[0].className = 'selected';
-
-// 9 - Crie um botão que, ao ser clicado, limpa o quadro preenchendo a cor de todos seus pixels com branco.
-
-const clearButton = document.getElementById('clear-board'); // Atribuir elemento do botão clear-board à variável clearButton
-
-function clearBoard() { // Adicionar evento 'click' para botão acionar função anônima
-  document.querySelectorAll('.pixel').forEach((element) => {
-    const pixel = element;
-    pixel.style.backgroundColor = 'white'; // Setar backgroundcolor em branco para cada pixel
-  });
-}
-
-clearButton.addEventListener('click', clearBoard);
