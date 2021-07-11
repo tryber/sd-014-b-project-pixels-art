a984d8a03b777d3dc868781a2d4dc999be6cf0fa
Luiz Gustavo <gusstavopdm@gmail.com>
2021-07-11 11:19:03 -0300

    Conclusao Requisito - 10/11 & 12

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // eslint-disable-next-line func-names
 
 // Criar div com as paletas de cores.
+const divPalette = document.getElementById('color-palette');
 
 function createDivColors(quantidadeColors) {
-  const divPalette = document.getElementById('color-palette');
   const classDiv = 'color';
   for (let index = 1; index <= quantidadeColors; index += 1) {
     const createBox = document.createElement('div');
@@ -14,25 +14,28 @@ function createDivColors(quantidadeColors) {
 createDivColors(4);
 
 // Preencher cores na paleta de cores.
+const divColor = document.getElementsByClassName('color');
 
 function addColor(colors) {
-  const divColor = document.getElementsByClassName('color');
   for (let index = 0; index < divColor.length; index += 1) {
     const divIndex = divColor[index];
     for (let sIndex = 0; sIndex < colors.length; sIndex += 1) {
       const colorIndex = colors[index];
       divIndex.style.backgroundColor = colorIndex;
-      divIndex.classList.add(`color-${colorIndex}`);
+      divIndex.classList.add(colorIndex);
     }
   }
 }
+const color1 = `#${Math.floor(Math.random() * 777215)}`;
+const color2 = `#${Math.floor(Math.random() * 777215)}`;
+const color3 = `#${Math.floor(Math.random() * 777215)}`;
 
-addColor(['black', 'red', 'blue', 'green']);
+addColor(['black', color1, color2, color3]);
 
 // Cria a tabela de divs, usando o display table.
+const pixelBoard = document.getElementById('pixel-board');
 
 function tablePixel(pixel) {
-  const pixelBoard = document.getElementById('pixel-board');
   for (let index = 1; index <= pixel; index += 1) {
     const createBoxCell = document.createElement('div');
     createBoxCell.classList.add('pixel-tr');
@@ -48,13 +51,12 @@ function tablePixel(pixel) {
     }
   }
 }
-
 tablePixel(5);
 
 // Define cor branco, assim que a pagina e carregada, no window.onload.
+const pixel = document.getElementsByClassName('pixel');
 
 function resetColorPixel() {
-  const pixel = document.getElementsByClassName('pixel');
   for (let index = 0; index < pixel.length; index += 1) {
     pixel[index].style.backgroundColor = 'white';
   }
@@ -63,11 +65,10 @@ function resetColorPixel() {
 // Definir cor inicial selecionada, a funcao colorSelectedInitial, e chamada no window.onload - LINHA 74, com a cor que deseja-se ser a inicial.
 
 function colorSeletectedInitial(color) {
-  const colors = document.querySelectorAll('.color');
   const setColorInitial = color;
-  for (let index = 0; index < colors.length; index += 1) {
-    const bgColor = colors[index].style.backgroundColor;
-    if (bgColor === setColorInitial) colors[index].classList.add('selected');
+  for (let index = 0; index < divColor.length; index += 1) {
+    const bgColor = divColor[index].style.backgroundColor;
+    if (bgColor === setColorInitial) divColor[index].classList.add('selected');
   }
 }
 
@@ -77,7 +78,8 @@ window.onload = function onload() {
 };
 
 // Difinir class selected
-function handleChangeTech(event) {
+
+function handleChangeSelected(event) {
   const colorElement = document.querySelector('.selected');
   colorElement.classList.remove('selected');
   event.target.classList.add('selected');
@@ -85,21 +87,24 @@ function handleChangeTech(event) {
 const divsColors = document.querySelectorAll('.color');
 for (let index = 0; index < divsColors.length; index += 1) {
   const divsIndex = divsColors[index];
-  divsIndex.addEventListener('click', handleChangeTech);
+  divsIndex.addEventListener('click', handleChangeSelected);
 }
 
 // Clicar em uma das cores da paleta faz com que ela seja selecionada e utilizada para preencher os pixels no quadro.
 
-const boxPixel = document.querySelectorAll('.pixel');
-for (let index = 0; index < boxPixel.length; index += 1) {
-  const pixelIndex = boxPixel[index];
-  pixelIndex.addEventListener('click', (event) => {
-    const selected = document.querySelector('.selected');
-    const colorSelected = selected.style.backgroundColor;
-    // eslint-disable-next-line no-param-reassign
-    event.target.style.backgroundColor = colorSelected;
-  });
+// const selected = document.querySelector('.selected');
+let boxPixel = document.querySelectorAll('.pixel');
+function colorBox() {
+  for (let index = 0; index < boxPixel.length; index += 1) {
+    const pixelIndex = boxPixel[index];
+    pixelIndex.addEventListener('click', (event) => {
+      const evento = event;
+      const selected = document.querySelector('.selected').style.backgroundColor;
+      evento.target.style.backgroundColor = selected;
+    });
+  }
 }
+colorBox();
 
 // Crie um botão que, ao ser clicado, limpa o quadro preenchendo a cor de todos seus pixels com branco.
 
@@ -110,4 +115,26 @@ clearButton.addEventListener('click', () => {
 });
 
 // Faça o quadro de pixels ter seu tamanho definido pela pessoa usuária.
+
+function clearBoard() {
+  pixelBoard.innerHTML = '';
+}
+
+const buttonSizeBoard = document.getElementById('generate-board');
+
+buttonSizeBoard.addEventListener('click', () => {
+  const valueInput = document.getElementById('board-size').value;
+  let inputNumber = parseInt(valueInput, 10);
+  if (inputNumber > 50) inputNumber = 50;
+  if (inputNumber < 5) inputNumber = 5;
+  if (inputNumber > 0) {
+    clearBoard();
+    tablePixel(inputNumber);
+    boxPixel = document.querySelectorAll('.pixel');
+    resetColorPixel();
+    colorBox();
+  } else {
+    alert('Board inválido!');
+  }
+});
 
