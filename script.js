0170062d6df6a4e24e9ecfbbc3aa23cc7a97b69b
mf <marcelo.cunha1@outlook.com>
2021-07-11 15:54:26 -0300

    resolved issue 7

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function getDivsColor() {
   const getDivs = document.querySelectorAll('.color');
-  getDivs[0].classList.add('black', 'selected');
+  getDivs[0].classList.add('black');
   getDivs[1].classList.add('red');
   getDivs[2].classList.add('blue');
   getDivs[3].classList.add('green');
+
+  
 }
 
 function creadPixelBoard() {
@@ -17,7 +19,20 @@ function creadPixelBoard() {
   }
 }
 
-window.onload = function () {
+function colorSelecte() {
+  const divs = document.querySelectorAll('.color');
+  for (let i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', function() {
+      for (let i = 0; i < divs.length; i++) {
+        divs[i].classList.remove('selected');
+      }
+      divs[i].classList.add('selected');
+    })
+  }
+}
+
+window.onload = function() {
   getDivsColor()
   creadPixelBoard()
+  colorSelecte()
 }
