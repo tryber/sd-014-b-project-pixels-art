3f6ceaa10fcf70e89fa79b22986f76341aae0b1c
GustavoDamm <gustavo.damm15@hotmail.com>
2021-07-12 11:27:29 -0300

    projeto em andamento

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ for (let i = 0; i < color.length; i += 1) {
   palette.appendChild(blu);
 }
 
-function tamanho(cell) {
+// https://www.w3schools.com/html/html_tables.asp
+function size(cell) {
   for (let i = 0; i < cell; i += 1) {
     const pixel = document.createElement('tr');
     pixel.classList = 'pboard';
@@ -31,23 +32,52 @@ function tamanho(cell) {
   }
 }
 
-tamanho(5);
+size(5);
 
-palette.firstChild.className.add('selected');
+palette.firstChild.classList.add('selected');
 
-// https://gomakethings.com/attaching-multiple-
-// elements-to-a-single-event-listener-in-vanilla-js/
-document.body.addEventListener('click', (event) => {
-  if (event.target.className === 'cor') {
-    document.querySelector('.selected').classList.remove('selected');
-    event.target.classList.add('selected');
+//  https://gomakethings.com/attaching-multiple
+//  -elements-to-a-single-event-listener-in-vanilla-js/
+document.addEventListener('click', (colorE) => {
+  if (colorE.target.classList.contains('color')) {
+    const selected = document.querySelector('.selected');
+    selected.classList.remove('selected');
+    colorE.target.classList.add('selected');
   }
 });
 
-document.body.addEventListener('click', (event) => {
-  if (event.target.className === 'pixel') {
-    const selectedBG = document.queryselector('selected').style.backgroundColor;
+document.body.addEventListener('click', (click) => {
+  if (click.target.classList.contains('pixel')) {
+    const selectedBG = document.querySelector('.selected').style.backgroundColor;
     // eslint-disable-next-line no-param-reassign
-    event.target.style.backgroundColor = selectedBG;
+    click.target.style.backgroundColor = selectedBG;
+  }
+});
+
+const buttonClear = document.querySelector('#clear-board');
+
+buttonClear.addEventListener('click', () => {
+  const cpixel = document.getElementsByClassName('pboard');
+  for (let i = 0; i < cpixel.length; i += 1) {
+    cpixel[i].style.backgroundColor = 'white';
+  }
+});
+
+// https://www.w3schools.com/jsref/prop_text_value.asp
+// https://stackoverflow.com/questions/11563638/how
+// -do-i-get-the-value-of-text-input-field-using-javascript
+
+const textValue = document.querySelector('#board-size').value;
+const buttomResize = document.querySelector('#resize');
+// https://developer.mozilla.org/pt-BR/docs
+// /Web/JavaScript/Reference/Global_Objects/parseInt
+
+buttomResize.addEventListener('click', () => {
+  if (textValue < 5) {
+    size(5);
+  } else if (textValue >= 50) {
+    size(50);
+  } else {
+    size(textValue);
   }
 });
