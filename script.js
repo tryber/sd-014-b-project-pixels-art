1a9da35473e611b73791827b4d8c054b44343219
BrennoBener <brennobbener@live.com>
2021-07-12 18:41:54 -0300

    vamo de novo, 1 e 2

---
+++ script.js
@@ -0,0 +1,6 @@
+const colors = document.querySelectorAll('.color');
+
+colors[0].setAttribute('style', 'background-color: black');
+colors[1].setAttribute('style', 'background-color: green');
+colors[2].setAttribute('style', 'background-color: blue');
+colors[3].setAttribute('style', 'background-color: pink');
