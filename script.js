01a873c7357ea98bd12ce31962e11307e798f5a2
Emmanuel <EmmanuelRodriguesBoo@gmail.com>
2021-07-12 00:11:21 -0300

    Projeto Finalizado

--- script.js
+++ script.js
@@ -1,50 +1,31 @@
-function create(value) {
-  const board = document.querySelector('#pixel-board');
-  for (let i = 0; i < value; i += 1) {
-    const boardItem = document.createElement('div');
-    boardItem.className = 'pixel';
-    board.appendChild(boardItem);
-  }
-}
-create(5 ** 2);
-
-function paletteColor() {
-  document.querySelector('#color1').style.backgroundColor = 'black';
-  document.querySelector('#color2').style.backgroundColor = 'red';
-  document.querySelector('#color3').style.backgroundColor = 'green';
-  document.querySelector('#color4').style.backgroundColor = 'blue';
-}
-paletteColor();
-
-const blackColor = document.querySelector('.black');
-const redColor = document.querySelector('.red');
-const greenColor = document.querySelector('.green');
-const blueColor = document.querySelector('.blue');
-
-function fillPixel(evento) {
-  const selected = document.querySelector('.selected');
-  selected.classList.remove('selected');
-  evento.target.classList.add('selected');
-}
-
-function changeColor(event) {
-  for (i = 0; i < 5; i++) {
-    let atual = document.querySelectorAll('.color');
-    if (atual[i] === document.querySelector('.selected'))
-      [(atual = atual[i].classList.remove('selected'))];
-  }
-  event.target.classList.add('selected');
-}
-
-const board= document.querySelector('#pixel-board');
-board.addEventListener('click', (e) => {
-  const select =
-    document.querySelector('.selected').style.backgroundColor;
-  e.target.style.backgroundColor = select;
-});
-
+const black = document.getElementById('black');
+const orange = document.getElementById('orange');
+const yellow = document.getElementById('yellow');
+const red = document.getElementById('red');
+let defColor = document.querySelector('.selected');
+const pixelBoard = document.querySelector('#pixel-board');
 const btn = document.querySelector('#clear-board');
-btn.addEventListener('click', clearBtn);
-function clearBtn() {
-  document.querySelector('#pixel-board').style.backgroundColor = 'white';
+const pixel = document.querySelectorAll('.pixel');
+function changeColor(event){
+  const selColor = event.target;
+  if (selColor !== defColor) {
+    defColor.classList.remove('selected');
+    selColor.classList.add('selected');
+  }
+  defColor = selColor;
 }
+black.addEventListener('click', changeColor);
+orange.addEventListener('click', changeColor);
+yellow.addEventListener('click', changeColor);
+red.addEventListener('click', changeColor);
+function paint(event){
+  const click = event.target;
+  click.style.backgroundColor = defColor.id;
+}
+pixelBoard.addEventListener('click', paint);
+function clear(){
+  for (let index = 0; index < pixel.length; index += 1) {
+    pixel[index].removeAttribute('style');
+  }
+}
+btn.addEventListener('click', clear());
