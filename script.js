258ae937c4844d27af0d11bba8f14705a929d062
Davisralves <davialves324@gmail.com>
2021-07-12 15:38:29 -0300

    adicionando requisito bonus 10

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-function criaPixels(x,y){           // cria a tabela de pixel art com o tamanho x de lagura e y de altura.
-    let div = document.querySelector('#pixel-board');
-    
-    for (let index = 0; index < y; index ++){
-        let ul = document.createElement('ul');
-        div.appendChild(ul);
-        for (let index = 0; index < x; index ++){
-            let li = document.createElement('li');
-            ul = document.querySelector('div').lastChild;
-            ul.appendChild(li);
-            li = ul.lastChild;
-            li.className = 'pixel';
-        }
-    } 
-};
+function criaPixels(x, y) { // cria a tabela de pixel art com o tamanho x de lagura e y de altura.
+  const div = document.querySelector('#pixel-board');
+  for (let index = 0; index < y; index += 1) {
+    let ul = document.createElement('ul');
+    div.appendChild(ul);
+    for (let index = 0; index < x; index += 1) {
+      let li = document.createElement('li');
+      ul = document.querySelector('div').lastChild;
+      ul.appendChild(li);
+      li = ul.lastChild;
+      li.className = 'pixel';
+    }
+  }
+}
 
 function selectColorAndRemove(event){                    // Apartir da variavel 'color' adiciona a classe selected
     let cor = event.target.classList[1];
@@ -81,23 +80,43 @@ function addEventPixels(){
 }
 
 function apagar() {
-    let tabela = document.querySelector('div').querySelectorAll('li');
-    for (let pixel of tabela){
-        pixel.className = 'pixel';
-        pixel.style.backgroundColor = 'white';
-    }
+  const tabela = document.querySelector('div').querySelectorAll('li');
+  // eslint-disable-next-line no-restricted-syntax
+  for (let pixel of tabela) {
+    pixel.className = 'pixel';
+    pixel.style.backgroundColor = 'white';
+  }
 }
 
-let button = document.querySelector('button');
-var colors = document.querySelectorAll('.color');
+function removeTabela() {
+  let tabela = document.getElementById('pixel-board');
+  let ul = document.querySelectorAll('ul');
+  for (let index = 1; index <= ul.length - 1; index += 1) {
+    tabela.removeChild(ul[index]);  
+  }
+}
+
+function recriaPixels() {
+  if (document.getElementById('board-size').value === "") {
+    alert('Board inválido!');
+    return;
+  }
+  let input = parseInt(document.getElementById('board-size').value);
+  if (input < 5 || input > 50 ) {
+    alert('Digite um valor entre 5 e 50')
+  } else {
+    removeTabela();
+    criaPixels(input, input);
+    addEventPixels();
+   }      
+}
+
+const button = document.querySelector('#clear-board');
+const colors = document.querySelectorAll('.color');
 criaPixels(5,5);
 colors[0].classList.add('selected'); // seleciona o preto.
 criarEventosPaletaDeCores();
 addEventPixels();
 button.addEventListener('click', apagar);
-
-
-
-
-
-
+let buttonVqv = document.getElementById('generate-board');
+buttonVqv.addEventListener('click', recriaPixels);
