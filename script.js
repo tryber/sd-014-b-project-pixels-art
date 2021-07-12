a726ed2ecb65107476f478887245ba0f078a7000
BrennoBener <brennobbener@live.com>
2021-07-12 16:58:07 -0300

    requisito 6 talvez

---
+++ script.js
@@ -0,0 +1,12 @@
+{
+    let corPreto = document.getElementsByClassName('color1')[0]
+corPreto.style.backgroundColor = 'black';
+let corAzul = document.getElementsByClassName('color2')[1]
+corAzul.style.backgroundColor = 'aqua';
+let corVermelhoDark = document.getElementsByClassName('color3')[2]
+corVermelhoDark.style.backgroundColor = 'darkred';
+let corVerde = document.getElementsByClassName('color4')[3]
+corVerde.style.backgroundColor = 'lawngreen';
+
+corPreto.classList.add('selected'); 
+}
