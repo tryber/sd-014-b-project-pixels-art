8a7e8879e11605fd65313ff9a5576b4ebd775395
BrennoBener <brennobbener@live.com>
2021-07-12 16:08:27 -0300

    testando de novo

---
+++ script.js
@@ -0,0 +1,10 @@
+let corPreto = document.getElementsByClassName('color')[0]
+corPreto.style.backgroundColor = 'black';
+let corAqua = document.getElementsByClassName('color')[1]
+corAqua.style.backgroundColor = 'aqua';
+let corLawngreen = document.getElementsByClassName('color')[2]
+corDarkRed.style.backgroundColor = 'darkred';
+let cor = document.getElementsByClassName('color')[3]
+corLawngreen.style.backgroundColor = 'lawngreen';
+
+corPreto.classList.add('selected');
