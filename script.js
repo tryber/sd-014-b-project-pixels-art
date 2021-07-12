aee64338157d56c3e38b9b99b33119c4d8bb6600
Thiago Frozzi <thiagofrozzi@hotmail.com>
2021-07-12 20:23:59 -0300

    Atualizando o requisito 10

--- script.js
+++ script.js
@@ -3,14 +3,9 @@ const secondDiv = document.getElementById('yellow');
 const thirdDiv = document.getElementById('red');
 const fourthDiv = document.getElementById('blue');
 const clearButton = document.getElementById('clear-board');
-const generateButton = documen.getElementById('generate-board');
+const generateButton = document.getElementById('generate-board');
 const pixelBoard = document.getElementById('pixel-board');
 
-    for(let cont=0;cont <=24; cont+=1){
-        let box = document.querySelectorAll('.pixel');
-        box[cont].addEventListener("click",printColor);
-    }
-    
 
 function selectColor(event){
     let color = document.querySelector('.selected');
@@ -21,7 +16,7 @@ function selectColor(event){
 firstDiv.addEventListener("click",selectColor)
 secondDiv.addEventListener("click",selectColor)
 thirdDiv.addEventListener("click",selectColor)
-forDiv.addEventListener("click",selectColor)
+fourthDiv.addEventListener("click",selectColor)
 
 
 function printColor(event){
@@ -33,28 +28,66 @@ function printColor(event){
 function resetColor(){
     
     let boxPrinted = document.querySelectorAll('.pixel')
-    for (let cont = 0; cont <=24;cont+=1){
+    for (let cont = 0; cont < boxPrinted.length;cont+=1){
     boxPrinted[cont].style.backgroundColor = 'white';
     } 
 }
 
 clearButton.addEventListener("click",resetColor);
-generateButton.addEventListener("click",creatDiv);
+generateButton.addEventListener("click",creatPixels);
 
-function creatDiv(){
-    let n = input.value;
-    if (n > 0){
-        for (let cont=0; cont < n; cont+=1){
-            let div = document.createElement('div')
+function creatPixels(){
+    let n = document.getElementById('board-size').value;
+    if (n >= 5 && n <= 50){
+        for (let line=0; line < n; line+=1){
+          let div = document.createElement('div')
             pixelBoard.appendChild(div);
             div.classList.add('pixel');
-        }
-        }else {
-            alert('Board inválido!')
-        }
+            
+            
+            for (let colun = 0; colun < n; colun+=1){
+              let div2 = document.createElement('div')
+              let ultimaDiv = document.querySelector('#pixel-board').lastElementChild
+                ultimaDiv.appendChild(div2);
+                div2.classList.add('pixel');
+            }
+            
+        }    
+            
+        for(let cont=0;cont < n*n; cont+=1){
+            let box = document.querySelectorAll('.pixel');
+                box[cont].addEventListener("click",printColor);
+            }
+    
+    }else{
+        alert('Board inválido!')
+    
     }
+}
 
+function creatDefault(){
+    let n = 5;
+    for (let line=0; line < n; line+=1){
+        let div = document.createElement('div')
+          pixelBoard.appendChild(div);
+          div.classList.add('pixel');
+          
+          
+    
+          for (let colun = 0; colun < n; colun+=1){
+            let div2 = document.createElement('div')
+            let ultimaDiv = document.querySelector('#pixel-board').lastElementChild
+              ultimaDiv.appendChild(div2);
+              div2.classList.add('pixel');
+          }
+      }
+          
+      for(let cont=0;cont < n*n; cont+=1){
+          let box = document.querySelectorAll('.pixel');
+              box[cont].addEventListener("click",printColor);
+          }
+}
 
-
+creatDefault();
 
 
