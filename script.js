e361e4dc8a4eaa2068816c7a058150d8639abcaf
miguel-alexandre <sauloj@protonmail.com>
2021-07-12 17:25:39 -0300

    Projeto refatorado V

--- script.js
+++ script.js
@@ -1,107 +1,105 @@
-const divColors = document.querySelector('#color-palette');
-const color1 = document.querySelector('.color1');
-const color2 = document.querySelector('.color2');
-const color3 = document.querySelector('.color3');
-const color4 = document.querySelector('.color4');
-const boardSize = document.querySelector('#board-size');
-const generateBoard = document.querySelector('#generate-board');
-const cleaner = document.querySelector('#clear-board');
-const board = document.querySelector('#pixel-board');
-let selectedDiv = document.querySelector('.selected');
-let selectedColor;
+const colorSelected = 'color selected';
 
-//  FUNCTIONS
-//  Função responsável por remover classe 'selected' da cor selecionada no momento
-function clearSelection() {
-  selectedDiv.classList.remove('selected');
-}
-//  Função responsável por adicionar classe 'selected' ao elemento selecionado
-function addSelection(target) {
-  target.classList.add('selected');
-  selectedDiv = document.querySelector('.selected');
-}
-//  Função Responsável por pegar a cor da div selecionada
-function getColor() {
-  const property = window.getComputedStyle(selectedDiv, null).getPropertyValue('background-color');
-  return property;
-}
-//  Função responsável por pintar pixel
-function paint(target) {
-  target.style.backgroundColor = selectedColor;
-}
-//  Função responsável por limpar a board
-function clearBoard() {
-  const pixels = document.querySelectorAll('.pixel');
-  for (let index = 0; index < pixels.length; index += 1) {
-    pixels[index].style.backgroundColor = '';
+function createBoardLines(input) {
+  const local = document.getElementById('pixel-board');
+  for (let index = 0; index < input; index += 1) {
+    const line = document.createElement('div');
+    line.className = 'box-line';
+    local.appendChild(line);
   }
 }
-//  Função responsável por criar rows da matriz
-function buildRow(number) {
-  const divRow = document.createElement('div');
-  divRow.className = 'row';
-  for (let index = 0; index < number; index += 1) {
-    const divCell = document.createElement('div');
-    divCell.className = 'pixel';
-    divRow.appendChild(divCell);
-  }
-  board.appendChild(divRow);
-}
-//  Função responsável por construir board
-function buildBoard(number) {
-  board.innerHTML = '';
-  for (let index = 0; index < number; index += 1) {
-    buildRow(number);
-  }
-}
-function generateColor() { // Inspirado em código achado no seguint url (https://stackoverflow.com/questions/1152024/best-way-to-generate-a-random-color-in-javascript/1152508)
-  const first = Math.floor(Math.random() * 256);
-  const second = Math.floor(Math.random() * 256);
-  const third = Math.floor(Math.random() * 256);
-  const forth = Math.floor(Math.random()* 256);
-  
-  return `rgb(${first}, ${second}, ${third}, ${forth})`;
-}
-//  DEFINIÇÕES PÓS FUNÇÕES
-selectedColor = getColor(selectedDiv);
 
-//  EVENTOS
-//  Gerando cores
-window.onload = function gerar () {
-  color1.style.backgroundColor = generateColor();
-  color2.style.backgroundColor = generateColor();
-  color3.style.backgroundColor = generateColor();
-  color4.style.backgroundColor = generateColor();
-  
+function createBoard(input) {
+  const local = document.querySelectorAll('.box-line');
+  for (let index = 0; index < local.length; index += 1) {
+    for (let index2 = 0; index2 < input; index2 += 1) {
+      const column = document.createElement('div');
+      column.className = 'pixel';
+      local[index].appendChild(column);
+    }
+  }
+}
+
+function innitialSelection() {
+  const local = document.querySelector('#color1');
+  local.className = colorSelected;
+}
+
+function setColor() {
+  const selectedColor = document.getElementsByClassName(colorSelected);
+  const color = document.getElementsByClassName('color');
+  for (let index = 0; index < color.length; index += 1) {
+    color[index].addEventListener('click', () => {
+      if (color[index].className === 'color') {
+        selectedColor[0].className = 'color';
+        color[index].className = colorSelected;
+      }
+    });
+  }
+}
+
+function paintPixel() {
+  const pixel = document.getElementsByClassName('pixel');
+  for (let index = 0; index < pixel.length; index += 1) {
+    const element = pixel[index];
+    element.addEventListener('click', () => {
+      const colorId = document.getElementsByClassName(colorSelected)[0];
+      const color = window.getComputedStyle(colorId).getPropertyValue('background-color');
+      element.style.backgroundColor = color;
+    });
+  }
+}
+
+function clearBtn() {
+  const btn = document.getElementById('clear-board');
+  btn.addEventListener('click', () => {
+    const pixel = document.getElementsByClassName('pixel');
+    for (let index = 0; index < pixel.length; index += 1) {
+      const element = pixel[index];
+      element.style.backgroundColor = 'white';
+    }
+  });
+}
+
+function removeBoard() {
+  const line = document.getElementsByClassName('box-line');
+  while (line.length > 0) {
+    line[0].remove();
+  }
+}
+
+function boardWorking(input) {
+  createBoardLines(input);
+  createBoard(input);
+  paintPixel();
+  clearBtn();
+}
+
+function getNumber() {
+  const btn = document.getElementById('generate-board');
+  const size = document.getElementById('board-size');
+  let input = 5;
+  btn.addEventListener('click', () => {
+    removeBoard();
+    if (size.value === '') {
+      window.alert('Board inválido!');
+    } else {
+      input = size.value;
+      boardWorking(input);
+    }
+  });
+  boardWorking(input);
+}
+
+window.onload = function run() {
+  getNumber();
+  innitialSelection();
+  setColor();
 };
-//  Adicionando evento de clique nas cores da paleta
-divColors.addEventListener('click', function (event) {
-  clearSelection();
-  addSelection(event.target);
-  selectedColor = getColor();
+document.getElementById("board-size")
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.key === 'Enter') {
+        document.getElementById("generate-board").click();
+    }
 });
-//  Adicionando evento de clique na pixel-board
-board.addEventListener('click', function (event) {
-  paint(event.target);
-});
-// Adicionando evento de clique no botão limpar
-cleaner.addEventListener('click', function () {
-  clearBoard();
-});
-//  Adicionando evento de clique no botão generate board
-generateBoard.addEventListener('click', function () {
-  let number = 0;
-  if (boardSize.value === '') {
-    alert('Board inválido!');
-  } else if (boardSize.value < 5) {
-    number = 5;
-    buildBoard(number);
-  } else if (boardSize.value > 50) {
-    number = 50;
-    buildBoard(number);
-  } else {
-    number = boardSize.value;
-    buildBoard(number);
-  }
-});
-
