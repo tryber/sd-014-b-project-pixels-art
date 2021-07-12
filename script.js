a2127c285bd57845a460a6624d706351ea21f33e
S4bot4 <sabotamail@gmail.com>
2021-07-11 21:00:58 -0300

    corrigindo erros de identação

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let initialColors = document.getElementsByClassName('color');
+const initialColors = document.getElementsByClassName('color');
 initialColors[0].style.backgroundColor = 'black';
 initialColors[1].style.backgroundColor = 'yellow';
 initialColors[2].style.backgroundColor = 'red';
@@ -10,10 +10,23 @@ function removeColor(event) {
   event.target.classList.add('selected');
 }
 
+//trocar de classse Requisito 8
 function selectedColor() {
   const deletedColor = document.querySelectorAll('.color');
   for (let index = 0; index < deletedColor.length; index += 1) {
-    deletedColor[index].addEventListener('click', removeColor)
-    console.log(deletedColor[index].addEventListener('click', removeColor));
+    deletedColor[index].addEventListener('click', removeColor);
+   
+console.log(click);  
+  }
+}
+
+
+// Requisito 9
+// limpar background / class pixel
+const bgwall = document.querySelectorAll('.pixel');
+function clearColors() {
+ 
+  for (let index = 0; index < bgwall.length; index += 1) {
+    bgwall[index].style.backgroundColor = 'rgb(255, 255, 255)';
   }
 }
