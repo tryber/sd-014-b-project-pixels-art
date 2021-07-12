60b5676df3395e0d2ac9640fae0d35830b5398ed
dsmarcelo <celoygm@gmail.com>
2021-07-12 15:05:50 -0300

    Requisito 10

--- script.js
+++ script.js
@@ -4,9 +4,9 @@
 //   const box1 = document.getElementById('box1');
 //   box1.classList.add = 'selected';
 // };
+const pixels = document.querySelectorAll('.pixel');
 
 const paletteColors = document.querySelectorAll('.color');
-console.log(paletteColors.length);
 
 function getColor() {
   const currentPalette = document.querySelector('.selected');
@@ -20,7 +20,6 @@ function addClassSelected(event) {
   const lastColor = document.querySelector('.selected');
   lastColor.classList.remove('selected');
   event.target.classList.add('selected');
-  console.log(event);
   getColor();
 }
 
@@ -28,14 +27,25 @@ for (let i = 0; i < paletteColors.length; i += 1) {
   paletteColors[i].addEventListener('click', addClassSelected);
 }
 
-function applyColor(event) {
-  event.target.style.backgroundColor = 'getColor';
-}
+// function applyColor(event) {
+//   event.target.style.backgroundColor = 'getColor';
+// }
 
-document.querySelectorAll('.pixel').forEach((item) => {
+pixels.forEach((item) => {
   item.addEventListener('click', (event) => {
     event.target.style.backgroundColor = getColor();
-    console.log(event);
-    console.log(getColor());
   });
 });
+
+function clearBoard() {
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].style.backgroundColor = 'white';
+  }
+}
+
+// document.querySelectorAll('.pixel').forEach((item) => {
+//   item.style.backgroundColor = 'rgb (0, 0, 0)';
+//   console.log(item);
+// });
+
+document.getElementById('clear-board').addEventListener('click', clearBoard);
