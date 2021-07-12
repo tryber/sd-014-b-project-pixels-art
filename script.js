1f06fc3292cc0b5998138d85d132ab991c62260a
Vinicius <vinicius@DESKTOP-NBO50A7.localdomain>
2021-07-12 19:28:46 -0300

    Requisito #8 - Pintando os pixels

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function createColors() {
-  let cores = ['black', 'red', 'blue', 'green']
+  const cores = ['black', 'red', 'blue', 'green'];
   const elementId = document.querySelector('#color-palette');
   for (let i = 0; i < 4; i += 1) {
     const elementColor = document.createElement('span');
     elementColor.className = 'color';
     elementId.appendChild(elementColor);
     elementColor.style.backgroundColor = cores[i];
-  } 
+  }
 }
 createColors();
 
@@ -25,6 +25,34 @@ function addBlocks() {
 addBlocks();
 
 window.onload = function () {
-  const colorInitial = document.querySelector('.color');
-  colorInitial.classList.add('selected');
+  const colorInitial = document.querySelectorAll('.color');
+  colorInitial[0].classList.add('selected');
 };
+
+function selectedClass() {
+  const elementClass = document.querySelectorAll('.color');
+  for (let i = 0; i < elementClass.length; i += 1) {
+    elementClass[i].addEventListener('click', () => {
+      for (let x = 0; x < elementClass.length; x += 1) {
+        const elementX = elementClass[x].classList;
+        if (elementX.contains('selected')) {
+          elementX.remove('selected');
+        }
+        elementClass[i].classList.add('selected');
+      }
+    });
+  }
+}
+selectedClass();
+
+function paintPixel() {
+  const pixels = document.querySelectorAll('.pixel');
+  for(let i = 0; i < pixels.length; i += 1) {
+    pixels[i].addEventListener('click', function () {
+      const selectedBlock = document.querySelector('.selected');
+      const colorBlock = window.getComputedStyle(selectedBlock, null).backgroundColor;
+      pixels[i].style.backgroundColor = colorBlock;
+    });
+  }
+}
+paintPixel();
