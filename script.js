a6949d0058679d96500b44899e142551268327bf
Vinicius <vinicius@DESKTOP-NBO50A7.localdomain>
2021-07-12 18:46:49 -0300

    Reiniciandos alguns tópicos

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+function createColors() {
+  let cores = ['black', 'red', 'blue', 'green']
+  const elementId = document.querySelector('#color-palette');
+  for (let i = 0; i < 4; i += 1) {
+    const elementColor = document.createElement('span');
+    elementColor.className = 'color';
+    elementId.appendChild(elementColor);
+    elementColor.style.backgroundColor = cores[i];
+  } 
+}
+createColors();
+
 function addBlocks() {
   const lines = 5;
   const colunms = 5;
@@ -13,43 +25,6 @@ function addBlocks() {
 addBlocks();
 
 window.onload = function () {
-  const colorInitial = document.querySelector('.colorBlack');
+  const colorInitial = document.querySelector('.color');
   colorInitial.classList.add('selected');
 };
-
-function selectColor() {
-  const colorSelected = document.querySelectorAll('.color');
-  for (let i = 0; i < colorSelected.length; i += 1) {
-    colorSelected[i].addEventListener('click', function () {
-      if (!colorSelected[i].classList.contains('selected')) {
-        for (let index = 0; index < colorSelected.length; index += 1) {
-          colorSelected[index].classList.contains('selected');
-          if (colorSelected[index].classList.contains('selected')) {
-            colorSelected[index].classList.remove('selected');
-          }
-        }
-        colorSelected[i].classList.add('selected');
-      } else colorSelected[i].classList.remove('selected');
-    });
-  }
-}
-selectColor();
-
-// function getColor() {
-//   const getColorSelected = document.querySelectorAll('.selected');
-//   const getColorActive = window.getComputedStyle(getColorSelected, null).background;         
-//   console.log(getColorActive);
-// }
-// getColor();
-
-function selectPixels() {
-  const pixelsWhite = document.querySelectorAll('.pixel');
-  for (let i = 0; i < pixelsWhite.length; i += 1) { 
-    pixelsWhite[i].addEventListener('click', function () {
-    pixelsWhite[i].classList.add('selected');
-    pixelsWhite[i].style.backgroundColor = 'black';
-    console.log(pixelsWhite[i].style.backgroundColor);
-    });
-  }
-}
-selectPixels();
