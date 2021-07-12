ebc24bcdbc3b3d5e127ad3459ee07e780bbee842
Ali21ne <bezerraalineg@gmail.com>
2021-07-12 17:11:48 -0300

    requisito 8 e refatoraçao do 7

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-function addSelected (event){
-    document.querySelector('.selected').classList.remove('selected');
-    event.target.classList.add('selected');
-};
-document.getElementsByClassName('color')[0].addEventListener("click", addSelected);
-document.getElementsByClassName('color')[1].addEventListener("click", addSelected);
-document.getElementsByClassName('color')[2].addEventListener("click", addSelected);
-document.getElementsByClassName('color')[3].addEventListener("click", addSelected);
+let black = document.querySelector('.black').style.backgroundColor = 'black';
+let yellow = document.querySelector('.yellow').style.backgroundColor = 'yellow';
+let red = document.querySelector('.red').style.backgroundColor = 'red';
+let green = document.querySelector('.green').style.backgroundColor = 'green'; 
+
+function addSelected(event) {
+  const selected1 = document.querySelector('.selected');
+  selected1.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+const colors = document.querySelector('#color-palette');
+colors.addEventListener('click', addSelected);
+
+function changeColor(event) {
+  const cor = document.querySelector('.selected');
+  event.target.style.backgroundColor = cor.style.backgroundColor;
+}
+const caixaDeQuadrados = document.querySelector('#pixel-board');
+caixaDeQuadrados.addEventListener('click', changeColor);
