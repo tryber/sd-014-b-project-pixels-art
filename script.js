47bced3fcf603c68e2b3bc3f985a3596ea2c2e8b
Paulo França <paulovinicius.franca@gmail.com>
2021-07-12 09:40:36 -0300

    Cria o Clear Button

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ window.onload = function getBlackColor() {
   getBlack.classList.add('selected');
 };
 
+function createClearButton() {
+  const buttonSection = document.createElement('section');
+  document.querySelector('body').appendChild(buttonSection);
+  const button = document.createElement('button');
+  button.type = "button";
+  button.id = 'clear-board';
+  button.innerText = 'Limpar';
+  buttonSection.appendChild(button);
+  button.addEventListener('click', clearPixelTable);
+}
+
+createClearButton();
+
 const pixelTableSize = 5;
 
 function createPixelBoard() {
@@ -47,6 +60,7 @@ function selectColor(color) {
 }
 
 const getPixel = document.querySelectorAll('.pixel');
+
 for (let index = 0; index <= getPixel.length - 1; index += 1) {
   let selected = getPixel[index].addEventListener('click', changeColor);
 }
@@ -54,4 +68,8 @@ for (let index = 0; index <= getPixel.length - 1; index += 1) {
 function changeColor(pixel) {
   const currentSelectedColor = document.querySelector('.selected');
   pixel.target.style.backgroundColor = currentSelectedColor.id;
+}
+
+function clearPixelTable(){
+  for (let index = 0; index <= getPixel.length-1;  )
 }
