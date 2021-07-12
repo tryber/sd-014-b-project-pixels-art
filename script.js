2b591eee84ec43267ad0c0e6b9354908188925c4
Elton Rodrigues <el_dantas1999@hotmail.com>
2021-07-12 16:33:24 -0300

    Requisito #8 - Correção lint e indentação.

--- script.js
+++ script.js
@@ -1,38 +1,36 @@
-let black = document.querySelector('#black');
-black.addEventListener('click', function () {
-  let element = document.querySelector('.selected');
+const black = document.querySelector('#black');
+black.addEventListener('click', () => {
+  const element = document.querySelector('.selected');
   element.classList.remove('selected');
   black.className = 'color selected';
 });
 
-let blue = document.querySelector('#blue');
-blue.addEventListener('click', function () {
-  let element = document.querySelector('.selected');
+const blue = document.querySelector('#blue');
+blue.addEventListener('click', () => {
+  const element = document.querySelector('.selected');
   element.classList.remove('selected');
   blue.className = 'color selected';
 });
 
-let green = document.querySelector('#green');
-green.addEventListener('click', function () {
-  let element = document.querySelector('.selected');
+const green = document.querySelector('#green');
+green.addEventListener('click', () => {
+  const element = document.querySelector('.selected');
   element.classList.remove('selected');
   green.className = 'color selected';
 });
 
-let yellow = document.querySelector('#yellow');
-yellow.addEventListener('click', function () {
-  let element = document.querySelector('.selected');
+const yellow = document.querySelector('#yellow');
+yellow.addEventListener('click', () => {
+  const element = document.querySelector('.selected');
   element.classList.remove('selected');
   yellow.className = 'color selected';
 });
 
 function colorBlock() {
-  const selectedItem = document.querySelectorAll('.selected');
-  const selectedColor = document.querySelectorAll('.color');
   const colorTable = document.querySelectorAll('#pixel-board .pixel');
   colorTable.forEach((click) => {
     click.addEventListener('click', (event) => {
-      let getColor = event.target.style;
+      const getColor = event.target.style;
       getColor.backgroundColor = window
         .getComputedStyle(document.querySelector('.selected'))
         .getPropertyValue('background-color');
