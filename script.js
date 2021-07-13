6637d317ec1b122d903a66c7f0cd86e882d06f85
Thiago Gomez <thiagogomez2906@hotmail.com>
2021-07-12 21:01:52 -0300

    qse surtando

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
+window.onload=function(){
 let linha =document.querySelectorAll(".line");
-var brek=document.createElement("br");
-let cet=linha[index];
-selectedCor();
+let cor='black'
+changedcor(cor);
+finalcor(cor);
+function changedcor(cor){
+document.querySelectorAll(".color").forEach(unit=>{
+    unit.addEventListener('click',selecionaCor=>{
+        let cor= unit.id
+       
+    });
+})}
+
 readline(linha);
+
+function finalcor(cor){
+document.querySelectorAll(".pixel").forEach(item=>{
+    item.addEventListener("click",mudacor=>{
+        item.styles.backgroundColor= cor
+    })
+})}
+
 function createPixel(className){
     let pixel=document.createElement("div");
     pixel.className=className;
     return pixel;
 }
-function addpixel(cet){
+
+function addpixel(divLine){
     for (let v=1;v<=5;v++){ 
-        cet.appendChild(createPixel("pixel"));
+        divLine.appendChild(createPixel("pixel"));
         }
     }
 
 function readline(linha){
     for(let index=0;index<linha.length;index+=1){
-        addpixel(linha[index]);
-       linha.appendChild(brek);
+        addpixel(linha[index]);      
     }
 }
 
+}
