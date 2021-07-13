76a1a5ad7ab3a448cc496235a5435c35f6e2a13c
Riba Jr <ribastudio@gmail.com>
2021-07-13 00:53:35 -0300

    Requisito pinta todos os blocos de branco cumprido

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const myBoard = document.getElementById('pixel-board');
+let myBoard = document.getElementById('pixel-board');
 const palette = document.getElementById('color-palette');
 const btnVQV = document.getElementById('generate-board');
 const btnClean = document.getElementById('clear-board');
 const inputBoard = document.querySelector('#board-size');
-const colorArray = palette.children;
 
 // função vista - tem explicação de como funciona - aqui, https://wallacemaxters.com.br/blog/2021/02/20/como-gerar-cores-aleatorias-no-javascript. Cores hexadecimais vão de 0 a 0xFFFFFF. Usa-se a high level function Math.random, que gera valores de 0 a 'quase' 1 dentro de uma função parseInt, que 'força' o número a ser um inteiro. Após isso, ele chama o método toString, que retorna uma string representando um objeto - no nosso caso, gerado pelo resultado da função parseInt. Também passa um método padStart, que preenche a string com um determinado número de caracteres e sinaliza qual caractere deve ser usado (o zero, no nosso caso).
 
@@ -21,15 +20,9 @@ function generateColor() {
       y += 1;
       z += 1;
     }
-  palette.children[i].style.backgroundColor = `rgb(${x}, ${y}, ${z})`;
+    palette.children[i].style.backgroundColor = `rgb(${x}, ${y}, ${z})`;
   }
 }
-
-/*
-função antiga random colors
-return `#${parseInt((Math.random() * 0xFFF))
-  .toString(16)
-  .padStart(6, '0')}`; */
 
 function initialColors() {
   const firstColor = palette.firstElementChild;
@@ -38,19 +31,12 @@ function initialColors() {
   generateColor();
 }
 
-function generateBoard(numero) {
-  numero = inputBoard.value;
-  // numero = 5;
-  if (numero < 5 && numero !== '') {
-    numero = 5;
-  } else if (numero > 50) {
-    numero = 50;
-  }
-  for (let i = 0; i < numero; i += 1) {
-    const div = document.createElement('div');
+function generateBoard() {
+  for (let i = 0; i < inputBoard.value; i += 1) {
+    let div = document.createElement('div');
     div.classList.add('pixel-line');
     myBoard.appendChild(div);
-    for (let i2 = 0; i2 < numero; i += 1) {
+    for (let i2 = 0; i2 < inputBoard.value; i += 1) {
       let divPixel = document.createElement('div');
       divPixel.classList.add('pixel');
       div.appendChild(divPixel);
@@ -62,8 +48,13 @@ myBoard.addEventListener('click', clickSelectColor);
 
 palette.addEventListener('click', clickPaintColor);
 
+btnVQV.addEventListener('click', generateBoard);
+
 function clickSelectColor(event) {
   event.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
+  if (event.target === myBoard) {
+    event.target.style.backgroundColor = myBoard.style.backgroundColor = 'initial';
+  }
 }
 
 function clickPaintColor(event) {
@@ -74,14 +65,14 @@ function clickPaintColor(event) {
 
 // função que limpa toda a tabela, baseado no exercício da pirâmide
 function erasePixelTable() {
-  const childMyBoard = myBoard.children;
-  childMyBoard.style.backgroundColor = 'white';
-  console.log(childMyBoard);
+  for (let i = 0; i < myBoard.children.length; i += 1) {
+    myBoard.children[i].style.backgroundColor = 'white';
+  }
 }
 
 window.onload = function () {
   initialColors();
-  generateBoard();
+  // generateBoard();
   btnClean.addEventListener('click', erasePixelTable);
-  btnVQV.addEventListener('click', generateBoard);
+  //
 };
