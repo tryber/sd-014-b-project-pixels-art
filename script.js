78a1a4f3cf806512549caa7ea262cb3042ed957b
Riba Jr <ribastudio@gmail.com>
2021-07-12 22:48:35 -0300

    requisito 3, refatorado, implemetando requisito 4, requisito 5 e requisito 6

--- script.js
+++ script.js
@@ -1,56 +1,72 @@
-const myBoard = document.getElementById('#pixel-board');
-const palette = document.getElementById('#color-palette');
+const myBoard = document.getElementById('pixel-board');
+const palette = document.getElementById('color-palette');
 const btnVQV = document.getElementById('generate-board');
-const inputBoard = document.getElementById('board-size');
+const btnClean = document.getElementById('clear-board');
+const inputBoard = document.querySelector('#board-size');
+const colorArray = palette.children;
 
 // função vista - tem explicação de como funciona - aqui, https://wallacemaxters.com.br/blog/2021/02/20/como-gerar-cores-aleatorias-no-javascript. Cores hexadecimais vão de 0 a 0xFFFFFF. Usa-se a high level function Math.random, que gera valores de 0 a 'quase' 1 dentro de uma função parseInt, que 'força' o número a ser um inteiro. Após isso, ele chama o método toString, que retorna uma string representando um objeto - no nosso caso, gerado pelo resultado da função parseInt. Também passa um método padStart, que preenche a string com um determinado número de caracteres e sinaliza qual caractere deve ser usado (o zero, no nosso caso).
 
 function generateColor() {
-  return `#${parseInt((Math.random() * 0xFFF))
-    .toString(16)
-    .padStart(6, '0')}`;
+  for (let i = 1; i < palette.children.length; i += 1) {
+    const x = Math.round(Math.random() * (255 - 1));
+    const y = Math.round(Math.random() * (255 - 1));
+    const z = Math.round(Math.random() * (255 - 1));
+    if (x === 255 && y === 255 && z === 255) {
+      x -= 1;
+      y -= 1;
+      z -= 1;
+    }
+    palette.children[i].style.backgroundColor = `rgb(${x}, ${y}, ${z})`;
+  }
+// Math.round - verificar cor aleatoria
 }
+/*
+função antiga random colors
+return `#${parseInt((Math.random() * 0xFFF))
+  .toString(16)
+  .padStart(6, '0')}`; */
 
-function initialColor() {
-  const firstColor = document.querySelector('.color');
+function initialColors() {
+  const firstColor = palette.firstElementChild;
+  firstColor.style.backgroundColor = 'black';
   if (firstColor.style.backgroundColor !== 'black') {
-    firstColor.style.backgroundColor = 'black';
+    firstColor.style.backgroundColor = palette.children[0];
+    firstColor.classList.add('selected');
   }
-  for (let i = 1; i < palette.length; i += 1) {
-    const element = palette.childNodes[i];
-      element.style.backgroundColor = generateColor();
-    
-  }
-  
+  generateColor();
 }
 
-window.onload = function() {
-  initialColor();
+
+window.onload = function () {
+  initialColors();
   generateBoard();
-  btnVQV.addEventListener('click', erasePixelTable);
+  btnClean.addEventListener('click', erasePixelTable);
+  btnVQV.addEventListener('click', generateBoard);
 };
 
 function generateBoard(numero) {
+  numero = inputBoard.value;
+  if (numero === '') {
 
+  }
 }
 
 function generateBoardLine() {
   const newPixel = document.createElement('div');
   newPixel.classList.add('.pixel');
   myBoard.appendChild(newPixel);
-
 }
 
-
-
 function clickPaintColor(event) {
-  let actualColor = document.querySelector(".selected");
+  const actualColor = document.querySelector('.selected');
   actualColor.classList.remove('selected');
   event.target.classList.add('selected');
 }
 
-//função que limpa toda a tabela, baseado no exercício da pirâmide
+// função que limpa toda a tabela, baseado no exercício da pirâmide
 function erasePixelTable() {
-  
-
+  const childMyBoard = myBoard.children;
+  childMyBoard.style.backgroundColor = 'white';
+  console.log(childMyBoard);
 }
