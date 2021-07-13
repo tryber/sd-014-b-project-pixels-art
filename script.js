3f4fa10fd4123b6dd65e3ce8ba9cd4156416d6e3
S4bot4 <sabotamail@gmail.com>
2021-07-13 12:05:50 -0300

    Cumprido requisito 7

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Recupera as cores backgroundcolor da Color-Palette
 const initialColors = document.getElementsByClassName('color');
 initialColors[0].style.backgroundColor = 'black';
 initialColors[1].style.backgroundColor = 'yellow';
@@ -5,49 +6,46 @@ initialColors[2].style.backgroundColor = 'red';
 initialColors[3].style.backgroundColor = 'green';
 
 
+
+// add event listen para remover a classe .selected
+initialColors[0].addEventListener('click', addClass);
+initialColors[1].addEventListener('click', addClass);
+initialColors[2].addEventListener('click', addClass);
+initialColors[3].addEventListener('click', addClass);
+
+// criar funcao com itens
 // Requisito 7
-function mouseEmCima(evento) {
-  console.log('Mouse passou por cima');
-evento.target
-console.log(evento.target);
+
+function addClass(event) {
+  for (let index = 0; index < initialColors.length; index += 1) {
+ //   console.log(initialColors[index].classList.contain('selected'));
+//    if (initialColors[index].classList.includes('selected')) {
+  initialColors[index].classList.remove('selected');    
+console.log('clicou e removeu');
+    }
+  event.target.classList.add('selected');
 }
-// Funcao para remover a classe "Selected"
-function removeClass(evento) {
-  evento.target.classList.add('.selected');
-  evento.target.classList.remove('.selected');
-  console.log(evento.target);
-  console.log(evento.type);
-}
-const remColor = document.querySelector('.selected');
-remColor.addEventListener('click', removeClass);
-remColor.addEventListener('mouseover', mouseEmCima);
 
 
-// trocar de classse  e adicionar em outra cor a calsse "Selected" Requisito 8
-const deletedColor = document.querySelectorAll('.color');
-function selectedColor(event) {
-  for (let index = 0; index < deletedColor.length; index += 1) {
-    deletedColor[index].addEventListener('click', removeClass);
-  }
-}
+// Requisito 8 trocar de classse removendo a classe "Selected"  e adicionar em outra cor a classe.
+
 // Direciona o pixel a ser pintado
 const blocksToPaint = document.getElementsByClassName('pixel');
-function paintColor(obj) {
-  obj.target.style.backgroundColor = 'yellow';
+const colorSelected = document.querySelector('.selected');
+function paintColor(p) {
+  p.target.style.backgroundColor = 'yellow';
 }
-// Seleciona o clique para pintar
+// usar initalColors para poder pintar fazendo com que remColor troque o .selected e use a cor para ser pintada adicionando
+// Seleciona o quadrado de pixel que vai ser clicado e adicionado a cor selecionada
 for (let index = 0; index < blocksToPaint.length; index += 1) {
   blocksToPaint[index].addEventListener('click', paintColor);
 }
 
-// Requisito 9
-
-// limpar background / class pixel
+// Requisito 9  limpar background / class pixel
 const bgwall = document.querySelectorAll('.pixel');
 function clearColors() { 
   for (let index = 0; index < bgwall.length; index += 1) {
     bgwall[index].style.backgroundColor = 'white';
-    console.log(bgwall);
   }
 }
 // Requisito 10
