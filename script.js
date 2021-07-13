4e143b536d0981d0129eaf54f6aa228b9d5a2e8c
Paola Gay <paolagaydossantos@gmail.com>
2021-07-12 21:04:10 -0300

    Arruma os primeiros itens (-2) e cria tabela

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 
-let linha = document.createElement("tr");
-let coluna = Document.createElement("th");
+ 
+    let table = document.createElement('table');
+    table.id = 'pixel-board';
+    table.setAttribute('border', '1');
+    for (let indexTr = 0; indexTr < 5; indexTr += 1) {
+      let tr = document.createElement('tr');
+      for (let indexTd = 0; indexTd < 5; indexTd += 1) {
+        if (indexTr == 5 && indexTd == 5) {
+          break;
+        } else {
+          let td = document.createElement('td');
+          td.className = 'pixel';
+          td.style.background = 'white';
+          tr.appendChild(td);      
+        } 
+      }
+      table.appendChild(tr);
+    }
+    document.getElementById('tabela').appendChild(table);
+ 
