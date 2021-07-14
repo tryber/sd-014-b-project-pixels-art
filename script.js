da9b203dd559f374e9cd421c0d19b0023a6aeb8c
Lucas-GSS <lucasgabrieldesouza9@gmail.com>
2021-07-14 09:56:34 -0300

    Refatora o requisto 6. Adciona o requisito 7

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-const initialColor = document.getElementById('black');
+const initialColor = document.querySelector('#black');
 initialColor.classList.add('selected');
+
+const colorPalette = document.querySelectorAll('.color');
+
+function setClass(event) {
+  document.querySelector('.selected').classList.remove('selected');
+  event.target.classList.add('selected');
+  console.log(event);
+}
+
+for (let index = 0; index < colorPalette.length; index += 1) {
+  colorPalette[index].addEventListener('click', setClass);
+}
