4753b39955e1b2ebddeb6af136094bf12a41592d
Riba Jr <ribastudio@gmail.com>
2021-07-14 14:46:26 -0300

    Entrega do projeto Pixels Art, após verificação no GitHub

--- script.js
+++ script.js
@@ -7,20 +7,34 @@ const inputBoard = document.querySelector('#board-size');
 
 // função vista - tem explicação de como funciona - aqui, https://wallacemaxters.com.br/blog/2021/02/20/como-gerar-cores-aleatorias-no-javascript. Cores hexadecimais vão de 0 a 0xFFFFFF. Usa-se a high level function Math.random, que gera valores de 0 a 'quase' 1 dentro de uma função parseInt, que 'força' o número a ser um inteiro. Após isso, ele chama o método toString, que retorna uma string representando um objeto - no nosso caso, gerado pelo resultado da função parseInt. Também passa um método padStart, que preenche a string com um determinado número de caracteres e sinaliza qual caractere deve ser usado (o zero, no nosso caso).
 
+// transformado em função, após monitoria do Iago
+function verifyBlack(x, y, z) {
+  if (x === 255 && y === 255 && z === 255) {
+    x -= 1;
+    y -= 1;
+    z -= 1;
+  }
+}
+
+function verifyWhite(x, y, z) {
+  if (x === 0 && y === 0 && z === 0) {
+    x += 1;
+    y += 1;
+    z += 1;
+  }
+}
+
+function generateRGB() {
+  return Math.round(Math.random() * (255 - 1));
+}
+
 function generateColor() {
   for (let i = 1; i < palette.children.length; i += 1) {
-    const x = Math.round(Math.random() * (255 - 1));
-    const y = Math.round(Math.random() * (255 - 1));
-    const z = Math.round(Math.random() * (255 - 1));
-    if (x === 255 && y === 255 && z === 255) {
-      x -= 1;
-      y -= 1;
-      z -= 1;
-    } else if (x === 0 && y === 0 && z === 0) {
-      x += 1;
-      y += 1;
-      z += 1;
-    }
+    const x = generateRGB();
+    const y = generateRGB();
+    const z = generateRGB();
+    verifyBlack(x, y, z);
+    verifyWhite(x, y, z);
     palette.children[i].style.backgroundColor = `rgb(${x}, ${y}, ${z})`;
   }
 }
@@ -32,17 +46,13 @@ function initialColors() {
   generateColor();
 }
 
-function generateBoard() {
-  inputBoard.value = 5;
-  if (inputBoard.value === '') {
-    alert('Não há valor para fazer nada');
-  }
-  for (let line = 0; line < inputBoard.value; line += 1) {
-    let oldDiv = document.querySelectorAll('.pixel-line');
+function generateBoard(value) {
+  myBoard.innerHTML = '';
+  for (let line = 0; line < value; line += 1) {
     let div = document.createElement('div');
     div.classList.add('pixel-line');
     myBoard.appendChild(div);
-    for (let element = 0; element < inputBoard.value; element += 1) {
+    for (let element = 0; element < value; element += 1) {
       let divPixel = document.createElement('div');
       divPixel.classList.add('pixel');
       div.appendChild(divPixel);
@@ -50,24 +60,36 @@ function generateBoard() {
   }
 }
 
+function verifyInput() {
+  if (inputBoard.value.length === 0 || inputBoard.value.length === '') {
+    alert('Board inválido!');
+  }
+  if (inputBoard.value > 50) {
+    alert('Board inválido!'); // retirar
+    generateBoard(50);
+  } 
+  if (inputBoard.value < 5) {
+    alert('Board inválido!');
+    generateBoard(5);
+  } else {
+    generateBoard(inputBoard.value);
+  }
+  
+}
+
 myBoard.addEventListener('click', clickSelectColor);
 
 palette.addEventListener('click', clickPaintColor);
 
-btnVQV.addEventListener('click', generateBoard);
+btnVQV.addEventListener('click', verifyInput);
 
 btnLucky.addEventListener('click', generateColor);
 
 btnClean.addEventListener('click', erasePixelTable);
 
 function clickSelectColor(event) {
-  event.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
-  const boardDefault = myBoard.style.backgroundColor = 'initial';
-  const lineDefault = document.querySelectorAll('.pixel-line').style.backgroundColor = 'white';
-
-  if (event.target === myBoard) {
-    event.target.style.backgroundColor = boardDefault;
-    event.target.style.backgroundColor = lineDefault;
+  if (event.target.classList.contains('pixel')) {
+    event.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
   }
 }
 
@@ -80,12 +102,20 @@ function clickPaintColor(event) {
 // função que limpa toda a tabela, baseado no exercício da pirâmide
 function erasePixelTable() {
   const lineDefault = document.querySelectorAll('.pixel');
-  for (let i = 0; i < myBoard.children.length; i += 1) {
-    lineDefault.item[i].style.backgroundColor = 'white';
+  for (let i = 0; i < lineDefault.length; i += 1) {
+    lineDefault[i].style.backgroundColor = 'white';
   }
 }
 
+inputBoard.addEventListener('keyup', (enter) => {
+  const key = enter.wich || enter.key;
+  if (key === 'Enter' || key === 3 || key === 13) {
+    generateBoard(inputBoard.value);
+    inputBoard.value = '';
+  }
+});
+
 window.onload = function () {
   initialColors();
-  generateBoard();
+  generateBoard(5);
 };
