a5d849717d46ed931dcddd41964dd48647687ef9
Lucas-GSS <lucasgabrieldesouza9@gmail.com>
2021-07-14 10:57:36 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -6,9 +6,19 @@ const colorPalette = document.querySelectorAll('.color');
 function setClass(event) {
   document.querySelector('.selected').classList.remove('selected');
   event.target.classList.add('selected');
-  console.log(event);
 }
 
 for (let index = 0; index < colorPalette.length; index += 1) {
   colorPalette[index].addEventListener('click', setClass);
+}
+
+const pixels = document.querySelectorAll('.pixel');
+const selected = document.getElementsByClassName('selected');
+
+function toColor(event) {
+  event.target.style.backgroundColor = selected[0].id;
+}
+
+for (let index = 0; index < pixels.length; index += 1) {
+  pixels[index].addEventListener('click', toColor);
 }
