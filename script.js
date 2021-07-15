1cf0cb0ee0026b3bc86ef6bb7d7ba4b9890dba2c
Mayara Andronico <mayandronico@msn.com>
2021-07-15 00:08:22 -0300

    req 7

--- script.js
+++ script.js
@@ -1,50 +1,14 @@
-// Requisito 7
-
-/// A classe selected sai de uma cor e passa pra outra com o clique
-
-const colorPalette = document.querySelector('#color-palette');
-const tablePixels = document.querySelector('#pixel-board');
-const buttonClear = document.querySelector('#clear-board');
-var allCollors = document.querySelector('#pixel-board');
-var cliquePixel = document.querySelector('.pixel');
-
-function removeSelectedClass() {
-  var cliquePixel = document.querySelectorAll('#color-palette');
-  for (let index = 0; index < cliquePixel.length; index += 1) {
-    if (cliquePixel[index].classList.contains('selected')) {
-     cliquePixel[index].classList.remove('selected');
-    }
+// Requisito 7 //referencia https://stackoverflow.com/questions/50883690/remove-class-from-one-element-while-adding-that-class-to-another-element-using
+let getColors = document.querySelectorAll('.color');
+for (let index = 0; index < getColors.length; index += 1) { 
+  GetColors[index].addEventListener('click', classColor); 
+   function classColor(event) { 
+   limpaSelect(); 
+    event.target.classList.add('selected'); 
   }
 }
-colorPalette.addEventListener('click', (element) => {
-  removeSelected();
-  element.target.classList.add('selected');
-});
-
-function colorRGB(color) {
-  switch (color) {
-  case 'black':
-    return 'rgb(0, 0, 0)';
-  case 'orange':
-    return 'rgb((255,165,0)';
-  case 'red':
-    return 'rgb((255,69,0)';
-  case 'blue':
-    return 'rgb(0,0,255)';
-  default:
-    return null;
+function limpaSelect(){
+  for (let index = 0; index < getColors.length; index += 1) { 
+    getColors[index].classList.remove('selected');
   }
 }
-/// requisito 9
-/// Criar um botão com o texto "limpar" - ok
-/// O botão precisa ter a função de limpar a cor do pixel e deixar branco
-/// A cor da paleta é selecionada com clique e passa para a tabela de pixels
-/// Adicionar ao botão um evento de clique
-
-clearButton.addEventListener('click', () => {
-  removeSelected();
-  const tablePixels= colorPalette.children[0];
-  tablePixels.classList.add('selected');
-
-  tablePixels.innerHTML = '';
-});
