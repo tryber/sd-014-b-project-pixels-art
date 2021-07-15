c3babe76a1bbd13facbbb25beebe49d42214fbe6
AntonioJSCardoso <antonio.js.cardoso26@gmail.com>
2021-07-15 12:51:03 -0300

    Adicionando item 6

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-
- //clicarParaPintar
- 
-
 //BotãoLimpar
-let buttonClear = document.getElementById('clear-board');
-buttonClear.addEventListener('click', function() {
-    let whites = document.getElementsByClassName('.pixel');
-    for(let index = 0; index < whites.length; index += 1)
-    if (whites[index].style.backgroundcolor === document.querySelector('#black') || 
-    whites[index].style.backgroundcolor === document.querySelector('#orange') || 
-    whites[index].style.backgroundcolor === document.querySelector('#blue') || 
-    whites[index].style.backgroundcolor === document.querySelector('#pink')) {
-        whites[index].style.backgroundcolor = 'white';
+
+const btclear = document.getElementById('clear-board');
+btclear.addEventListener('click', clearAll);
+
+function clearAll () {
+
+    let clean = document.querySelectorAll('.pixel');
+    for (let index = 0; index < clean.length; index =+ 1) {
+        clean[index].style.background = 'white';
     }
 
-});
+}
