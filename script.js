06632c014d059ae1372c43d7fef3f12d832d227c
AntonioJSCardoso <antonio.js.cardoso26@gmail.com>
2021-07-15 11:59:18 -0300

    Adicionando as funçoes

---
+++ script.js
@@ -0,0 +1,16 @@
+//BotãoLimpar
+let buttonClear = document.getElementById('clear-board');
+buttonClear.addEventListener('click', function() {
+    let whites = document.getElementsByClassName('.pixel');
+    for(let index = 0; index < whites.length; index += 1)
+    if (whites[index].style.backgroundcolor === document.querySelector('#black') || 
+    whites[index].style.backgroundcolor === document.querySelector('#orange') || 
+    whites[index].style.backgroundcolor === document.querySelector('#blue') || 
+    whites[index].style.backgroundcolor === document.querySelector('#pink')) {
+        whites[index].style.backgroundcolor = 'white';
+    }
+
+});
+ //clicarParaPintar
+
+ 
