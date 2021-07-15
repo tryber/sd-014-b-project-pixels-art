938e829f7d67547c9bb86b3cb94896699ac90c4b
Mayara Andronico <mayandronico@msn.com>
2021-07-15 03:20:17 -0300

    req7

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-// Requisito 7 //referencia https://stackoverflow.com/questions/50883690/remove-class-from-one-element-while-adding-that-class-to-another-element-using
-let getColors = document.querySelectorAll('.color');
+// Requisito 7 //
+const getColors = document.querySelectorAll('.color');
+
 for (let index = 0; index < getColors.length; index += 1) { 
-  GetColors[index].addEventListener('click', classColor); 
+  getColors[index].addEventListener('click', classColor); 
    function classColor(event) { 
    limpaSelect(); 
     event.target.classList.add('selected'); 
