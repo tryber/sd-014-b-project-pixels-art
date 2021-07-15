3b7291be543aedeba3cb5d939ebfb144a0acc0ca
AntonioJSCardoso <antonio.js.cardoso26@gmail.com>
2021-07-15 18:11:35 -0300

    adicionando preto como inicial e limpando quadro

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+window.onload = function () {
+    let colorFirst = document.getElementById('black');
+    colorFirst.classList.add('selected');
+  };
 
+  function paint(evento) {
+    let color = document.querySelector('.selected');
+    evento.target.style.backgroundColor = color.id;
+  };
 
+  let pixels = document.querySelectorAll('.pixel');
+for (let i = 0; i < pixels.length; i++) {
+  pixels[i].addEventListener('click', paint);
+}
 
+let cleaning = document.querySelector('#clear-board');
+cleaning.addEventListener('click', allDelete);
+
+function allDelete() {
+  for (let i = 0; i < pixels.length; i++) {
+    pixels[i].style.backgroundColor = 'white';
+  }
+};
