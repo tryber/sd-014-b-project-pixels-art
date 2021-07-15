bea4bbe0ef020e84a16f7b2252698cb92fab001e
bruholanda <brunaholandacunha7@gmail.com>
2021-07-15 11:33:59 -0300

    Requirements #1 to #7; Started over;

--- script.js
+++ script.js
@@ -1,66 +1,23 @@
-window.onload = function teste() {
-  const select = document.getElementById('black');
-  select.classList.add('selected');
-}
+window.onload = function () {
+  let colorSelected = document.getElementById('black');
+  colorSelected.classList.add('selected');
+};
 
-function clickRed() {
-  document.querySelector('#black').classList.remove('selected');
-  document.querySelector('#blue').classList.remove('selected');
-  document.querySelector('#green').classList.remove('selected');
-  document.querySelector('#red').classList.add('selected');
-  alert('vermelho');
-}
-
-function clickBlack() {
-  document.querySelector('#red').classList.remove('selected');
-  document.querySelector('#blue').classList.remove('selected');
-  document.querySelector('#green').classList.remove('selected');
-  document.querySelector('#black').classList.add('selected');
-  alert('preto');
-}
-
-function clickBlue() {
-  document.querySelector('#black').classList.remove('selected');
-  document.querySelector('#red').classList.remove('selected');
-  document.querySelector('#green').classList.remove('selected');
-  document.querySelector('#blue').classList.add('selected');
-  alert('azul');
-}
-
-function clickGreen() {
-  document.querySelector('#black').classList.remove('selected');
-  document.querySelector('#blue').classList.remove('selected');
-  document.querySelector('#red').classList.remove('selected');
-  document.querySelector('#green').classList.add('selected');
-  alert('verde');
-}
-
-const pixels = document.querySelectorAll('.pixel');
-
-function painting() {
-  for (let index = 0; index < pixels.length; index++) {
-    pixels[index].style.backgroundColor = 'white';
+function clickColor(evento) {
+  let colors = document.getElementsByClassName('color');
+  for (let index = 0; index < colors.length; index += 1) {
+    colors[index].classList.remove('selected');
   }
-}
+  evento.target.classList.add('selected');
+};
 
-function cleaning() {
-  for (let index = 0; index < pixels.length; index++) {
-    pixels[index].style.backgroundColor = 'white';
-  }
-}
+const clickRed = document.querySelector('#red');
+clickRed.addEventListener('click', clickColor);
+const clickBlue = document.querySelector('#blue');
+clickBlue.addEventListener('click', clickColor);
+const clickGreen = document.querySelector('#green');
+clickGreen.addEventListener('click', clickColor);
+const clickBlack = document.querySelector('#black');
+clickBlack.addEventListener('click', clickColor);
 
 
-const clickB = document.getElementById('black');
-clickB.addEventListener('click', clickBlack);
-
-const clickR = document.getElementById('red');
-clickR.addEventListener('click', clickRed);
-
-const clickBlu = document.getElementById('blue');
-clickBlu.addEventListener('click', clickBlue);
-
-const clickG = document.getElementById('green');
-clickG.addEventListener('click', clickGreen);
-
-const clean = document.getElementById('clear-board');
-clean.addEventListener('click', cleaning);
