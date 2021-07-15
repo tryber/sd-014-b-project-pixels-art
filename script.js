6d47f83688109e47d66abdbc47766a21ea3fb189
Patrick Paredes <patrickparedes44@gmail.com>
2021-07-14 23:47:35 -0400

    Correção html/js

--- script.js
+++ script.js
@@ -11,38 +11,37 @@ for (let index = 1; index <= 3; index += 1) {
 }
 
 const setColor = (thisColor) => {
-    colors.forEach((element, index) => {
-      if (index !== thisColor) {
-        element.className = 'color';
-      } else {
-        element.className = 'color selected';
-      }
-    });
+  colors.forEach((element, index) => {
+    if (index !== thisColor) {
+      element.className = 'color';
+    } else {
+      element.className = 'color selected';
+    }
+  });
 };
-  
+
 colors[0].addEventListener('click', () => setColor(0));
 colors[1].addEventListener('click', () => setColor(1));
 colors[2].addEventListener('click', () => setColor(2));
 colors[3].addEventListener('click', () => setColor(3));
 
 function actionPixels() {
-    const pixels = document.querySelectorAll('.pixel');
-  
-//  https://stackoverflow.com/questions/25238153/how-to-get-background-color-property-value-in-javascript
-    pixels.forEach((eachPixel) => {
-      eachPixel.addEventListener('click', () => {
-        const selectedColor = window
-          .getComputedStyle(document.querySelector('.selected'))
-          .getPropertyValue('background-color');
-        eachPixel.style.backgroundColor = selectedColor;
-      });
+  const pixels = document.querySelectorAll('.pixel');
+  //  https://stackoverflow.com/questions/25238153/how-to-get-background-color-property-value-in-javascript
+  pixels.forEach((eachPixel) => {
+    eachPixel.addEventListener('click', () => {
+      const selectedColor = window
+        .getComputedStyle(document.querySelector('.selected'))
+        .getPropertyValue('background-color');
+      eachPixel.style.backgroundColor = selectedColor;
     });
-  
-    const clear = document.querySelector('#clear');
-  
-    clear.addEventListener('click', () =>
-      pixels.forEach((eachPixel) => {
-        eachPixel.style.backgroundColor = 'white';
+  });
+
+  const clear = document.querySelector('#clear-board');
+
+  clear.addEventListener('click', () =>
+    pixels.forEach((eachPixel) => {
+      eachPixel.style.backgroundColor = 'white';
     }));
 }
 
