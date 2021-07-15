db55b8651484010f12b8ae96d9f62cde613bde4e
AntonioJSCardoso <antonio.js.cardoso26@gmail.com>
2021-07-15 18:37:30 -0300

    adicionando as pinturas dos pixels

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
+//Estabelecendo preto como principal
 window.onload = function () {
-    let colorFirst = document.getElementById('black');
-    colorFirst.classList.add('selected');
-  };
-
-  function paint(evento) {
-    let color = document.querySelector('.selected');
-    evento.target.style.backgroundColor = color.id;
-  };
-
-  let pixels = document.querySelectorAll('.pixel');
-for (let i = 0; i < pixels.length; i++) {
-  pixels[i].addEventListener('click', paint);
+  let firstColor = document.getElementById('black');
+  firstColor.classList.add('selected');
+}
+//Selecionando as cores
+function afterColor(evento) {
+  let oneColor = document.getElementsByClassName('color');
+  for (let index = 0; index < oneColor.length; index += 1) {
+    oneColor[index].classList.remove('selected');
+  }
+  evento.target.classList.add('selected');
 }
 
-let cleaning = document.querySelector('#clear-board');
-cleaning.addEventListener('click', allDelete);
+const blackColor = document.querySelector('#black');
+blackColor.addEventListener('click', afterColor);
+const blueColor = document.querySelector('#blue');
+blueColor.addEventListener('click', afterColor);
+const orangeColor = document.querySelector('#orange');
+orangeColor.addEventListener('click', afterColor);
+const pinkColor = document.querySelector('#pink');
+pinkColor.addEventListener('click', afterColor);
 
-function allDelete() {
+//pintando os pixels
+const pixelPaint = document.querySelectorAll('.pixel');
+for (let i = 0; i < pixelPaint.length; i++) {
+  pixelPaint[i].addEventListener('click', paint);
+}
+
+function paint(evento) {
+  let color = document.querySelector('.selected');
+  evento.target.style.backgroundColor = color.id;
+}
+
+//limpando o quadro
+const oneDelete = document.querySelector('#clear-board');
+oneDelete.addEventListener('click', deleteAll);
+
+function deleteAll() {
   for (let i = 0; i < pixels.length; i++) {
-    pixels[i].style.backgroundColor = 'white';
+    pixelPaint[i].style.backgroundColor = 'white';
   }
-};
+}
