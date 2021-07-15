036568d756d72e69e6ecc2246a490c7d6174e445
AntonioJSCardoso <antonio.js.cardoso26@gmail.com>
2021-07-15 16:31:48 -0300

    recomeçando JS

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
 
-function beginColor() {
-    let color = document.getElementById('black');
-    color.classList.add('selected');
-}
-//beginColor();
 
 
-
-//BotãoLimpar
-
-let btclear = document.querySelector('#clear-board');
-btclear.addEventListener('click', clearAll);
-
-function clearAll () {
-     document.querySelector('#pixel-board').style.backgroundColor = 'white';
-        
-}
