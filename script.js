809f89d18fd3fbb08dd2419492b105ca6f4e10a0
Vinicius TnK <viniciusbtanaka@hotmail.com>
2021-07-16 19:24:41 -0400

    add requisito #7

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let container = document.getElementById('pixel-board');
 let containerLines = container.children;
 let containerSize = 5;
 
+window.onload = AddColorListeners;
+
 for (let i = 0; i < containerSize; i++)
 {
     let line = document.createElement('div');
@@ -15,5 +17,35 @@ for (let i = 0; i < containerSize; i++)
         let pixel = document.createElement('span');
         pixel.className = 'pixel border';
         containerLines[i].appendChild(pixel);
+    }
+}
+
+let colorPalette = document.getElementById('color-palette');
+let colorSize = colorPalette.children.length;
+let color = colorPalette.children;
+
+function AddColorListeners() 
+{
+    for (let i = 0; i < colorSize; i++)
+    {
+        console.log(color[i]);
+        color[i].addEventListener('click', ChangeSelect);
+    }    
+}
+
+function ChangeSelect(param)
+{
+    param.target.classList.forEach(element => 
+    {
+        if (element == 'selected') return;        
+    });
+
+    param.target.classList.add('selected')
+
+    for (let i = 0; i < colorSize; i++)
+    {
+        if (param.target == color[i]) continue;
+
+        color[i].classList.remove('selected')
     }
 }
