df123f123c2b386ce9f8a2d078fb4bf73d119bb4
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-07-16 13:17:55 -0300

    Requesito #7 e #8.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+//Faz com que a paleta de coores possa ser funcional somente depois que a página estiver carregada.
 window.onload = function() {
-  function paletteColor() {
+  function paletteColor() { //Função que cria uma paleta de cores que já foram definidas.
     const getPalette = document.getElementsByClassName('color');
     for (let index of getPalette) {
       if (index === getPalette[0]) {
@@ -11,7 +12,9 @@ window.onload = function() {
       } else if (index === getPalette[3]) {
         index.style.background = 'yellow';
       }
+      //Aproveitando o mesmo for para adicionar o evento de click aos quadrados de cores da paleta de cores. 
       index.addEventListener('click', function(event) {
+        //Esse segundo loop adiciona a class selected onde não tem quando clicado e remove do outro que tinha.
         for (let indexTwo = 0; indexTwo < getPalette.length; indexTwo += 1) {
           getPalette[indexTwo].classList.remove('selected');
           event.target.classList.add('selected');
@@ -21,7 +24,7 @@ window.onload = function() {
   }
   paletteColor();
 }
-
+//Função para criar o quadro de pixels dinamicamente.
 function creatPixilsBoard() {
   const pixelsBoard = document.querySelector('#pixel-board');
   for (let index = 1; index <= 5; index += 1) {
@@ -37,3 +40,10 @@ function creatPixilsBoard() {
 
 creatPixilsBoard();
 
+//Requesito 08 onde adiciona o evento de click a tabela de pixel e a função faz com que eles recebam a cor da paleta.
+const getTdPixels = document.querySelector('#pixel-board');
+  
+getTdPixels.addEventListener('click', function(event) {
+  const selected = document.querySelector('.selected').style.background;
+  event.target.style.backgroundColor  = selected;
+});
