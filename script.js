7e5582b91e22310c3aa18091abbb69e188a0aa09
Mayara Andronico <mayandronico@msn.com>
2021-07-15 22:30:06 -0300

    req 7 e 8 9

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-// Requisito 7 //
-const getColors = document.querySelectorAll('.color');
+const colorBlack = document.querySelector('#black');
+const colorOrange = document.querySelector('#orange');
+const colorRed = document.querySelector('#red');
+const colorBlue = document.querySelector('#blue');
+const quadroPixels = document.querySelector('#pixel-board');
 
-for (let index = 0; index < getColors.length; index += 1) { 
-  getColors[index].addEventListener('click', classColor); 
-   function classColor(event) { 
-   limpaSelect(); 
-    event.target.classList.add('selected'); 
-  }
+// Requisitos 7 - 8
+
+function clickPixel(event) {
+  const classSelect = document.querySelector('.selected');
+  classSelect.classList.remove('selected');
+  event.target.classList.add('selected');
 }
-function limpaSelect(){
-  for (let index = 0; index < getColors.length; index += 1) { 
-    getColors[index].classList.remove('selected');
-  }
+
+colorBlack.addEventListener('click', clickPixel);
+colorOrange.addEventListener('click', clickPixel);
+colorRed.addEventListener('click', clickPixel);
+colorBlue.addEventListener('click', clickPixel);
+
+function paintPixel(event) {
+  const selectedColor = document.querySelector('.selected');
+  event.target.style.backgroundColor = selectedColor.id;
+}
+
+quadroPixels.addEventListener('click', paintPixel);
+
+// Requisito 9
+const clearButton = document.querySelector('#clear-board');
+function clearPixels() {
+  const whhite = document.querySelectorAll('.pixel');
+  whhite.style.backgroundColor = 'white';
 }
