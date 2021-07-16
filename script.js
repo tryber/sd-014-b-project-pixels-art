bfa269143b7270a144d8d6c85c92da466dc37274
Victoria Lorena <victorialorenaf.gomes@gmail.com>
2021-07-16 20:12:47 -0300

    item7 e 8

--- script.js
+++ script.js
@@ -3,5 +3,19 @@ function mudarSelected(evento) {
   selected.classList.remove('selected');
   evento.target.classList.add('selected');
 }
-const colorPallete = document.querySelector('#color-palette');
-colorPallete.addEventListener('click', mudarSelected);
+const pixelBoard = document.querySelector('#pixel-board');
+const black = document.getElementById('Black');
+const red = document.getElementById('Red');
+const green = document.getElementById('Green');
+const yellow = document.getElementById('Yellow');
+
+black.addEventListener('click', mudarSelected);
+red.addEventListener('click', mudarSelected);
+green.addEventListener('click', mudarSelected);
+yellow.addEventListener('click', mudarSelected);
+
+function color(evento) {
+  const select = document.querySelector('.selected');
+  evento.target.style.backgroundColor = select.id;
+}
+pixelBoard.addEventListener('click', color);
