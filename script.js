f17ac72606b15de11493e4197f5ea79b1cf82e28
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-07-16 12:26:36 -0300

    Requesito #6 e refatorando a function creatPixilsBoard.

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
-function paletteColor() {
-  const getPalette = document.getElementsByClassName('color');
-  for (let index of getPalette) {
-    if (index === getPalette[0]) {
-      index.style.background = 'black';
-    } else if (index === getPalette[1]) {
-      index.style.background = 'green';
-    } else if (index === getPalette[2]) {
-      index.style.background = 'blue';
-    } else if (index === getPalette[3]) {
-      index.style.background = 'yellow';
+window.onload = function() {
+  function paletteColor() {
+    const getPalette = document.getElementsByClassName('color');
+    for (let index of getPalette) {
+      if (index === getPalette[0]) {
+        index.style.background = 'black';
+      } else if (index === getPalette[1]) {
+        index.style.background = 'green';
+      } else if (index === getPalette[2]) {
+        index.style.background = 'blue';
+      } else if (index === getPalette[3]) {
+        index.style.background = 'yellow';
+      }
+      index.addEventListener('click', function(event) {
+        for (let indexTwo = 0; indexTwo < getPalette.length; indexTwo += 1) {
+          getPalette[indexTwo].classList.remove('selected');
+          event.target.classList.add('selected');
+        }
+      });
+    }
+  }
+  paletteColor();
+}
+
+function creatPixilsBoard() {
+  const pixelsBoard = document.querySelector('#pixel-board');
+  for (let index = 1; index <= 5; index += 1) {
+    const creatTr = document.createElement('tr');
+    pixelsBoard.appendChild(creatTr);
+    for (let indexTwo = 1; indexTwo <= 5; indexTwo += 1) {
+      const creatTd = document.createElement('td');
+      creatTr.appendChild(creatTd);
+      creatTd.classList.add('pixel')
     }
   }
 }
 
-paletteColor();
-
-function creatPixilsBoard() {
-  const pixelsBoard = document.querySelector('#pixel-board');
-  for (let index = 1; index <= 25; index += 1) {
-    const creatPixel = document.createElement('div');
-    creatPixel.classList.add('pixel');
-    pixelsBoard.appendChild(creatPixel);
-  }
-}
-
 creatPixilsBoard();
+
