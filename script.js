792eeb1e5969c3658c38f6d0398c390ada886ce4
GustavoPD <gudalmoro@gmail.com>
2021-07-17 20:11:55 -0300

    Coloquei titulo e paleta de cores

---
+++ script.js
@@ -0,0 +1,11 @@
+let getBlack = document.getElementsByClassName("color")[0];
+let getRed = document.getElementsByClassName("color")[1];
+let getGreen = document.getElementsByClassName("color")[2];
+let getYellow = document.getElementsByClassName("color")[3];
+
+getBlack.style.background = "black";
+getRed.style.background = "red";
+getGreen.style.background = "green";
+getYellow.style.background = "yellow";
+
+
