7a5fcb998a45cff1317d2ae63cb455dcb209655d
Arturo Vaine <arturo.vaine@gmail.com>
2021-07-17 12:29:07 -0300

    Update.

--- script.js
+++ script.js
@@ -1,88 +1,18 @@
 // 1 - Adicione à página o título "Paleta de Cores". OK
 // 2 - Adicione à página uma paleta de quatro cores distintas. - OK
+// 3 - Adicione a cor preta como a primeira cor da paleta de cores
+// 4 - Adicione à página um quadro de pixels, com 25 pixels.
+// 5 - Faça com que cada elemento do quadro de pixels possua 40 pixels de largura, 40 pixels de altura e seja delimitado por uma borda preta de 1 pixel. --> CSS
 
+// 6 - Defina a cor preta como cor inicial. Ao carregar a página a cor preta já deve estar selecionada para pintar os pixels
 function setBlackColorZero() {
   document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
   document.getElementById('black').classList.add('selected'); // Referência: https://www.w3schools.com/howto/howto_js_add_class.asp
 // const colorZero = 'black';
 }
 
-// 12 - Faça com que as cores da paleta sejam geradas aleatoriamente ao carregar a página.
-// Função abaixo baseada em: https://stackoverflow.com/questions/27406377/javascript-generate-random-number-except-some-values
-function numRandom() {
-  const num = Math.floor(Math.random() * 16777215);
-  return (num === 0 || num === 16777215) ? numRandom() : num; // 0 -> preto; 16777215-> branco
-}
-
-function setRandomColors() {
-  const firstColor = `#${numRandom().toString(16)}`; // Sugestão ESLint: prefer-template
-  const secondColor = `#${numRandom().toString(16)}`;
-  const thirdColor = `#${numRandom().toString(16)}`;
-  // const thirdColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`; --> Sem considerar exceções de preto e branco
-  // 16777215 <--> FFFFFF ; decimal <--> hexadecimal
-  // https://www.w3schools.com/jsref/jsref_tostring_number.asp
-  // https://www.rapidtables.com/convert/number/decimal-to-hex.html
-  document.getElementById('color1').style.backgroundColor = firstColor;
-  document.getElementById('color2').style.backgroundColor = secondColor;
-  document.getElementById('color3').style.backgroundColor = thirdColor;
-}
-
-// 9 - Crie um botão que, ao ser clicado, limpa o quadro preenchendo a cor de todos seus pixels com branco.
-
-const clearButton = document.getElementById('clear-board'); // Atribuir elemento do botão clear-board à constante clearButton
-
-function clearBoard() { 
-  const pixels = document.getElementsByClassName('pixel');
-  const index = document.getElementsByClassName('pixel').length;
-  for (let i = 0; i < index; i += 1) {
-    pixels[i].style.backgroundColor = 'white';
-  }
-}
-
-clearButton.addEventListener('click', clearBoard);
-
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
-
-/*
-function generatePixelBoard() {
-  const boardSize = document.querySelector('#board-size').value; // Atribuir à constante o elemento input com id 'board-size'
-  // const size = boardSize.value;
-  const pixelBoard = document.querySelector('#pixel-board');
-  pixelBoard.innerHTML = ''; // 'Zerar' elemento do board antes de popular com os pixels
-  let aux = size;
-  if (aux < 5) { aux = 5;}; // Requisito 11 - Valor menor que 5, considerar 5 como padrão
-  if (aux > 50) { aux = 50;}; // Requisito 11 - Valor maior que 50, considerar 50 como padrão
-  // Abaixo: duplo loop para percorrer colunas (2o loop) em cada linha da tabela (1o loop)
-  for (let i = 0; i < aux; i += 1) { // Loop de rows, linhas, da tabela
-    const pixelRow = document.createElement('tr'); // Criar e atribuir elemento 'table row' à constante pixelRow
-    document.getElementById('pixel-board').appendChild(pixelRow); // Append do 'table-row' ao pixel-board
-    for (let j = 0; j < size; j += 1) { //  Loop de colunas da tabela
-      const pixel = document.createElement('td'); // Criar e atribuir elemento 'td'(table cell) à constante pixel
-      pixel.className = 'pixel';
-      document.querySelector('tr')[i].appendChild(pixel); // Append do pixel à 'table row' de índice 'i'
-    }
-  }
-}
-
-function createPixelBoard() {
-  if (boardSize.length === 0) { // Input vazio
-    alert('Board inválido!');
-  }
-  generatePixelBoard();
-}
-
-const generateBoard = document.querySelector('#generate-board'); // Atribuir à constante o botão com id 'generate-board'
-generateBoard.addEventListener('click', createPixelBoard);
-*/
-
-window.onload = function start() {
-  setBlackColorZero(); // 6 - Defina a cor preta como cor inicial.
-  setRandomColors(); // 12 - Faça com que as cores da paleta sejam geradas aleatoriamente ao carregar a página.
-  //createPixelBoard();
-};
-
+// 7 - Clicar em uma das cores da paleta, faz com que ela seja selecionada e utilizada para preencher os pixels no quadro.
 const color = document.getElementsByClassName('color');
-
 function selectColor(event) {
   for (let i = 0; i < color.length; i += 1) {
     color[i].classList.remove('selected'); // Remover classe 'selected'
@@ -96,8 +26,10 @@ function pickColor() {
     color[i].addEventListener('click', selectColor);
   }
 }
-
 pickColor();
+
+// 8 - Clicar em um pixel dentro do quadro após selecionar uma cor na paleta,
+// faz com que o pixel seja preenchido com a cor selecionada.
 
 function paint() {
   const colorSelected = document.querySelector('.selected').style.backgroundColor;
@@ -112,11 +44,22 @@ function paintPixel() {
     pixels[i].addEventListener('click', paint);
   }
 }
-
 paintPixel();
 
-//const colorPainter = document.querySelector('.selected').style.backgroundColor;
-//document.getElementsByClassName('pixel')[i].style.backgroundColor = colorPainter; 
+// 9 - Crie um botão que, ao ser clicado, limpa o quadro preenchendo a cor de todos seus pixels com branco.
+
+const clearButton = document.getElementById('clear-board'); // Atribuir elemento do botão clear-board à constante clearButton
+function clearBoard() {
+  const pixels = document.getElementsByClassName('pixel');
+  const index = document.getElementsByClassName('pixel').length;
+  for (let i = 0; i < index; i += 1) {
+    pixels[i].style.backgroundColor = 'white';
+  }
+}
+clearButton.addEventListener('click', clearBoard);
+
+// const colorPainter = document.querySelector('.selected').style.backgroundColor;
+// document.getElementsByClassName('pixel')[i].style.backgroundColor = colorPainter;
 
 /*
 function createPixelBoard() {
@@ -137,3 +80,64 @@ function createPixelBoard() {
   }
 }
 } */
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+const boardSize = document.getElementById('board-size').value;
+function generatePixelBoard() {
+const boardSize = document.getElementById('board-size');
+  // Atribuir à constante o elemento input com id 'board-size'
+  // const size = boardSize.value;
+  let pixelBoard = document.querySelector('#pixel-board');
+  pixelBoard.innerHTML = ''; // 'Zerar' elemento do board antes de popular com os pixels
+  let aux = boardSize;
+  if (aux < 5) { aux = 5;}; // Requisito 11 - Valor menor que 5, considerar 5 como padrão
+  if (aux > 50) { aux = 50;}; // Requisito 11 - Valor maior que 50, considerar 50 como padrão
+  // Abaixo: duplo loop para percorrer colunas (2o loop) em cada linha da tabela (1o loop)
+  for (let i = 0; i < aux; i += 1) { // Loop de rows, linhas, da tabela
+    const pixelRow = document.createElement('tr'); // Criar e atribuir elemento 'table row' à constante pixelRow
+    document.getElementById('pixel-board').appendChild(pixelRow); // Append do 'table-row' ao pixel-board
+    for (let j = 0; j < boardSize; j += 1) { //  Loop de colunas da tabela
+      const pixel = document.createElement('td'); // Criar e atribuir elemento 'td'(table cell) à constante pixel
+      pixel.className = 'pixel';
+      document.querySelector('tr')[i].appendChild(pixel); // Append do pixel à 'table row' de índice 'i'
+    }
+  }
+}
+
+function createPixelBoard() {
+  if (document.querySelector('#generate-board').length === 0) { // Input vazio
+    alert('Board inválido!');
+  }
+  generatePixelBoard();
+}
+
+const generateBoard = document.querySelector('#generate-board'); // Atribuir à constante o botão com id 'generate-board'
+generateBoard.addEventListener('click', createPixelBoard);
+
+// 12 - Faça com que as cores da paleta sejam geradas aleatoriamente ao carregar a página.
+// Função abaixo baseada em: https://stackoverflow.com/questions/27406377/javascript-generate-random-number-except-some-values
+
+function numRandom() {
+  const num = Math.floor(Math.random() * 16777215);
+  return (num === 0 || num === 16777215) ? numRandom() : num; // 0 -> preto; 16777215-> branco
+}
+
+function setRandomColors() {
+  const firstColor = `#${numRandom().toString(16)}`; // Sugestão ESLint: prefer-template
+  const secondColor = `#${numRandom().toString(16)}`;
+  const thirdColor = `#${numRandom().toString(16)}`;
+  // const thirdColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`; -> Sem considerar exceções de preto e branco
+  // 16777215 <--> FFFFFF ; decimal <--> hexadecimal
+  // https://www.w3schools.com/jsref/jsref_tostring_number.asp
+  // https://www.rapidtables.com/convert/number/decimal-to-hex.html
+  document.getElementById('color1').style.backgroundColor = firstColor;
+  document.getElementById('color2').style.backgroundColor = secondColor;
+  document.getElementById('color3').style.backgroundColor = thirdColor;
+}
+
+window.onload = function start() {
+  setBlackColorZero(); // 6 - Defina a cor preta como cor inicial.
+  setRandomColors(); // 12 - Faça com que as cores da paleta sejam geradas aleatoriamente ao carregar a página.
+  // createPixelBoard();
+};
