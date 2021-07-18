e2c385926324cf42a1ee2dc5beaf59438893ee02
Vinicius TnK <viniciusbtanaka@hotmail.com>
2021-07-18 00:56:40 -0400

    add lint:styles fixes

--- script.js
+++ script.js
@@ -1,95 +1,36 @@
-let clearButton = document.getElementById("clear-board");
-let sizeButton = document.getElementById('generate-board');
+const clearButton = document.getElementById("clear-board");
+const sizeButton = document.getElementById('generate-board');
 
-let container = document.getElementById('pixel-board');
-let containerLines = container.children;
+const container = document.getElementById('pixel-board');
+const containerLines = container.children;
 let containerSize = 5;
 
-let colorPalette = document.getElementById('color-palette');
-let colorSize = colorPalette.children.length;
-let color = colorPalette.children;
+const colorPalette = document.getElementById('color-palette');
+const colorSize = colorPalette.children.length;
+const color = colorPalette.children;
 
-window.onload = Render;
-clearButton.addEventListener('click', ClearAll);
-sizeButton.addEventListener('click', UpdateBoard);
-
-function GenerateBoard(params)
+function ChangeSelect(param)
 {
-    for (let i = 0; i < containerSize; i++)
-    {
-        let line = document.createElement('div');
-        container.appendChild(line);
-    }
-    
-    for (let i = 0; i < containerSize; i++)
-    {
-        for (let index = 0; index < containerSize; index++) 
-        {
-            let pixel = document.createElement('span');
-            pixel.className = 'pixel border white';
-            containerLines[i].appendChild(pixel);
-            pixel.addEventListener('click', ChangeColor);
-        }
-    }
-}
+  param.target.classList.forEach(element => 
+  {
+    if (element == 'selected') return;        
+  });
 
-function UpdateBoard(params) 
-{
-    containerSize = document.getElementById('board-size').value;
+  param.target.classList.add('selected')
 
-    if (containerSize < 5 || containerSize > 50)
-    {
-        containerSize = (containerSize < 5)?5:50;
-    }
-
-    //todo
-    // #11 - don't know exactly what it whants and what is means
-    // thus i can't proceed
-
-    // if (containerSize != Number)
-    // {
-    //     window.alert('board invalido!');
-    //     return;
-    // }
-
-    DeleteBoard();
-}
-
-function DeleteBoard(params) 
-{
-    let size = document.getElementById('pixel-board').children.length;
-
-    if (size <= 0 || size == containerSize) return;
-    for (let i = 0; i < size; i++)
-    {
-        container.removeChild(container.firstChild);
-    }
-    GenerateBoard();
+  for (let i = 0; i < colorSize; i++)
+  {
+    if (param.target == color[i]) continue;
+    color[i].classList.remove('selected')
+  }
 }
 
 function AddColorListeners() 
 {
-    for (let i = 0; i < colorSize; i++)
-    {
-        color[i].addEventListener('click', ChangeSelect);
-    }    
-}
-
-function ChangeSelect(param)
-{
-    param.target.classList.forEach(element => 
-    {
-        if (element == 'selected') return;        
-    });
-
-    param.target.classList.add('selected')
-
-    for (let i = 0; i < colorSize; i++)
-    {
-        if (param.target == color[i]) continue;
-
-        color[i].classList.remove('selected')
-    }
+  for (let i = 0; i < colorSize; i++)
+  {
+    color[i].addEventListener('click', ChangeSelect);
+  }    
 }
 
 function ChangeColor(params) 
@@ -118,8 +59,66 @@ function ChangeColorClass(targetList, newColor)
     targetList.toggle(newColor);
 }
 
+function GenerateBoard(params){
+
+  for (let i = 0; i < containerSize; i++)
+  {
+    const line = document.createElement('div');
+    container.appendChild(line);
+  }
+
+  for (let i = 0; i < containerSize; i++)
+  {
+    for (let index = 0; index < containerSize; index++) 
+    {
+      const pixel = document.createElement('span');
+      pixel.className = 'pixel border white';
+      containerLines[i].appendChild(pixel);
+      pixel.addEventListener('click', ChangeColor);
+    }
+  }
+}
+
+function UpdateBoard() 
+{
+    containerSize = document.getElementById('board-size').value;
+
+    if (containerSize < 5 || containerSize > 50)
+    {
+        containerSize = (containerSize < 5)?5:50;
+    }
+
+    /* todo 
+    #11 - don't know exactly what it whants and what is means 
+    thus i can't proceed */
+
+    // if (containerSize != Number)
+    // {
+    //     window.alert('board invalido!');
+    //     return;
+    // }
+
+    DeleteBoard();
+}
+
+function DeleteBoard(params) 
+{
+    let size = document.getElementById('pixel-board').children.length;
+
+    if (size <= 0 || size == containerSize) return;
+    for (let i = 0; i < size; i++)
+    {
+        container.removeChild(container.firstChild);
+    }
+    GenerateBoard();
+}
+
 function Render(params) 
 {
     AddColorListeners();
     GenerateBoard();    
 }
+
+window.onload = Render;
+clearButton.addEventListener('click', ClearAll);
+sizeButton.addEventListener('click', UpdateBoard);
