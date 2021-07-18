fa9b5c967460d2726232e1b4158e8a65604b2712
Caetano Coimbra <caetanocoimbra@gmail.com>
2021-07-18 15:16:28 -0300

    Requisito #8

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ function clickColor (select) {
 const colorPalette = document.querySelector('#color-palette');
 colorPalette.addEventListener('click', clickColor);
 
+function paintPixel(clickEvent) {
+    const colorSelected = document.querySelector('.selected').getAttribute('style');
+    clickEvent.target.style = colorSelected;
+}
+const pixel = document.querySelectorAll('.pixel');
+for (let index = 0; index < pixel.length; index += 1) {
+    pixel[index].addEventListener('click', paintPixel);
+}
+
+  
