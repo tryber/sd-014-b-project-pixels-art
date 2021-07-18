e932ae4fbb860c0a77a1f15b06f96e36f973dbf4
Eliana Nunes <elianadnunes@gmail.com>
2021-07-18 13:06:57 -0300

    Adiciona Requisito 8

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ function recebeClick(evento) {
 
 const paletaDeCores = document.querySelector('#color-palette');
 paletaDeCores.addEventListener('click', recebeClick);
+
+function colorePixel(eventoDeOrigem) {
+  const corSelecionada = document.querySelector('.selected').id;
+  eventoDeOrigem.target.style.backgroundColor = corSelecionada;
+}
+// https://stackoverflow.com/questions/30112838/javascript-onclick-get-the-div-id
+
+const pixel = document.getElementsByClassName('pixel');
+
+for (let indice = 0; indice < pixel.length; indice += 1) {
+  pixel[indice].addEventListener('click', colorePixel);
+}
+
+if (window.onload) {
+  const preto = document.querySelector('#black');
+  document.getElementsByClassName('pixel').style.backgroundColor = preto;
+}
