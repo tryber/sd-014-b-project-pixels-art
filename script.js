00ca3a976e5c670e56739c1a8bbe449ef9019ce1
Vinicius TnK <viniciusbtanaka@hotmail.com>
2021-07-18 00:56:40 -0400

    add lint:styles fixes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const clearButton = document.getElementById("clear-board");
+const clearButton = document.getElementById('clear-board');
 const sizeButton = document.getElementById('generate-board');
 
 const container = document.getElementById('pixel-board');
@@ -7,69 +7,30 @@ let containerSize = 5;
 
 const colorPalette = document.getElementById('color-palette');
 const colorSize = colorPalette.children.length;
-const color = colorPalette.children;
+const colors = colorPalette.children;
 
-function ChangeSelect(param)
+function ChangeColorClass(targetList, newColor)
 {
-  param.target.classList.forEach(element => 
-  {
-    if (element == 'selected') return;        
-  });
+  if (targetList.contains(newColor)) return;
 
-  param.target.classList.add('selected')
-
-  for (let i = 0; i < colorSize; i++)
-  {
-    if (param.target == color[i]) continue;
-    color[i].classList.remove('selected')
-  }
+  targetList.remove(targetList[2]);
+  targetList.add(newColor);
 }
 
-function AddColorListeners() 
+function ChangeColor(params)
 {
-  for (let i = 0; i < colorSize; i++)
-  {
-    color[i].addEventListener('click', ChangeSelect);
-  }    
+  const color = document.querySelector('.selected').id;
+  const paramsClassList = params.target.classList;
+  ChangeColorClass(paramsClassList, color);
 }
 
-function ChangeColor(params) 
+function GenerateBoard()
 {
-    let color = document.querySelector('.selected').id;
-    let paramsClassList = params.target.classList;
-    ChangeColorClass(paramsClassList, color);
-}
-
-function ClearAll(params) 
-{
-    let pixels = document.getElementsByClassName('pixel');
-    let pixelsLenght = pixels.length;
-    
-    for (let i = 0; i < pixelsLenght; i++)
-    {
-        ChangeColorClass(pixels[i].classList, 'white');
-    }
-}
-
-function ChangeColorClass(targetList, newColor) 
-{
-    if (targetList.contains(newColor)) return;
-
-    targetList.toggle(targetList[2]);
-    targetList.toggle(newColor);
-}
-
-function GenerateBoard(params){
-
-  for (let i = 0; i < containerSize; i++)
+  for (let i = 0; i < containerSize; i += 1)
   {
     const line = document.createElement('div');
     container.appendChild(line);
-  }
-
-  for (let i = 0; i < containerSize; i++)
-  {
-    for (let index = 0; index < containerSize; index++) 
+    for (let index = 0; index < containerSize; index += 1)
     {
       const pixel = document.createElement('span');
       pixel.className = 'pixel border white';
@@ -79,44 +40,68 @@ function GenerateBoard(params){
   }
 }
 
-function UpdateBoard() 
+function DeleteBoard()
 {
-    containerSize = document.getElementById('board-size').value;
+  const size = document.getElementById('pixel-board').children.length;
 
-    if (containerSize < 5 || containerSize > 50)
-    {
-        containerSize = (containerSize < 5)?5:50;
-    }
-
-    /* todo 
-    #11 - don't know exactly what it whants and what is means 
-    thus i can't proceed */
-
-    // if (containerSize != Number)
-    // {
-    //     window.alert('board invalido!');
-    //     return;
-    // }
-
-    DeleteBoard();
+  if (size <= 0 || size === containerSize) return;
+  for (let i = 0; i < size; i += 1)
+  {
+    container.removeChild(container.firstChild);
+  }
+  GenerateBoard();
 }
 
-function DeleteBoard(params) 
+function UpdateBoard()
 {
-    let size = document.getElementById('pixel-board').children.length;
-
-    if (size <= 0 || size == containerSize) return;
-    for (let i = 0; i < size; i++)
-    {
-        container.removeChild(container.firstChild);
-    }
-    GenerateBoard();
+  containerSize = document.getElementById('board-size').value;
+  if (containerSize === '') return window.alert('Board inválido!');
+  if (containerSize < 0) return;
+  if (containerSize < 5 || containerSize > 50)
+  {
+    (containerSize < 5) ? containerSize = 5 : containerSize = 50;
+  }
+  DeleteBoard();
 }
 
-function Render(params) 
+function ChangeSelect(params)
 {
-    AddColorListeners();
-    GenerateBoard();    
+  const target = params.target;
+  if (target.classList.contains('selected')) return;
+
+  document.querySelector('.selected').classList.remove('selected');
+  target.classList.add('selected');
+}
+
+function AddColorListeners()
+{
+  for (let i = 0; i < colorSize; i += 1)
+  {
+    colors[i].addEventListener('click', ChangeSelect);
+  }
+}
+
+function ClearAll()
+{
+  const pixels = document.getElementsByClassName('pixel');
+  const pixelsLenght = pixels.length;
+
+  for (let i = 0; i < pixelsLenght; i += 1)
+  {
+    ChangeColorClass(pixels[i].classList, 'white');
+  }
+}
+
+function RandomizeColors()
+{
+  //TODO
+}
+
+function Render()
+{
+  AddColorListeners();
+  GenerateBoard();
+  RandomizeColors();
 }
 
 window.onload = Render;
