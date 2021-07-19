e820435bcef9eeba9ff7d9930c86558c5433492d
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-07-18 22:28:14 -0300

    Requesito #10.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-//Faz com que a paleta de coores possa ser funcional somente depois que a página estiver carregada.
 window.onload = function() {
-  function paletteColor() { //Função que cria uma paleta de cores que já foram definidas.
+  function paletteColor() { 
     const getPalette = document.getElementsByClassName('color');
     for (let index of getPalette) {
       if (index === getPalette[0]) {
@@ -12,9 +11,8 @@ window.onload = function() {
       } else if (index === getPalette[3]) {
         index.style.background = 'yellow';
       }
-      //Aproveitando o mesmo for para adicionar o evento de click aos quadrados de cores da paleta de cores. 
+     
       index.addEventListener('click', function(event) {
-        //Esse segundo loop adiciona a class selected onde não tem quando clicado e remove do outro que tinha.
         for (let indexTwo = 0; indexTwo < getPalette.length; indexTwo += 1) {
           getPalette[indexTwo].classList.remove('selected');
           event.target.classList.add('selected');
@@ -24,13 +22,16 @@ window.onload = function() {
   }
   paletteColor();
 }
-//Função para criar o quadro de pixels dinamicamente.
-function creatPixilsBoard() {
-  const pixelsBoard = document.querySelector('#pixel-board');
-  for (let index = 1; index <= 5; index += 1) {
+
+const tableSize = 5;
+const pixelsBoard = document.querySelector('#pixel-board');
+
+function creatPixilsBoard(boardSize) {
+  pixelsBoard.innerHTML = '';
+  for (let index = 0; index < boardSize; index += 1) {
     const creatTr = document.createElement('tr');
     pixelsBoard.appendChild(creatTr);
-    for (let indexTwo = 1; indexTwo <= 5; indexTwo += 1) {
+    for (let indexTwo = 0; indexTwo < boardSize; indexTwo += 1) {
       const creatTd = document.createElement('td');
       creatTr.appendChild(creatTd);
       creatTd.classList.add('pixel')
@@ -38,7 +39,7 @@ function creatPixilsBoard() {
   }
 }
 
-creatPixilsBoard();
+creatPixilsBoard(tableSize);
 
 //Requesito 08 onde adiciona o evento de click a tabela de pixel e a função faz com que eles recebam a cor da paleta.
 const getTdPixels = document.querySelector('#pixel-board');
@@ -55,7 +56,31 @@ const clearButton = document.querySelector('#clear-board');
 const getPixels = document.querySelectorAll('.pixel');
 
 clearButton.addEventListener('click', function() {
-  for  (let index = 0; index <getPixels.length; index += 1) {
+  for  (let index = 0; index < getPixels.length; index += 1) {
     getPixels[index].style.backgroundColor = 'white';
   }
 });
+
+function verifyNumberUser(boardSize) {
+  if (boardSize < 6) {
+    return 5;
+  }  
+  if (boardSize > 50) {
+    return 50;
+  }
+  return boardSize;
+}
+
+const buttonVqv = document.querySelector('#generate-board');
+
+buttonVqv.addEventListener('click', function() {
+  let boardSize = document.getElementById('board-size').value;
+  
+  if (boardSize.length < 1) {
+    alert('Board Inválido!');
+  } else {
+    boardSize = verifyNumberUser(boardSize);
+    creatPixilsBoard(boardSize);
+  }
+});
+
