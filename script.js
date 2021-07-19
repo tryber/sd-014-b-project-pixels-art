ca1e384ff48afaa353d9518d83de1cffb2abd136
GustavoPD <gudalmoro@gmail.com>
2021-07-19 12:49:24 -0300

    trocando cores

--- script.js
+++ script.js
@@ -3,10 +3,34 @@ let getRed = document.getElementsByClassName("color")[1];
 let getGreen = document.getElementsByClassName("color")[2];
 let getYellow = document.getElementsByClassName("color")[3];
 
+let symbols;
+let color;
+color = "#";
+symbols = '0123456789ABCDEF';
+for (let i=0;i<6;i++){
+color = color + symbols[Math.floor(Math.random() * 16)];
+}
+
+let symbols2;
+let color2;
+color2 = "#";
+symbols2 = '0123456789ABCDEF';
+for (let i=0;i<6;i++){
+color2 = color2 + symbols2[Math.floor(Math.random() * 16)];
+}
+
+let symbols3;
+let color3;
+color3 = "#";
+symbols3 = '0123456789ABCDEF';
+for (let i=0;i<6;i++){
+color3 = color3 + symbols3[Math.floor(Math.random() * 16)];
+}
+
 getBlack.style.background = "black";
-getRed.style.background = "red";
-getGreen.style.background = "green";
-getYellow.style.background = "yellow";
+getRed.style.background = color;
+getGreen.style.background = color2;
+getYellow.style.background = color3;
 
 getBlack.classList.add("selected");
 
