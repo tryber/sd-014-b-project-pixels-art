29bd225db2783deabe18cff534bce1598817feea
Ana-Alice-Honorio <ana.alice.honorio@gmail.com>
2021-07-19 15:17:11 -0300

    Testando alguns requisitos

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-window.onload =function (){
-  
-  let black = document.getElementById('black');
-  let blue = document.getElementById('blue');
-  let red = document.getElementById('red');
-  let green = document.getElementById('green');
+const colorBlack = document.querySelector('#black');
+const colorGreen = document.querySelector('#green');
+const colorPurple = document.querySelector('#purple');
+const colorGrey = document.querySelector('#grey');
+const boxPixels = document.querySelector('#pixel-board');
 
-  function paintBrush(event) {
-    let preColor = document.querySelectorAll('.selected');
-preColor.classList.remove('selected');
-event.target.classList.add('selected');
-input.value = '';
-  }
+//selected (7)//
+function clickPixel (event) {
+  const classSelect = document.querySelector('.selected');
+  classSelect.classList.remove('selected');
+  event.target.classList.add('selected');
 }
+//preenchendo pixel-board (8)//
+colorBlack.addEventListener('click', clickPixel);
+colorGreen.addEventListener('click', clickPixel);
+colorPurple.addEventListener('click', clickPixel);
+colorGrey.addEventListener('click', clickPixel);
+
+function colorPixel(event) {
+  const selectedColor = document.querySelector('.selected');
+  event.target.style.backgroundColor = selectedColor.id;
+}
+
+boxPixels.addEventListener('click', colorPixel);
+
+//botão Limpar (9)//
