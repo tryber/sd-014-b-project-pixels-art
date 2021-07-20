af4a2fecac896ace5ae8d0c45446cae24aa0e76d
paulo <paulothg99@gmail.com>
2021-07-20 12:58:05 -0300

    requisito 4

--- script.js
+++
@@ -1,6 +0,0 @@
-let cleanbottom = document.querySelector('#clear-board');
-cleanbottom.addEventListener('click', cleanbottom);
-
-functioncleanAll () {
-    document.querySelector('#pixel-board').style.backgroundColor = 'white';
-  }
