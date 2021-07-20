c8d6b36bbef8b9bec2dcc978d40b1e46c643cc6f
paulo <paulothg99@gmail.com>
2021-07-19 21:52:42 -0300

    requisitos 1-5

---
+++ script.js
@@ -0,0 +1,5 @@
+ console.log(document.querySelector('.color '))
+documento.querySelector ( '.color' ) . estilo.backgroundColor  =  'preto';
+
+documento.querySelectorAll ( '.color' ) [ 0 ] . classList . substituir ( 'cor' , 'selecionado' )
+console.log(document.querySelectorAll('.selected')[0]);
