5ba7c472a86ab59819a33c4eccad9e3756d994c8
Ana-Alice-Honorio <ana.alice.honorio@gmail.com>
2021-07-24 18:50:23 -0300

    Requisito 12

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function apagarTudo() {
   const pixels = document.querySelectorAll('.pixel');
   for (let index = 0; index < pixels.length; index += 1) {
     pixels[index].style.backgroundColor = 'rgb(255, 255, 255)';
-    //ao ser clicado deixa a tela branca (que é a união mágica de vermelho 255, verde 255 e azul 255//
+    //ao ser clicado deixa a tela branca//
   }
 }
 
@@ -37,3 +37,38 @@ function adicionarApagarEvento() {
 }
 
 adicionarApagarEvento();
+
+//questões 10//  
+const gerarButton= document.querySelector('#generate-board');
+function createBoard() {
+  const media = document.querySelector('#board-size').value;
+
+    if (media === '') {
+    window.alert('Board inválido!');
+    return false;
+}
+ if (media <= 5){
+  media = 5;
+  } else if (media >= 50){
+    media = 50;
+ }
+   return true;
+
+};
+
+gerarButton.addEventListener ('click' , createBoard);
+
+
+// questão 12// referências: https://www.youtube.com/watch?v=mys7LGIlEf8&ab_channel=TECHNOLOGYFAQ
+  function generatorColor() {
+    const newColor = document.getElementsByClassName('color');
+    for (let index = 1; index < newColor.length; index += 1) {
+      const index1 = Math.floor(Math.random() * (255));
+      const index2 = Math.floor(Math.random() * (255));
+      const index3 = Math.floor(Math.random() * (255));
+      newColor[index].style.backgroundColor = `rgb(${index1}, ${index2},${index3})`; 
+//aula da semana, uso do ${//
+    }
+  }
+  
+  generatorColor();
