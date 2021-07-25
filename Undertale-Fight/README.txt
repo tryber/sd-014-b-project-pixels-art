d80c60a9c26ae49c161fd5635d1f6178df4cfa2c
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-07-25 20:14:27 -0300

    Comitao bomba. Inicia implementaçao de tema UNDERTALE

---
+++ Undertale-Fight/README.txt
@@ -0,0 +1,4 @@
+MERCY Font, the UNDERTALE font for the battle buttons!
+
+2016 - MaxiGamer  /  Made w/ Fontstruct(tm)
+UNDERTALE is TM and (c) tobyfox. 
--- script.js
+++ script.js
@@ -2,45 +2,25 @@ window.onload = function colorsPalet(){
     const board = document.getElementById('pixel-board');
     const clearButton = document.getElementById('clear-board');
     const generateBoardButton = document.getElementById("generate-board");
-    let clicado = false;
-
-function generatePalet(){     
-    let palet = document.getElementById('color-palette'); 
-    for(let index = 1; index <= 4; index += 1){   
-        let paletItem = document.createElement('div');
-        paletItem.className = "color";
-        palet.appendChild(paletItem);
-    }
-    let paletItem = document.querySelectorAll('.color');
-    paletItem[0].style.backgroundColor = "black";
-    paletItem[0].classList.add('selected');
-    paletItem[0].id = "black";
-    paletItem[1].style.backgroundColor = "red";
-    paletItem[1].id = "red";
-    paletItem[2].style.backgroundColor = "blue";
-    paletItem[2].id = "blue";
-    paletItem[3].style.backgroundColor = "green";
-    paletItem[3].id = "green";
-}    
-
-function generateFirstBoard(){
-    boardForInputNumber(5);
-}    
-
-
-
+    const boarSizeInput = document.getElementById('board-size');
+    let  printed = false;
+    let changeSizeBoard = false;
+    /* const audioVolume = document.getElementById('audio'); */
+    audio.volume = 0.1;
+    // referência de como alterar volume do audio.
+    //https://stackoverflow.com/questions/33747398/html-audio-tag-volume
+    //Não precisei da const audioVolume ? Oo
  function boardForInputNumber(input){
         for(indexColumn = 1; indexColumn  <= input; indexColumn  += 1){
             const board = document.getElementById('pixel-board');
             let column = document.createElement('div');
             board.appendChild(column);
-            column.className = 'col';
-            column.addEventListener('mousedown', paintingBoard);
+            column.className = 'col';             
             for(indexRow = 1; indexRow <= input; indexRow += 1){
                 let row = document.createElement('div');
                 row.className = "pixel";
+                row.addEventListener('mousedown', paintingBoard);  
                 column.appendChild(row);
-                board.addEventListener('click', paintingBoard);
             }   
         }   
 } 
@@ -56,67 +36,38 @@ function deleteBoard(){
 }
 
 function generateBoard(){
-  let input = document.getElementById("board-size").value;
-    if(input === ''){
-        alert('Board inválido!');
-    }else if(input <= 5){
+  let input = document.getElementById("board-size");
+  input.addEventListener('mousemove', () => {
+    changeSizeBoard = true;
+    if(changeSizeBoard === true) {
         deleteBoard();
-        boardForInputNumber(5);
-    }else if(input >= 50){
-        deleteBoard();
-        boardForInputNumber(50);
-    }else{
-        deleteBoard();
-        boardForInputNumber(input);
-    }     
-} 
+        boardForInputNumber(input.value);
+    }
+    input.addEventListener('mouseup', () => {
+      changeSizeBoard = false;
+    })
+  });
+}
 
-function selectColor(){
-    let optionsPalet = document.getElementsByClassName('color');
-        optionsPalet[0].addEventListener('click', function(event){
-            for(index = 0; index < optionsPalet.length; index += 1){
-                optionsPalet[index].classList.remove('selected');
-                event.target.classList.add('selected');
-            }
-        })    
-        optionsPalet[1].addEventListener('click', function(event){
-                for(index = 0; index < optionsPalet.length; index += 1){
-                    optionsPalet[index].classList.remove('selected');
-                    event.target.classList.add('selected');
-                }
-        })            
-        optionsPalet[2].addEventListener('click', function(event){
-            for(index = 0; index < optionsPalet.length; index += 1){
-                optionsPalet[index].classList.remove('selected');
-                event.target.classList.add('selected');
-            }
-        })        
-        optionsPalet[3].addEventListener('click', function(event){
-            for(index = 0; index < optionsPalet.length; index += 1){
-                optionsPalet[index].classList.remove('selected');
-                event.target.classList.add('selected');
-            }
-        })        
-    
-            
-        
-}     
+
 function paintingBoard(event){        
     let colorSelected = document.querySelector('.selected');
-    event.target.style.backgroundColor = colorSelected.id; 
-     clicado = true;   
- /*    let pix = document.querySelectorAll('.pixel');
-    for(let key of pix){
-    key.addEventListener('mouseup', function () {
-        clicado = false;
-      });
-      key.addEventListener('mouseover', function () {
-        if (clicado) {
-          key.backgroundColor = 'yellow';
+    event.target.style.backgroundColor = colorSelected.value; 
+    printed = true;
+     let pix = document.querySelectorAll('.pixel');
+    for(let index = 0; index < pix.length; index += 1){
+      pix[index].addEventListener('mousemove', function () {
+        if (printed === true) {
+            pix[index].style.backgroundColor = colorSelected.value;
         }
-      }); 
+      });   
+      pix[index].addEventListener('mouseup', function () {
+        printed = false;
+        
+      });
                 
-    } */
+    }
+    
 }    
     
 
@@ -130,11 +81,16 @@ function clearBoard(){
 }       
 
 
-/* board.addEventListener('mousedown', paintingBoard); */
 clearButton.addEventListener('click', clearBoard);        
 generateBoardButton.addEventListener('click', generateBoard);
-generateFirstBoard();
-generatePalet();    
-selectColor();
+boarSizeInput.addEventListener('mousedown', () => {
+    changeSizeBoard = true;
+    generateBoard();
+});
 
+boardForInputNumber(30);
 }
+
+
+
+  
