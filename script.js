c73570a2a0da0a7702086515083a5dbc0e0e0dc9
Adilson Gabriel <adilsongb.rabelo@gmail.com>
2021-07-26 10:56:05 -0300

    Refatorando projeto - consertado alguns erros

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function setColors() {
   const botoesCores = document.querySelectorAll('.color');
 
   for (let i = 1; i <= 3; i += 1) {
-    const r = parseInt(Math.random() * 255);
-    const g = parseInt(Math.random() * 255);
-    const b = parseInt(Math.random() * 255);
+    const r = parseInt(Math.random() * 255, 10);
+    const g = parseInt(Math.random() * 255, 10);
+    const b = parseInt(Math.random() * 255, 10);
 
-    botoesCores[i].style.backgroundColor = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+    botoesCores[i].style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
   }
 }
 
@@ -22,20 +22,8 @@ function selectColor(event) {
 }
 
 function colorPixel(event) {
-  if (pintarTudo === true) {
-
-    const pixel = document.querySelectorAll('.pixel');
-    let corPixel = event.target.style.backgroundColor;
-
-    for (let i = 0; i < pixel.length; i += 1) {
-      if (pixel[i].style.backgroundColor == corPixel) {
-        pixel[i].style.backgroundColor = colorSelected;
-      }
-    }
-
-  } else {
-    event.target.style.backgroundColor = colorSelected;
-  }
+  const pixelClicado = event.target;
+  pixelClicado.style.backgroundColor = colorSelected;
 }
 
 function resetPalette() {
@@ -171,34 +159,30 @@ function initialBoard() {
     tablePaint.id = 'pixel-board';
   
     let linhaBoard = document.createElement('tr');
-    let pixel = '<td></td>';
-  
+    const pixel = '<td></td>';
+
     for (let i = 1; i <= 5; i += 1) {
-  
       linhaBoard.innerHTML += pixel;
       linhaBoard.children[i - 1].classList.add('pixel');
-  
     }
-  
-      for (let i = 1; i <= 5; i += 1) {
-        tablePaint.innerHTML += linhaBoard.innerHTML;
-      }
-  
-      paitingArea.appendChild(tablePaint);
-  
-      let pixels = document.querySelectorAll('.pixel');
-      for (let i = 0; i < pixels.length; i += 1) {
-        pixels[i].addEventListener('click', colorPixel);
-      }
 
+    for (let i = 1; i <= 5; i += 1) {
+      tablePaint.innerHTML += linhaBoard.innerHTML;
+    }
+
+    paitingArea.appendChild(tablePaint);
+
+    const pixels = document.querySelectorAll('.pixel');
+    for (let i = 0; i < pixels.length; i += 1) {
+      pixels[i].addEventListener('click', colorPixel);
+    }
   }
-
 }
 
-window.onload = function () {
+window.onload = () => {
   setColors();
   eventAdd();
   initialBoard();
   const colorBlack = document.querySelector('.color');
   colorBlack.classList.add('selected');
-}
+};
