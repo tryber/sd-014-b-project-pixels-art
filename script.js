53a5bcec20740f5e72b753b551c150e1923cedba
= <luz.matheus11@gmail.com>
2021-07-29 09:30:56 -0300

    Requesito 7 e 8

--- script.js
+++ script.js
@@ -1,42 +1,73 @@
-function createPixels(number = 5 * 5){
+window.onload = function(){
+
+const one = document.querySelector('.um')
+const two = document.querySelector('.dois');
+const three = document.querySelector('.tres');
+const four = document.querySelector('.quatro');
+
+// Requesito 4
+  function createPixels(number = 5 * 5){
     for(let index = 0; index < number; index += 1){
      const pixelBoard = document.querySelector('#pixel-board');
      const quadrado = document.createElement('div');
      quadrado.classList = 'pixel';
      pixelBoard.appendChild(quadrado);
+     quadrado.style.backgroundColor = 'white';
    }
  }
-
  createPixels();
 
+// Requesito 7
 
-//function colorRandom (){
-  //let b = Math.random() * 255;
-  //let c = Math.random() * 255;
-  //let d = Math.random() * 255;
-  //document.querySelector('.dois').style.backgroundColor = rgb(b, c, d);
-  //document.querySelector('.tres').style.backgroundColor = rgb(c, b, d);
-  //document.querySelector('.quatro').style.backgroundColor = rgb(c, d, b);
-//};
+one.addEventListener('click', selectedColor);
+two.addEventListener('click', selectedColor);
+three.addEventListener('click', selectedColor);
+four.addEventListener('click', selectedColor);
 
-//colorRandom();
-
-
-function colorOneBlack (){
-  let colorOne = document.querySelector('.um');
-  colorOne.classList.add('selected');
-  colorOne.style.background = 'black';
-}
-
-colorOneBlack();
+function selectedColor(event){
+  const selectedClass = document.querySelector('.selected');
+  selectedClass.classList.remove('selected');
+  event.target.classList.add('.selected');
+};
 
 
 
+// Requesito 8
+const pixelColor = document.querySelector('.pixel');
+pixelColor.addEventListener('click', function(event){
+  event.target.classList.contains('.selected');
+})
 
-//const color1 = document.querySelector('.um');
-//const color2 = document.querySelector('.dois');
-//const color3 = document.querySelector('.tres');
-//const color4 = document.querySelector('.quatro');
+// Requesito 9
+const button = document.createElement('button');
+button.id = 'clear-board';
+button.innerText = 'Limpar';
+const div = document.querySelector('.button-limpar');
+div.appendChild(button);
 
+button.addEventListener('click', function(event){
+  const button = document.querySelector('clear-board');
+  const selected = document.querySelector('.selected');
+
+})
+
+// Requesito 10
+const input = document.createElement('input');
+const buttonInput = document.createElement('button');
+const divInput = document.querySelector('.input-button');
+input.minLength = '5';
+input.maxLength = '50';
+input.id = 'board-size';
+input.placeholder = 'Defina';
+buttonInput.id = 'generate-board';
+buttonInput.innerText = 'VQV';
+divInput.appendChild(input);
+divInput.appendChild(buttonInput);
+
+buttonInput.addEventListener('click', function(){
+
+});
+
+};
 
 
