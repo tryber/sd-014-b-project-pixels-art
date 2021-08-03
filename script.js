650f302853a8ffb954a167775b2845297986a837
Vitor <vitoremerich.contato@gmail.com>
2021-08-03 17:43:45 -0300

    Requisitos 8 e 9

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const preto = document.getElementById('preto');
-const vermelho = document.getElementById('vermelho');
-const azul = document.getElementById('azul');
-const verde = document.getElementById('verde');
+const preto = document.getElementById('black');
+const vermelho = document.getElementById('red');
+const azul = document.getElementById('blue');
+const verde = document.getElementById('green');
 let corPrimaria = document.querySelector('.selected');
+const pixels = document.querySelectorAll('.pixel');
+const botao = document.getElementById('clear-board');
 
 function trocaCor(event) {
   const selecionaCor = event.target;
@@ -16,3 +18,18 @@ preto.addEventListener('click', trocaCor);
 vermelho.addEventListener('click', trocaCor);
 azul.addEventListener('click', trocaCor);
 verde.addEventListener('click', trocaCor);
+
+function pintar(event) {
+  const selected = document.getElementsByClassName('selected');
+  event.target.style.backgroundColor = selected[0].id;
+} for (let index = 0; index < pixels.length; index += 1) {
+  pixels[index].addEventListener('click', pintar);
+}
+
+function limpar() {
+  const pixelBoard = document.querySelectorAll('.pixel');
+  for (const key of pixelBoard) {
+    key.style.backgroundColor = 'rgb(255,255,255)';
+  }
+}
+botao.addEventListener('click', limpar);
