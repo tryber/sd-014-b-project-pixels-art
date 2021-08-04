8e9f7fcb8092a3054d53f051295dc5dc73b74b48
Jamez Mayke <jamezmayke@gmail.com>
2021-08-04 00:44:39 -0300

    corrigindo erros no lint

---
+++ script.js
@@ -0,0 +1,5 @@
+const colorPalette = document.querySelector('#color-palette');
+const firstColor = document.querySelector('.first-color');
+const secondColor = document.querySelector('.second-color');
+const thirdColor = document.querySelector('.third-color');
+const fourColor = document.querySelector('.four-color');
